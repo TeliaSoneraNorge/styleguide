d53c42f220157fb7b1e4ce33c9a9096d21fb7cc9
Alex York <alexyork1985@gmail.com>
2017-05-31 07:09:06 +0200

    Show component names on HomePage

--- src/client/pages/HomePage.jsx
+++ src/client/pages/HomePage.jsx
@@ -1,9 +1,38 @@
+import _ from 'lodash';
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+import Heading from '../../components/atoms/Heading/Heading';
+import { getMergedMetadata } from '../../componentMetadata';
+import { extractComponentNameFromPath, groupComponentMetadataByType } from '../utils/componentUtil';
+
+const componentMetadata = getMergedMetadata();
+const groupedComponentMetadata = groupComponentMetadataByType(componentMetadata);
 
 const HomePage = () =>
-    <div>
-        <p>This is the HomePage</p>
+    <div className="content-wrapper">
+        <Heading level={1} text="Telia Norge Styleguide" />
+        <p>Welcome to the Telia Norge styleguide.</p>
+
+        <h2 className="heading heading--level-2"><a href="/components">Components</a></h2>
+        <Heading level={3} text="Atoms" />
+        <ul>
+            {_.map(groupedComponentMetadata.atoms, (metadata, path) =>
+                <li key={path}>{extractComponentNameFromPath(path)} ({path})</li>
+            )}
+        </ul>
+        <Heading level={3} text="Molecules" />
+        <ul>
+            {_.map(groupedComponentMetadata.molecules, (metadata, path) =>
+                <li key={path}>{extractComponentNameFromPath(path)} ({path})</li>
+            )}
+        </ul>
+        <Heading level={3} text="Organisms" />
+        <ul>
+            {_.isEmpty(groupedComponentMetadata.organisms) ? <li>(none)</li> : null}
+            {_.map(groupedComponentMetadata.organisms, (metadata, path) =>
+                <li key={path}>{extractComponentNameFromPath(path)} ({path})</li>
+            )}
+        </ul>
     </div>;
 
 export default HomePage;
--- src/client/utils/componentUtil.js
+++ src/client/utils/componentUtil.js
@@ -1,5 +1,27 @@
+import _ from 'lodash';
+
 export function extractComponentNameFromPath(path) {
     const pathParts = path.split('/');
     const componentFileName = pathParts[pathParts.length - 1];
     return componentFileName.split('.')[0];
+}
+
+export function groupComponentMetadataByType(componentMetadata) {
+    const groupedMetadata = {
+        atoms: {},
+        molecules: {},
+        organisms: {}
+    };
+
+    _.forEach(componentMetadata, (metadata, path) => {
+        if (_.includes(path, 'atoms')) {
+            groupedMetadata.atoms[path] = metadata;
+        } else if (_.includes(path, 'molecules')) {
+            groupedMetadata.molecules[path] = metadata;
+        } else if (_.includes(path, 'organisms')) {
+            groupedMetadata.organisms[path] = metadata;
+        }
+    });
+
+    return groupedMetadata;
 }
