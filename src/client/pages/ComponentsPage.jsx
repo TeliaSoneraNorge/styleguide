f7cf4b6062dea81d619f8c2c057f52e5c6d74a40
Alex York <alexyork1985@gmail.com>
2017-05-31 07:08:56 +0200

    Code simplification

--- src/client/pages/ComponentsPage.jsx
+++ src/client/pages/ComponentsPage.jsx
@@ -1,8 +1,7 @@
-import _ from 'lodash';
 import React from 'react';
 
-import { getMergedMetadata } from '../../componentMetadata';
 import Heading from '../../components/atoms/Heading/Heading';
+import { getMergedMetadata } from '../../componentMetadata';
 
 import HtmlComponent from '../components/HtmlComponent';
 import ReactComponent from '../components/ReactComponent';
@@ -14,16 +13,10 @@ const ComponentsPage = () =>
         <Heading level={1} text="Components" />
         {Object.keys(componentMetadata).map((path) => {
             const metadata = componentMetadata[path];
-            return (
-                <div key={path}>
-                    <p>{path}</p>
-                    {metadata.isHtmlComponent
-                        ? <HtmlComponent path={path} metadata={metadata} />
-                        : <ReactComponent path={path} metadata={metadata} />}
-                </div>
-            );
+            return metadata.isHtmlComponent
+                ? <HtmlComponent key={path} path={path} metadata={metadata} />
+                : <ReactComponent key={path} path={path} metadata={metadata} />;
         })}
-
     </div>;
 
 export default ComponentsPage;
