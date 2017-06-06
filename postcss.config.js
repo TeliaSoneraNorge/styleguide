aeac9aae3dca11e3c74096226399a35b6abac929
Cathrine Holm <cathrholm@gmail.com>
2017-06-06 19:35:39 +0200

    accordion done

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     plugins: [
         require('postcss-easy-import'),
+        require('postcss-variables'),
         require('postcss-nested')
         //require('autoprefixer')
     ]
