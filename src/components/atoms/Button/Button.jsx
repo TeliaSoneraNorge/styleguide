57bbd4930078ed643357379aebd25df974ec2ea4
Cathrine Holm <cathrholm@gmail.com>
2017-06-09 11:41:45 +0200

    divided button styling into small and big buttons

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 /**
  * This is a Button. Use it when you want people to click it. HELLO?
  */
-const Button = ({ text, kind = 'default', onClick }) =>
-    <button className={`button button--${kind}`} onClick={onClick}>{text}</button>;
+const Button = ({ size, text, kind = 'default', onClick }) =>
+    <button className={`button button--${size} button--${kind}`} onClick={onClick}>{text}</button>;
 
 export default Button;
