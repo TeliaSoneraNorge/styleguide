1f86630e60d8dfd095ac12de30e3eaed22ac3d57
Alex York <alexyork1985@gmail.com>
2017-06-09 15:03:45 +0200

    Footer is styled and responsive

--- src/components/molecules/PageFooter/PageFooter.jsx
+++ src/components/molecules/PageFooter/PageFooter.jsx
@@ -5,13 +5,26 @@ import React from 'react';
  * It contains a small number of important, side-wide links.
  */
 const PageFooter = () =>
-    <header className="page-footer">
-        <div className="content-wrapper">
-            <a className="page-footer__link" href="#">Om Telia</a>
-            <a className="page-footer__link" href="#">Telia butikker</a>
-            <a className="page-footer__link" href="#">Presse</a>
-            <a className="page-footer__link" href="#">Jobb i Telia</a>
+    <footer className="page-footer">
+        <div className="page-footer__content">
+            <div className="page-footer__content-wrapper content-wrapper">
+                <nav className="page-footer__main-nav">
+                    <a className="page-footer__link" href="#">Om Telia</a>
+                    <a className="page-footer__link" href="#">Telia butikker</a>
+                    <a className="page-footer__link" href="#">Presse</a>
+                    <a className="page-footer__link" href="#">Jobb i Telia</a>
+                </nav>
+                <nav className="page-footer__special-nav">
+                    <a className="page-footer__link" href="#">Personvern og Cookies</a>
+                </nav>
+
+                <div className="page-footer__pebble page-footer__pebble--0"></div>
+                <div className="page-footer__pebble page-footer__pebble--1"></div>
+                <div className="page-footer__pebble page-footer__pebble--2"></div>
+                <div className="page-footer__pebble page-footer__pebble--3"></div>
+                <div className="page-footer__pebble page-footer__pebble--4"></div>
+            </div>
         </div>
-    </header>;
+    </footer>;
 
 export default PageFooter;
