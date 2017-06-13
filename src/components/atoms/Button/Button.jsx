4a5a8f4cde817d6ba41a24313af7c2fe58304f95
Alex York <alexyork1985@gmail.com>
2017-06-13 15:42:37 +0200

    Improved Button components

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+function classNames(kind, size) {
+    const classNames = ['button'];
+    if (kind) {
+        classNames.push(`button--${kind}`);
+    }
+    if (size) {
+        classNames.push(`button--${size}`);
+    }
+    return classNames.join(' ');
+}
+
 /**
- * This is a Button. Use it when you want people to click it. HELLO?
+ * This is a Button. Use it when you want people to click it.
  */
-const Button = ({ size, text, kind = 'default', onClick }) =>
-    <button className={`button button--${size} button--${kind}`} onClick={onClick}>{text}</button>;
+const Button = ({ text, kind, size, onClick }) =>
+    <button className={classNames(kind, size)} onClick={onClick}>{text}</button>;
 
 export default Button;
