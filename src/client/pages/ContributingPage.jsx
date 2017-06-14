815864c8aec9492fc80baf0c74d4c9825bad9722
Alex York <alexyork1985@gmail.com>
2017-06-14 10:10:27 +0200

    Removed dead code

--- src/client/pages/ContributingPage.jsx
+++ src/client/pages/ContributingPage.jsx
@@ -6,12 +6,6 @@ import WritingCssSection from '../components/WritingCssSection';
 
 import Heading from '../../components/atoms/Heading/Heading';
 
-import { getMergedMetadata } from '../../componentMetadata';
-import { extractComponentNameFromPath, groupComponentMetadataByType } from '../utils/componentUtil';
-
-const componentMetadata = getMergedMetadata();
-const groupedComponentMetadata = groupComponentMetadataByType(componentMetadata);
-
 const ContributingPage = ({ colors }) =>
     <div className="content-wrapper">
         <Heading level={1} text="Contributing to the Styleguide" />
