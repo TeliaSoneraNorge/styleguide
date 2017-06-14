93cff84740846442a9dde4c950a75a13ecf0f5a6
Alex York <alexyork1985@gmail.com>
2017-06-14 10:11:20 +0200

    Correct heading sizes

--- src/client/components/TypographySection.jsx
+++ src/client/components/TypographySection.jsx
@@ -7,7 +7,7 @@ const TypographySection = () =>
         <Heading level={2} text="Typography" />
         <p>Our fonts, sizes and weights should be consistent across the page. The Pebble font should only be used in (TODO) scenarios because (TODO).</p>
         <div className="sg-component__example-rendered">
-            {_.map([1, 2, 3, 4, 5, 6], (level) =>
+            {_.map([1, 2, 3, 4], (level) =>
                 <Heading key={level} level={level} text={`h${level}. Heading level ${level}`} />
             )}
         </div>
