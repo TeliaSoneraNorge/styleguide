5e8f614e4d9a5539dae8f7cc4ccaa8e99138e3b0
Cathrine Holm <cathrine.holm@bouvet.no>
2017-06-30 13:17:32 +0200

    Update heading and add rem function

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  plugins: {
-    'postcss-easy-import': {
-      extensions: '.pcss'
-    },
-    'postcss-nested': {},
-    'postcss-custom-properties': {},
-    'postcss-calc': {}
-  }
+    plugins: {
+        'postcss-easy-import': {
+            extensions: '.pcss'
+        },
+        'postcss-functions': {
+            functions: {
+                rem: value => `${value / 16}rem`
+            }
+        },
+        'postcss-nested': {},
+        'postcss-custom-properties': {},
+        'postcss-calc': {}
+    }
 };
