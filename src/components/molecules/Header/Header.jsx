e9c146869362a136e5ba26b8be4550e61da1373e
Cathrine Holm <cathrine.holm@bouvet.no>
2017-07-06 14:28:46 +0200

    fixed typo

--- src/components/molecules/Header/Header.jsx
+++ src/components/molecules/Header/Header.jsx
@@ -1,10 +1,10 @@
-import ClassNames from 'classnames';
+import classNames from 'classnames';
 import React from 'react';
 
 import Heading from '../../atoms/Heading/Heading';
 
 const Header = ({ iconUrl, runningTitle, pageTitle, withMask, withContentOverlap, children }) =>
-    <header className={ClassNames('header', { 'header--with-mask': withMask, 'header--with-content-overlap': withContentOverlap })}>
+    <header className={classNames('header', { 'header--with-mask': withMask, 'header--with-content-overlap': withContentOverlap })}>
         <img className="header__icon" src={iconUrl} role="presentation" />
         <span className="header__subtitle ">{runningTitle}</span>
         <Heading level={1} children={pageTitle} />
