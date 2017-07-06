f68779132716ec618acbc59da669b3aae0c01407
Alex York <alexyork1985@gmail.com>
2017-07-06 09:53:38 +0200

    Use the classnames module in Header

--- src/components/molecules/Header/Header.jsx
+++ src/components/molecules/Header/Header.jsx
@@ -1,20 +1,10 @@
+import ClassNames from 'classnames';
 import React from 'react';
+
 import Heading from '../../atoms/Heading/Heading';
 
-function classNames(withMask, withContentOverlap) {
-    const classNames = ['header'];
-    if (withMask) {
-        classNames.push(`header--with-mask`)
-    }
-    if (withContentOverlap) {
-        classNames.push(`header--with-content-overlap`)
-    }
-
-    return classNames.join(' ');
-}
-
 const Header = ({ iconUrl, runningTitle, pageTitle, withMask, withContentOverlap, children }) =>
-    <header className={classNames(withMask, withContentOverlap)}>
+    <header className={ClassNames('header', { 'header--with-mask': withMask, 'header--with-content-overlap': withContentOverlap })}>
         <img className="header__icon" src={iconUrl} role="presentation" />
         <span className="header__subtitle ">{runningTitle}</span>
         <Heading level={1} text={pageTitle} />
