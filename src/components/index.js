38a076be3f1c5ae1a91ef501cb60a5d03f62067e
Alex York <alexyork1985@gmail.com>
2017-08-08 11:40:55 +0200

    Renamed some components

--- src/components/index.js
+++ src/components/index.js
@@ -8,6 +8,7 @@ import Button from './atoms/Button/Button';
 import Heading from './atoms/Heading/Heading';
 import Link from './atoms/Link/Link';
 import LabelledCheckbox from './atoms/LabelledCheckbox/LabelledCheckbox';
+import ToggleButton from './atoms/ToggleButton/ToggleButton';
 
 import FocusBox from './molecules/FocusBox/FocusBox';
 import Header from './molecules/Header/Header'
@@ -19,6 +20,7 @@ export default {
     'src/components/atoms/Heading/Heading.jsx': Heading,
     'src/components/atoms/Link/Link.jsx': Link,
     'src/components/atoms/LabelledCheckbox/LabelledCheckbox.jsx': LabelledCheckbox,
+    'src/components/atoms/ToggleButton/ToggleButton.jsx': ToggleButton,
 
     'src/components/molecules/FocusBox/FocusBox.jsx': FocusBox,
     'src/components/molecules/Header/Header.jsx': Header,
