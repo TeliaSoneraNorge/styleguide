667007cde90a0cb951904bfd6335f5d0b96ee5b5
Alex York <alexyork1985@gmail.com>
2017-08-24 15:29:36 +0200

    Added some subscriptions to the sample page

--- src/client/pages/SubscriptionSamplePage.jsx
+++ src/client/pages/SubscriptionSamplePage.jsx
@@ -1,5 +1,54 @@
 import React from 'react';
 
+import Subscription from '../../components/molecules/Subscription/Subscription';
+
+const subscriptionPropsList = [
+    {
+        "name": "SMART Mini",
+        "dataAmount": 1,
+        "dataUnit": "GB",
+        "price": 249,
+        "color": "pink"
+    },
+    {
+        "name": "SMART Basis",
+        "dataAmount": 3,
+        "dataUnit": "GB",
+        "price": 299,
+        "color": "light-orange"
+    },
+    {
+        "name": "SMART Pluss",
+        "dataAmount": 6,
+        "dataUnit": "GB",
+        "price": 399,
+        "color": "green",
+        "isExpanded": true
+    },
+    {
+        "name": "SMART Super",
+        "dataAmount": 15,
+        "dataUnit": "GB",
+        "price": 449,
+        "color": "teal",
+        "isShowingFeatures": true
+    },
+    {
+        "name": "SMART Total",
+        "dataAmount": 40,
+        "dataUnit": "GB",
+        "price": 699,
+        "color": "blue",
+        "isShowingFeatures": true
+    }
+];
+
+// 0) Currently, only content area 3 has some subscriptions, so add some other subscriptions to the other content areas
+//    rename "subscriptionPropsList" to "forAlleSubscriptionProps" and create a new "kontantSubscriptionProps" etc etc
+// 1) Make this an ES6 class, not a functional component
+// 2) Set a default state (selected tab index = 0)
+// 3) Add the "--selected" and "--open" classes if the selected state is  blah blah
+
 const SubscriptionSamplePage = () =>
     <div>
         <div className="center-align">
@@ -42,8 +91,8 @@ const SubscriptionSamplePage = () =>
             </ul>
             <div className="funky-tabs__content" id="tab-panel-1" role="tabpanel" aria-labelledby="tab-1">
                 <section className="main-content">
-                    This is the Funky Tabs component which consists of four tabs and four panels.
-                    Each tab contains a link with an image and a text, and this link controls the associated panel's visibility.
+                    {subscriptionPropsList.map((subscriptionProps, i) =>
+                        <Subscription key={i} {...subscriptionProps} />)}
                 </section>
             </div>
             <div className="funky-tabs__content" id="tab-panel-2" role="tabpanel" aria-labelledby="tab-2">
@@ -54,8 +103,8 @@ const SubscriptionSamplePage = () =>
             </div>
             <div className="funky-tabs__content funky-tabs__content--open" id="tab-panel-3" role="tabpanel" aria-labelledby="tab-3">
                 <section className="main-content">
-                    This is the Funky Tabs component which consists of four tabs and four panels.
-                    Each tab contains a link with an image and a text, and this link controls the associated panel's visibility.
+                    {subscriptionPropsList.map((subscriptionProps, i) =>
+                        <Subscription key={i} {...subscriptionProps} />)}
                 </section>
             </div>
             <div className="funky-tabs__content" id="tab-panel-4" role="tabpanel" aria-labelledby="tab-4">
