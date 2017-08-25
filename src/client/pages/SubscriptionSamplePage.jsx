672434c44ee44614690780401ffbcfb920c49e33
Cathrine Holm <cathrholm@gmail.com>
2017-08-25 13:33:08 +0200

    SubscriptionSamplePage with clickable tabs

--- src/client/pages/SubscriptionSamplePage.jsx
+++ src/client/pages/SubscriptionSamplePage.jsx
@@ -94,75 +94,110 @@ const kontantkortSubscriptionProps = [
     }
 ];
 
-// 1) Make this an ES6 class, not a functional component
-// 2) Set a default state (selected tab index = 0)
-// 3) Add the "--selected" and "--open" classes if the selected state is  blah blah
+export default class SubscriptionSamplePage extends React.Component {
+    constructor(props) {
+        super(props);
 
-const SubscriptionSamplePage = () =>
-    <div>
-        <div className="center-align">
-            <h1 className="heading heading--level-1">Subscription</h1>
-        </div>
-        <div className="funky-tabs">
-            <ul className="funky-tabs__tabs" role="tablist">
-                <li className="funky-tabs__tab" id="tab-1" aria-selected="true" role="tab" aria-controls="tab-panel-1">
-                    <a href="#">
-                        <img className="funky-tabs__tab-image" src="/public/images/under15.png" />
-                        <div className="funky-tabs__tab-text">
-                            <span className="link">Under 15 år</span>
-                        </div>
-                    </a>
-                </li>
-                <li className="funky-tabs__tab" id="tab-2" aria-selected="false" role="tab" aria-controls="tab-panel-2">
-                    <a href="#">
-                        <img className="funky-tabs__tab-image" src="/public/images/under28.png" />
-                        <div className="funky-tabs__tab-text">
-                            <span className="link">Under 28 år</span>
-                        </div>
-                    </a>
-                </li>
-                <li className="funky-tabs__tab funky-tabs__tab--selected" id="tab-3" aria-selected="false" role="tab" aria-controls="tab-panel-3">
-                    <a href="#">
-                        <img className="funky-tabs__tab-image" src="/public/images/for-alle.png" />
-                        <div className="funky-tabs__tab-text">
-                            <span className="link"> For alle</span>
-                        </div>
-                    </a>
-                </li>
-                <li className="funky-tabs__tab" id="tab-4" aria-selected="false" role="tab" aria-controls="tab-panel-4">
-                    <a href="#">
-                        <img className="funky-tabs__tab-image" src="/public/images/under15.png" />
-                        <div className="funky-tabs__tab-text">
-                            <span className="link">Kontantkort</span>
-                        </div>
-                    </a>
-                </li>
-            </ul>
-            <div className="funky-tabs__content" id="tab-panel-1" role="tabpanel" aria-labelledby="tab-1">
-                <section className="main-content">
-                    {underFemtenSubscriptionProps.map((subscriptionProps, i) =>
-                        <Subscription key={i} {...subscriptionProps} />)}
-                </section>
-            </div>
-            <div className="funky-tabs__content" id="tab-panel-2" role="tabpanel" aria-labelledby="tab-2">
-                <section className="main-content">
-                    {underTjueAatteSubscriptionProps.map((subscriptionProps, i) =>
-                        <Subscription key={i} {...subscriptionProps} />)}
-                </section>
-            </div>
-            <div className="funky-tabs__content funky-tabs__content--open" id="tab-panel-3" role="tabpanel" aria-labelledby="tab-3">
-                <section className="main-content">
-                    {forAlleSubscriptionProps.map((subscriptionProps, i) =>
-                        <Subscription key={i} {...subscriptionProps} />)}
-                </section>
-            </div>
-            <div className="funky-tabs__content" id="tab-panel-4" role="tabpanel" aria-labelledby="tab-4">
-                <section className="main-content">
-                    {kontantkortSubscriptionProps.map((subscriptionProps, i) =>
-                        <Subscription key={i} {...subscriptionProps} />)}
-                </section>
-            </div>
-        </div>
-    </div>;
+        this.state = {
+            selectedTabIndex: 1
+        };
 
-export default SubscriptionSamplePage;
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(tabIndex, e) {
+        var closingTab = document.getElementById("tab-"+this.state.selectedTabIndex);
+        var openingTab = e.currentTarget;
+
+        if(closingTab === openingTab) {
+            return;
+        }
+
+        this.closeTabPanel(closingTab);
+        this.openTabPanel(openingTab);
+
+        this.setState( {selectedTabIndex: tabIndex} );
+    }
+    closeTabPanel(closingTab) {
+        closingTab.classList.toggle("funky-tabs__tab--selected");
+        closingTab.setAttribute('aria-selected', false);
+        var closingPanel = document.getElementById(closingTab.getAttribute("aria-controls"));
+        closingPanel.classList.toggle("funky-tabs__panel--open");
+        closingPanel.setAttribute('aria-expanded', false);
+    }
+    openTabPanel(openingTab) {
+        openingTab.classList.toggle("funky-tabs__tab--selected");
+        openingTab.setAttribute('aria-selected', true);
+        var openingPanel = document.getElementById(openingTab.getAttribute("aria-controls"));
+        openingPanel.classList.toggle("funky-tabs__panel--open");
+        openingPanel.setAttribute('aria-expanded', true);
+    }
+    render() {
+        return(
+            <div>
+                <div className="center-align">
+                    <h1 className="heading heading--level-1">Subscription</h1>
+                </div>
+                <div className="funky-tabs">
+                    <ul className="funky-tabs__tabs" role="tablist">
+                        <li className="funky-tabs__tab funky-tabs__tab--selected" id="tab-1" onClick={this.handleClick.bind(this, 1)} aria-selected="true" role="tab" aria-controls="tab-panel-1">
+                            <a href="#">
+                                <img className="funky-tabs__tab-image" src="/public/images/under15.png" />
+                                <div className="funky-tabs__tab-text">
+                                    <span className="link">Under 15 år</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li className="funky-tabs__tab" id="tab-2" onClick={this.handleClick.bind(this, 2)} aria-selected="false" role="tab" aria-controls="tab-panel-2">
+                            <a href="#">
+                                <img className="funky-tabs__tab-image" src="/public/images/under28.png" />
+                                <div className="funky-tabs__tab-text">
+                                    <span className="link">Under 28 år</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li className="funky-tabs__tab" id="tab-3" onClick={this.handleClick.bind(this, 3)} aria-selected="false" role="tab" aria-controls="tab-panel-3">
+                            <a href="#">
+                                <img className="funky-tabs__tab-image" src="/public/images/for-alle.png" />
+                                <div className="funky-tabs__tab-text">
+                                    <span className="link"> For alle</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li className="funky-tabs__tab" id="tab-4" onClick={this.handleClick.bind(this, 4)} aria-selected="false" role="tab" aria-controls="tab-panel-4">
+                            <a href="#">
+                                <img className="funky-tabs__tab-image" src="/public/images/under15.png" />
+                                <div className="funky-tabs__tab-text">
+                                    <span className="link">Kontantkort</span>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                    <div className="funky-tabs__panel funky-tabs__panel--open" id="tab-panel-1" role="tabpanel" aria-labelledby="tab-1" aria-expanded="true">
+                        <section className="main-content">
+                            {underFemtenSubscriptionProps.map((subscriptionProps, i) =>
+                                <Subscription key={i} {...subscriptionProps} />)}
+                        </section>
+                    </div>
+                    <div className="funky-tabs__panel" id="tab-panel-2" role="tabpanel" aria-labelledby="tab-2" aria-expanded="false">
+                        <section className="main-content">
+                            {underTjueAatteSubscriptionProps.map((subscriptionProps, i) =>
+                                <Subscription key={i} {...subscriptionProps} />)}
+                        </section>
+                    </div>
+                    <div className="funky-tabs__panel" id="tab-panel-3" role="tabpanel" aria-labelledby="tab-3" aria-expanded="false">
+                        <section className="main-content">
+                            {forAlleSubscriptionProps.map((subscriptionProps, i) =>
+                                <Subscription key={i} {...subscriptionProps} />)}
+                        </section>
+                    </div>
+                    <div className="funky-tabs__panel" id="tab-panel-4" role="tabpanel" aria-labelledby="tab-4" aria-expanded="false">
+                        <section className="main-content">
+                            {kontantkortSubscriptionProps.map((subscriptionProps, i) =>
+                                <Subscription key={i} {...subscriptionProps} />)}
+                        </section>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
