23674f5f93f3c03e0fad73dd09f590bb63051fcc
Cathrine Holm <cathrine.holm@bouvet.no>
2017-09-21 14:34:20 +0200

    Renamed Table to FullWidthTable and added styling targeting html elements within full-width-table class

--- src/client/pages/InformationArticleSample4.jsx
+++ src/client/pages/InformationArticleSample4.jsx
@@ -12,44 +12,44 @@ const InformationArticleSample4 = () =>
         </Header>
         <div className="main-content">
             <section className="body-text-container">
-                <table className="table">
-                    <thead className="table__header">
-                    <tr className="table__row">
-                        <th className="table__cell">Column 1</th>
-                        <th className="table__cell">Column 2</th>
-                        <th className="table__cell">Column 3</th>
+                <table className="full-width-table">
+                    <thead className="full-width-table__header">
+                    <tr className="full-width-table__row">
+                        <th className="full-width-table__cell">Column 1</th>
+                        <th className="full-width-table__cell">Column 2</th>
+                        <th className="full-width-table__cell">Column 3</th>
                     </tr>
                     </thead>
-                    <tbody className="table__body">
-                    <tr className="table__row">
-                        <td className="table__cell">Nulla quis lorem ut libero malesuada feugiat</td>
-                        <td className="table__cell">Lorem</td>
-                        <td className="table__cell">Ipsum</td>
+                    <tbody className="full-width-table__body">
+                    <tr className="full-width-table__row">
+                        <td className="full-width-table__cell">Nulla quis lorem ut libero malesuada feugiat</td>
+                        <td className="full-width-table__cell">Lorem</td>
+                        <td className="full-width-table__cell">Ipsum</td>
                     </tr>
-                    <tr className="table__row">
-                        <td className="table__cell">Lorem ipsum dolor sit amet</td>
-                        <td className="table__cell">Consectetur</td>
-                        <td className="table__cell">Elit</td>
+                    <tr className="full-width-table__row">
+                        <td className="full-width-table__cell">Lorem ipsum dolor sit amet</td>
+                        <td className="full-width-table__cell">Consectetur</td>
+                        <td className="full-width-table__cell">Elit</td>
                     </tr>
-                    <tr className="table__row">
-                        <td className="table__cell">Donec rutrum congue leo</td>
-                        <td className="table__cell">Eget</td>
-                        <td className="table__cell">Malesuada</td>
+                    <tr className="full-width-table__row">
+                        <td className="full-width-table__cell">Donec rutrum congue leo</td>
+                        <td className="full-width-table__cell">Eget</td>
+                        <td className="full-width-table__cell">Malesuada</td>
                     </tr>
-                    <tr className="table__row">
-                        <td className="table__cell">Nulla quis lorem ut libero malesuada feugiat</td>
-                        <td className="table__cell">Lorem</td>
-                        <td className="table__cell">Ipsum</td>
+                    <tr className="full-width-table__row">
+                        <td className="full-width-table__cell">Nulla quis lorem ut libero malesuada feugiat</td>
+                        <td className="full-width-table__cell">Lorem</td>
+                        <td className="full-width-table__cell">Ipsum</td>
                     </tr>
-                    <tr className="table__row">
-                        <td className="table__cell">Lorem ipsum dolor sit amet</td>
-                        <td className="table__cell">Consectetur</td>
-                        <td className="table__cell">Elit</td>
+                    <tr className="full-width-table__row">
+                        <td className="full-width-table__cell">Lorem ipsum dolor sit amet</td>
+                        <td className="full-width-table__cell">Consectetur</td>
+                        <td className="full-width-table__cell">Elit</td>
                     </tr>
-                    <tr className="table__row">
-                        <td className="table__cell">Donec rutrum congue leo</td>
-                        <td className="table__cell">Eget</td>
-                        <td className="table__cell">Malesuada</td>
+                    <tr className="full-width-table__row">
+                        <td className="full-width-table__cell">Donec rutrum congue leo</td>
+                        <td className="full-width-table__cell">Eget</td>
+                        <td className="full-width-table__cell">Malesuada</td>
                     </tr>
                     </tbody>
                 </table>
