243d0000bd921a63286cee6438c272d95dcaa2db
Alex York <alexyork1985@gmail.com>
2017-10-02 13:31:21 +0200

    Added some more info to the Contributing page

--- src/client/pages/ContributingPage.jsx
+++ src/client/pages/ContributingPage.jsx
@@ -12,22 +12,54 @@ const ContributingPage = ({ colors }) =>
     <div>
         <Header pageTitle="Contributing">
             <p>We welcome all frontend developers - in collaboration with designers - to contribute to the styleguide.</p>
+            <p>Together, we can Make Telia's Frontend Great Again&trade;!</p>
         </Header>
-        <div className="container container--small">
-
+        <div className="container container--small container--no-margin">
             <Heading level={2} text="The repository" />
-            <p>The repository can be found on TeliaSoneraNorge's GitHub account: <a className="link" href="https://github.com/TeliaSoneraNorge/styleguide">TeliaSoneraNorge/styleguide</a></p>
-
+            <p>
+                The repository can be found on TeliaSoneraNorge's GitHub
+                account: <a className="link" href="https://github.com/TeliaSoneraNorge/styleguide">TeliaSoneraNorge/styleguide</a>.
+                Ask someone in #styleguide if you need access.
+            </p>
+        </div>
+        <div className="container container--small container--no-margin">
             <Heading level={2} text="Writing CSS" />
-            <p>For our own components, follow the <b>BEM</b> (Block, Element, Modifier) method for writing &amp; organising CSS. BEM helps keep our CSS logical and predictable. The naming convention follows this pattern:</p>
+            <p>
+                Use <b>BEM</b> (Block, Element, Modifier) for writing &amp; organising CSS.
+                BEM helps keep our CSS logical and predictable. The naming convention follows this pattern:
+            </p>
             <CodeSnippet code={codeSnippets.bem} language="css" />
             <ul>
                 <li><code className="language-css">.block</code> represents the higher level of an abstraction or component.</li>
-                <li><code className="language-css">.block__element</code> represents a descendent of <code className="language-css">.block</code> that helps form <code className="language-css">.block</code> as a whole.</li>
+                <li>
+                    <code className="language-css">.block__element</code> represents a descendent of <code className="language-css">.block</code> that
+                    helps form <code className="language-css">.block</code> as a whole. No more than 1 level of elements!
+                </li>
                 <li><code className="language-css">.block--modifier</code> represents a different state or version of <code className="language-css">.block</code>.</li>
             </ul>
             <p>For more information on BEM, <a className="link" href="http://csswizardry.com/2013/01/mindbemding-getting-your-head-round-bem-syntax/">please see this article</a>.</p>
-
+        </div>
+        <div className="container container--small container--no-margin">
+            <Heading level={2} text="Code style" />
+            <p>
+                Be <strong>consistent</strong>. Copy the code style, formatting, etc. that all other files use.
+                If we want to change a rule in the code styling, we should do so globally in a single commit.
+                Also, don't reformat lots of code at the same time as a feature commit.
+            </p>
+        </div>
+        <div className="container container--small">
+            <Heading level={2} text="Feature branches & Pull Requests" />
+            <p>
+                All changes must be done in feature branches with this naming convention:
+            </p>
+            <ul>
+                <li><code>feature/your-feature-name</code></li>
+                <li><code>bugfix/some-bugfix-name</code></li>
+                <li><code>refactor/some-refactoring-name</code></li>
+            </ul>
+            <p>
+                Once the feature is complete, create a Pull Request on GitHub and post a link to it on the #styleguide Slack channel.
+            </p>
         </div>
     </div>;
 
