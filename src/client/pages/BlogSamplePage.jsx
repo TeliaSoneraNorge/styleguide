af07a4ee87f9678d212e58b0d8fb99081057ce96
Cathrine Holm <cathrine.holm@bouvet.no>
2017-10-17 14:53:48 +0200

    Image component with caption

--- src/client/pages/BlogSamplePage.jsx
+++ src/client/pages/BlogSamplePage.jsx
@@ -32,6 +32,7 @@ const BlogSamplePage = () =>
             </div>
         </section>
         <section className="container container--small container--extra-margin-bottom">
+
             <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus.
                 Curabitur aliquet quam id dui posuere blandit.
                 <blockquote className="quote quote--inline-right">
@@ -53,15 +54,37 @@ const BlogSamplePage = () =>
                 tincidunt. Curabitur aliquet quam id dui posuere blandit. Donec rutrum congue leo eget malesuada.
                 Quisque velit nisi, pretium ut lacinia in, elementum id enim.</p>
 
+            <figure className="image image--inline-left">
+                <img className="image__image" src="/public/website-images/woman-holding-ipad.jpg" alt="Woman looking at tablet"/>
+                <figcaption className="image__caption">Woman looking at tablet</figcaption>
+            </figure>
+
             <p>Cras ultricies ligula sed magna dictum porta. Nulla porttitor accumsan tincidunt. Vivamus suscipit tortor
                 eget felis porttitor volutpat. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Curabitur
                 arcu erat, accumsan id imperdiet et, porttitor at sem.</p>
+
+            <p>Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla quis lorem ut libero malesuada feugiat.
+                Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Proin eget tortor risus.
+                Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.</p>
+
+            <figure className="image image--inline-right">
+                <img className="image__image" src="/public/website-images/woman-mobile.jpg" alt="Woman looking at mobile"/>
+                <figcaption className="image__caption">Woman looking at mobile</figcaption>
+            </figure>
 
             <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit
                 neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus. Curabitur
                 aliquet quam id dui posuere blandit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
                 posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+            <figure className="image">
+                <img className="image__image" src="/public/website-images/kids-ipad.jpg" alt="Kids playing with tablet"/>
+                <figcaption className="image__caption">
+                    Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Pellentesque in ipsum id orci porta dapibus. Curabitur aliquet quam id dui posuere blandit. Donec rutrum congue leo eget malesuada.
+                </figcaption>
+            </figure>
+
         </section>
         <section className="related-articles">
             <div className="container container--large container--no-padding container--extra-margin-bottom">
