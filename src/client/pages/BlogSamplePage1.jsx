21a7f293cd5cdd1a3d20d6b865a7feea075e1518
Cathrine Holm <cathrine.holm@bouvet.no>
2017-10-23 15:20:02 +0200

    FactBox component

--- src/client/pages/BlogSamplePage1.jsx
+++ src/client/pages/BlogSamplePage1.jsx
@@ -88,6 +88,21 @@ const BlogSamplePage = () =>
                 </figcaption>
             </figure>
 
+            <section className="fact-box">
+                <img className="fact-box__icon" src="/public/icons/ico_info.svg"/>
+                <h3 className="heading heading--level-3">Fact about Lorem Ipsum:</h3>
+                <hr className="fact-box__line"/>
+                <div className="fact-box__text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Pellentesque in ipsum id orci porta dapibus.</p>
+
+                    <p>Cras ultricies ligula sed magna dictum porta. Donec sollicitudin molestie malesuada. Quisque velit nisi, pretium ut lacinia in, elementum id enim.</p>
+
+                    <p>Donec sollicitudin molestie malesuada. Donec sollicitudin molestie malesuada. Donec rutrum congue leo eget malesuada.</p>
+
+                    <p>Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vivamus suscipit tortor eget felis porttitor volutpat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.</p>
+                </div>
+            </section>
+
         </section>
         <section className="related-articles">
             <div className="container container--large container--no-padding container--extra-margin-bottom">
