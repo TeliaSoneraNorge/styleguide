b1c9beda335105594ef33508692ed69ab2c0bc74
Cathrine Holm <cathrine.holm@bouvet.no>
2017-10-26 16:42:43 +0200

    Removed heading level h4 and adjusted font-sizes in heading-levels

--- src/components/atoms/Heading/Heading.jsx
+++ src/components/atoms/Heading/Heading.jsx
@@ -6,7 +6,7 @@ import React from 'react';
  * Work remaining: a designer should take a look at these heading levels and adjust fonts, sizes & colors.
  *
  * The Heading component should be used for page titles, sub-titles, etc.
- * Currently, only h1, h2, h3 and h4 headings have been defined.
+ * Currently, only h1, h2 and h3 headings have been defined.
  */
 const Heading = ({ level, text }) => {
     const TagName = `h${level}`;
