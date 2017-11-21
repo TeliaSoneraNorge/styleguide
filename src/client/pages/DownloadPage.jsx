c2b1e4835df1b49dc1f49775caa16b4cbf4f0382
Alex York <alexyork1985@gmail.com>
2017-11-21 16:13:58 +0100

    Alert on download page

--- src/client/pages/DownloadPage.jsx
+++ src/client/pages/DownloadPage.jsx
@@ -3,14 +3,24 @@ import React from 'react';
 import CodeSnippet from '../components/CodeSnippet';
 import Heading from '../../components/atoms/Heading/Heading';
 import Header from '../../components/molecules/Header/Header';
+import Alert from '../../components/molecules/Alert/Alert';
 
 const codeSnippets = {
     stylesheetHtmlTag: '<link href="/path/to/your/styleguide/telia-styleguide.css" rel="stylesheet" type="text/css" />'
 };
 
+const warningMessageHtml =
+    `<p>
+        The styleguide is <strong>NOT YET READY TO BE USED!</strong> If you go ahead and use it anyway,
+        you use it at your own risk. Can you at least drop us a line over at #styleguide and let us know
+        what you're upto first? Thanks!
+    </p>`;
+
 const DownloadPage = ({ colors }) =>
     <div>
         <Header pageTitle="Download Styleguide" />
+
+        <Alert kind="negative" headingText="WAIT! Read this first :-)" bodyHtml={warningMessageHtml} />
 
         <div className="container container--small container--extra-margin-bottom">
             <p>To use the Telia Styleguide in your project, follow these steps:</p>
