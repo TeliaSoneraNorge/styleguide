f8092587a84e81f67e130be33bbf5c3ac5b10c1e
Cathrine Holm <cathrine.holm@bouvet.no>
2017-12-01 15:55:50 +0100

    Changed the content of Content Rating

--- src/components/organisms/ContentRating/ContentRating.jsx
+++ src/components/organisms/ContentRating/ContentRating.jsx
@@ -4,9 +4,10 @@ export default class ContentFeedback extends React.Component {
     render() {
         return (
             <section className="container container--small content-rating">
-                <h3 className="heading heading--level-3 content-rating__heading">Var denne siden nyttig?</h3>
                 {this.props.feedbackState === 'initial' ?
                     <div>
+                        <h3 className="heading heading--level-3 content-rating__heading">Var denne siden nyttig?</h3>
+
                         <button className="button button--small" onClick={this.props.positiveFeedbackClick}>JA</button>
                         <button className="button button--small" onClick={this.props.negativeFeedbackClick}>NEI</button>
                     </div> : null
@@ -14,21 +15,27 @@ export default class ContentFeedback extends React.Component {
 
                 {this.props.feedbackState === 'receipt-positive' ?
                     <div>
+                        <h3 className="heading heading--level-3 content-rating__heading">Var denne siden nyttig?</h3>
+
                         <img className="content-rating__icon" src="/public/icons/ico_heart.svg"/>
-                        <p className="content-rating__text">Så bra!<br/>Takk for din tilbakemelding</p>
+                        <p className="content-rating__text">Så bra!<br/>Takk for din tilbakemelding.</p>
                     </div> : null
                 }
 
                 {this.props.feedbackState === 'negative-feedback' ?
                     <div>
-                        <textarea ref={(textArea) => { this.textAreaRef = textArea; }} className="text-area" placeholder="Hva kunne vært bedre?"></textarea>
-                        <p className="paragraph--small-text">Tilbakemeldinger vil ikke bli besvart, men brukes til å forbedre nettsidene våre.</p>
+                        <h3 className="heading heading--level-3 content-rating__heading">Å, nei!</h3>
+                        <textarea ref={(textArea) => { this.textAreaRef = textArea; }}
+                                  className="text-area text-area--margin-bottom"
+                                  placeholder="Hjelp oss, er du grei - hva kunne vi gjort bedre?"></textarea>
                         <button className="button" onClick={() => this.props.negativeFeedbackSubmitText(this.textAreaRef.value)}>Send</button>
                     </div> : null
                 }
 
                 {this.props.feedbackState === 'receipt-negative' ?
-                    <p>Auda, det var dumt...<br/>Takk for at du hjelper oss til å bli bedre!</p> : null
+                    <div>
+                        <h3 className="heading heading--level-3 content-rating__heading">Takk for at du hjelper oss!</h3>
+                    </div> : null
                 }
             </section>
         );
