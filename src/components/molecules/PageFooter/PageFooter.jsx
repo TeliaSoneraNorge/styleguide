ce94718744dafa73668c34f3eea3642cb5e9bdf8
Cathrine Holm <cathrine.holm@bouvet.no>
2017-12-05 10:57:17 +0100

    Removed pebbles in PageFooter

--- src/components/molecules/PageFooter/PageFooter.jsx
+++ src/components/molecules/PageFooter/PageFooter.jsx
@@ -4,8 +4,6 @@ import React from 'react';
 /**
  * Status: *finished*.
  *
- * Future work: improved animations.
- *
  * The PageFooter is the main footer on the page, and should be included only once.
  * It contains a small number of important, side-wide links.
  */
@@ -21,9 +19,6 @@ const PageFooter = ({ links, specialLink }) =>
                 <nav className="page-footer__special-nav">
                     <a className="page-footer__special-link page-footer__link" href={specialLink.url}>{specialLink.text}</a>
                 </nav>
-                {_.map([0, 1, 2, 3, 4], (i) =>
-                    <div key={i} className={`page-footer__pebble page-footer__pebble--${i}`}></div>
-                )}
             </div>
         </div>
     </footer>;
