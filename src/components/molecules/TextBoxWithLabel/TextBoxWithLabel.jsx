635296e282b970d0059992bb16a5b90e64a0200b
Cathrine Holm <cathrine.holm@bouvet.no>
2017-12-13 10:48:09 +0100

    Added Label component to TextBox component

--- src/components/molecules/TextBoxWithLabel/TextBoxWithLabel.jsx
+++ src/components/molecules/TextBoxWithLabel/TextBoxWithLabel.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 
+import Label from '../../atoms/Label/Label';
+
 /**
  * Status: *in progress*.
  *
  * Be sure to set the correct type when using this component as it helps
  * the user to give correct input.
  *
- * TODO: wrap Label component around the input element
- *
 **/
-export default class TextBox extends React.Component {
-    constructor(props){
-        super(props);
-    }
-    render() {
-        return (
-            <input class="textbox__input" type={this.props.type} placeholder={this.props.placeholder} id={this.props.id} />
-        )
-    }
-}
+const TextBoxWithLabel = ({ label, type, placeholder}) =>
+    <Label className="textbox-with-label" mode="text-on-top">
+        <span className="textbox-with-label__label-text">{label}</span>
+        <input className="textbox-with-label__input" type={type} placeholder={placeholder} />
+    </Label>;
+
+export default TextBoxWithLabel;
