331d16e6746024a1cbd806a89535ba4d6a96cc51
Alex York <alexyork1985@gmail.com>
2018-01-08 10:53:59 +0100

    WIP

--- src/components/molecules/ProductPromoBox/ProductPromoBox.jsx
+++ src/components/molecules/ProductPromoBox/ProductPromoBox.jsx
@@ -9,6 +9,7 @@ function classNames(isShowingFeature) {
     }
     return classNames.join(' ');
 }
+
 /**
  * Status: *in progress*.
  *
@@ -18,12 +19,16 @@ function classNames(isShowingFeature) {
 const ProductPromoBox = ({ id, productName, productColor, imagePath, description, monthlyPrice, totalPrice, buttonText, isShowingFeature }) => {
     const imageAltText = `${productName}`;
     return (
-        <Box color="grey" id={id} canExpand={false} isShowingFeature={isShowingFeature}>
+        <Box id={id} color="grey" canExpand={false} isShowingFeature={isShowingFeature}>
             <section className={classNames(isShowingFeature)}>
-                <img className="product-promo-box__image" src={imagePath} alt={imageAltText}/>
-                <h2 className="heading heading--level-2 product-promo-box__product-name">{productName}</h2>
-                <div className="product-promo-box__text">
+                <div className="product-promo-box__image-section">
+                    <img className="product-promo-box__image" src={imagePath} alt={imageAltText}/>
+                </div>
+                <div className="product-promo-box__content-section">
+                    <h2 className="heading heading--level-2 product-promo-box__product-name">{productName}</h2>
                     <div className="product-promo-box__description">{description}</div>
+                </div>
+                <div className="product-promo-box__price-section">
                     <div className="product-promo-box__monthly-price">
                         <span className="product-promo-box__monthly-price-number">{monthlyPrice}</span>/mnd
                     </div>
@@ -31,7 +36,8 @@ const ProductPromoBox = ({ id, productName, productColor, imagePath, description
                     <button className="button button--primary">{buttonText}</button>
                 </div>
             </section>
-        </Box>);
+        </Box>
+    );
 };
 
 export default ProductPromoBox;
