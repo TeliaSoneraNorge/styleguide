97d60c4876ebbabcf75350cb4ee550ca1fded8a6
Cathrine Holm <cathrholm@gmail.com>
2018-01-21 21:36:56 +0100

    ProductPromoBox with extra line for smalltext

--- src/components/molecules/ProductPromoBox/ProductPromoBox.jsx
+++ src/components/molecules/ProductPromoBox/ProductPromoBox.jsx
@@ -21,7 +21,7 @@ function classNames(isShowingFeature) {
  * If multiple of these promo boxes will be used on a page, a regular button can be used
  * instead of the primary button.
  */
-const ProductPromoBox = ({ id, productName, productColor, imagePath, description, price, priceSmallprint, buttonText, isShowingFeature }) =>
+const ProductPromoBox = ({ id, productName, productColor, imagePath, description, price, priceSmallprint1, priceSmallprint2, buttonText, isShowingFeature }) =>
     <Box id={id} className={classNames(isShowingFeature)} color="grey" canExpand={false} isShowingFeature={isShowingFeature}>
         <div className="product-promo-box__image-container">
             <img className="product-promo-box__image" src={imagePath} alt={productName}/>
@@ -34,7 +34,8 @@ const ProductPromoBox = ({ id, productName, productColor, imagePath, description
             <div className="product-promo-box__price-and-action">
                 <div className="product-promo-box__price-info">
                     <span className="product-promo-box__price">{price},-</span>
-                    <span className="product-promo-box__price-smallprint">{priceSmallprint}</span>
+                    <span className="product-promo-box__price-smallprint">{priceSmallprint1}</span>
+                    <div className="product-promo-box__price-smallprint">{priceSmallprint2}</div>
                 </div>
                 <div className="product-promo-box__button-container">
                     <button className="button button--primary">{buttonText}</button>
