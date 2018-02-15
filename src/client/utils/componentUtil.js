4f4903280c79f92e1bac303673f2b4886c1c7816
Marek Magdziak <marek.magdziak@hotmail.cz>
2018-02-15 12:08:50 +0100

    fact-box with image (#76)

--- src/client/utils/componentUtil.js
+++ src/client/utils/componentUtil.js
@@ -56,6 +56,7 @@ export function isFullWidthComponent(componentName) {
         'PageFooter',
         'RelatedArticles',
         'UserDashboard',
+        'FactBox',
 
         // Organisms
         'FocusSubscription'
