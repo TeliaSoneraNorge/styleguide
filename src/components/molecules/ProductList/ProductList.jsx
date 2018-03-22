378648d424fe5813282d782674e422e56fda2b71
Magnus Indregard <magnus.indregard@netcom-gsm.no>
2018-03-22 16:02:15 +0100

    ProductList: Refactored HTML to allow links
    Also added focus styling (although improvement is needed)

--- src/components/molecules/ProductList/ProductList.jsx
+++ src/components/molecules/ProductList/ProductList.jsx
@@ -6,19 +6,34 @@ import React from "react";
  * 
  * Used for showing lists of products like subscriptions and services, in a less "in your face" manner than the Subscription component.
  */
-const ProductList = ({ items }) => (
+const ProductList = ({ items }) =>
     <div className="product-list">
-        {items.map((item, index) =>
-            <article key={index} className="product-list__item">
-                <img className="product-list__item-icon" src={`/public/icons/${item.icon}.svg`} />
+        {items.map((item, index) => {
+            const icon = <img className="product-list__item-icon" src={`/public/icons/${item.icon}.svg`} />;
+            const content = (
                 <div className="product-list__item-content">
-                    <div className="product-list__item-heading">{item.title}</div>
+                    <div className="product-list__item-heading">{item.title}</div> 
                     {item.description ?
                         <div className="product-list__item-description">{item.description}</div> : null}
                 </div>
-            </article>
-        )}
-    </div>
-);
+            );
+            
+            if (item.href) {
+                return (
+                    <a href={item.href} key={index} className="product-list__item product-list__item--link">
+                        {icon}
+                        {content}
+                    </a>
+                );
+            }
+
+            return (
+                <article key={index} className="product-list__item">
+                    {icon}
+                    {content}
+                </article>
+            );
+        })}
+    </div>;
 
 export default ProductList;
