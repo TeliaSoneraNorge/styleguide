ea6b21938c8ba5753ac50112d00f8e29a1fd202a
Sven Hoferer <sven.hoferer@smartrefill.se>
2018-03-26 14:33:19 +0200

    Link with icon

--- src/components/atoms/Link/Link.jsx
+++ src/components/atoms/Link/Link.jsx
@@ -6,8 +6,8 @@ import React from 'react';
  * The target attribute is set to '_self' which is the default value if the attribute is not specified.
  * If you rather prefer to open the linked document in a new browser window or tab, you can set the target to '_blank' instead.
  */
-const Link = ({ text, href }) =>
-    <a className="link" href={href} target="_self">
+const Link = ({ text, href, className }) =>
+    <a className={className} href={href} target="_self">
         {text}
     </a>;
 
