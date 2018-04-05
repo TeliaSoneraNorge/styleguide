e1f3fd286087d686122541b237f03e5a074eee40
Magnus Indregard <magnus.indregard@netcom-gsm.no>
2018-04-05 16:30:53 +0200

    Added link variation
    Backlink - Move to previous step or page

--- src/components/atoms/Link/Link.jsx
+++ src/components/atoms/Link/Link.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
+function classNames(type) {
+    const classNames = ['link'];
+   
+    if (type){
+        var className = 'link--' + type;
+        classNames.push(className);
+    }
+
+    return classNames.join(' ');
+}
+
 /**
  * Status: *finished*.
  *
  * The target attribute is set to '_self' which is the default value if the attribute is not specified.
  * If you rather prefer to open the linked document in a new browser window or tab, you can set the target to '_blank' instead.
  */
-const Link = ({ text, href }) =>
-    <a className="link" href={href} target="_self">
+const Link = ({ text, href, type }) =>
+    <a className={classNames(type)} href={href} target="_self">
         {text}
     </a>;
 
