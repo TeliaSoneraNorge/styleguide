f44009eb83c1ba2b653927a59af6c817ea4ac24d
Richard Vašek <richard.vasek@tieto.com>
2018-06-06 15:45:09 +0200

    TelephoneNumberLink component

---
+++ src/client/examples/atoms/TelephoneNumberLink/Default.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { TelephoneNumberLink } from '@telia/styleguide';
+
+const DefaultTelephoneNumberLink = () => (
+    <TelephoneNumberLink>+47 924 05 050</TelephoneNumberLink>
+);
+
+export default DefaultTelephoneNumberLink;
