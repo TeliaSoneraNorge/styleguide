628633d0e50dc5360f43b57dc9c493d575df1731
Richard Vašek <richard.vasek@tieto.com>
2018-06-06 16:27:02 +0200

    InputError component
    export

---
+++ src/client/examples/atoms/InputError/Default.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { InputError } from '@telia/styleguide';
+
+const DefaultInputError = () => (
+    <InputError>Some error message</InputError>
+);
+
+export default DefaultInputError;
