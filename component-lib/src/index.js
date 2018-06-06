2d27b4994557525782ce73e81c1c9ab4c8263ed5
Richard Vašek <richard.vasek@tieto.com>
2018-06-06 16:27:02 +0200

    InputError component
    export

--- component-lib/src/index.js
+++ component-lib/src/index.js
@@ -2,6 +2,7 @@
 export { default as Box } from './atoms/Box/Box';
 export { default as Button } from './atoms/Button/Button';
 export { default as Heading } from './atoms/Heading/Heading';
+export { default as InputError } from './atoms/InputError/InputError';
 export { default as Label } from './atoms/Label/Label';
 export { default as Link } from './atoms/Link/Link';
 export { default as ProgressBar } from './atoms/ProgressBar/ProgressBar';
@@ -57,6 +58,7 @@ export default [
     'src/atoms/Box/Box.jsx',
     'src/atoms/Button/Button.jsx',
     'src/atoms/Heading/Heading.jsx',
+    'src/atoms/InputError/InputError.jsx',
     'src/atoms/Label/Label.jsx',
     'src/atoms/Link/Link.jsx',
     'src/atoms/ProgressBar/ProgressBar.jsx',
