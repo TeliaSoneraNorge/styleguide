7261f0034361d5df4312beed9143569cd6b36813
Richard Vašek <vasekrichard@seznam.cz>
2018-06-07 11:14:00 +0200

    ShadowBox component (#152)

--- component-lib/src/index.js
+++ component-lib/src/index.js
@@ -13,6 +13,7 @@ export { default as NumberBoxes } from './atoms/NumberBoxes/NumberBoxes';
 export { default as PagePebbles } from './atoms/PagePebbles/PagePebbles';
 export { default as Paragraph } from './atoms/Paragraph/Paragraph';
 export { default as ProgressBar } from './atoms/ProgressBar/ProgressBar';
+export { default as ShadowBox } from './atoms/ShadowBox/ShadowBox';
 export { default as Spinner } from './atoms/Spinner/Spinner';
 export { default as TelephoneNumberLink } from './atoms/TelephoneNumberLink/TelephoneNumberLink';
 export { default as ToggleButton } from './atoms/ToggleButton/ToggleButton';
@@ -79,6 +80,7 @@ export default [
     'src/atoms/PagePebbles/PagePebbles.jsx',
     'src/atoms/Paragraph/Paragraph.jsx',
     'src/atoms/ProgressBar/ProgressBar.jsx',
+    'src/atoms/ShadowBox/ShadowBox.jsx',
     'src/atoms/Spinner/Spinner.jsx',
     'src/atoms/TelephoneNumberLink/TelephoneNumberLink.jsx',
     'src/atoms/ToggleButton/ToggleButton.jsx',
---
+++ src/client/examples/atoms/ShadowBox/Default.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ShadowBox } from '@telia/styleguide';
+
+const DefaultShadowBox = () => (
+    <ShadowBox>This is a box with some content inside</ShadowBox>
+);
+
+export default DefaultShadowBox;
