4942ac96c6898521a3b9c91bf56c1f52ec9b61a5
Richard Vašek <richard.vasek@tieto.com>
2018-06-07 17:02:24 +0200

    DescriptionList component

---
+++ src/client/examples/atoms/DescriptionList/[02]Wrap-by-Two.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { DescriptionList } from '@telia/styleguide';
+
+const { Dt, Dd } = DescriptionList;
+
+const WrapByTwo = () => (
+    <DescriptionList wrapByTwo>
+        <Dt>First term:</Dt>
+        <Dd>First description</Dd>
+        <Dt>Second term:</Dt>
+        <Dd>Second description</Dd>
+        <Dt>Third term:</Dt>
+        <Dd>Third description</Dd>
+        <Dt>Fourth term:</Dt>
+        <Dd>Fourth description</Dd>
+    </DescriptionList>
+);
+
+export default WrapByTwo;
