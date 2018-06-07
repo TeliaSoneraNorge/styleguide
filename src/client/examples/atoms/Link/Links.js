6f5574772c7a599f8bdacf03a440ca0a1adc2fef
Alex York <alexyork1985@gmail.com>
2018-06-07 12:26:03 +0200

    Links now hide their internal class names

--- src/client/examples/atoms/Link/Links.js
+++ src/client/examples/atoms/Link/Links.js
@@ -4,8 +4,8 @@ import { Link } from '@telia/styleguide';
 const Links = () => (
     <div>
         <Link href="#" text="Standard text link" /><br />
-        <Link href="#" text="Link with icon after" className="link link--with-icon link--forward-icon link--icon-after" /><br />
-        <Link href="#" text="Link with icon before" className="link link--with-icon link--back-icon link--icon-before" />
+        <Link href="#" text="Link with icon after" icon="forward-icon" iconPosition="after" /><br />
+        <Link href="#" text="Link with icon before" icon="back-icon" iconPosition="before" />
     </div>
 );
 
