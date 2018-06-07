0fa736dd6e78820c34dd9c8eb58b7ab06edce199
Richard Vašek <richard.vasek@tieto.com>
2018-06-07 10:11:10 +0200

    PagePebbles component

--- component-lib/src/index.js
+++ component-lib/src/index.js
@@ -9,6 +9,7 @@ export { default as IconLink } from './atoms/IconLink/IconLink';
 export { default as InputError } from './atoms/InputError/InputError';
 export { default as Label } from './atoms/Label/Label';
 export { default as Link } from './atoms/Link/Link';
+export { default as PagePebbles } from './atoms/PagePebbles/PagePebbles';
 export { default as ProgressBar } from './atoms/ProgressBar/ProgressBar';
 export { default as Spinner } from './atoms/Spinner/Spinner';
 export { default as TelephoneNumberLink } from './atoms/TelephoneNumberLink/TelephoneNumberLink';
@@ -72,6 +73,7 @@ export default [
     'src/atoms/InputError/InputError.jsx',
     'src/atoms/Label/Label.jsx',
     'src/atoms/Link/Link.jsx',
+    'src/atoms/PagePebbles/PagePebbles.jsx',
     'src/atoms/ProgressBar/ProgressBar.jsx',
     'src/atoms/Spinner/Spinner.jsx',
     'src/atoms/TelephoneNumberLink/TelephoneNumberLink.jsx',
