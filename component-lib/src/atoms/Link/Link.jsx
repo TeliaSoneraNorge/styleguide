5a7cf571b5185ccef0c52bfbffd6d74d8ddad7b7
Alex York <alexyork1985@gmail.com>
2018-06-07 13:51:08 +0200

    Links now hide their internal class names (#158)

--- component-lib/src/atoms/Link/Link.jsx
+++ component-lib/src/atoms/Link/Link.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import classnames from 'classnames';
 
 /**
  * Status: *finished*.
  *
  * The target attribute is set to '_self' which is the default value if the attribute is not specified.
  * If you rather prefer to open the linked document in a new browser window or tab, you can set the target to '_blank' instead.
+ * 
+ * Our links have _two visual means_ of being identified: an underline and bolder font.
  */
-const Link = ({ text, href, className }) =>
-    <a className={className} href={href} target="_self">
+const Link = ({ className, text, href, icon, iconPosition }) =>
+    <a
+        className={classnames('link', {
+            [`link--with-icon link--${icon}`]: icon,
+            [`link--icon-${iconPosition}`]: icon && iconPosition,
+            [className]: className
+        })}
+        href={href}
+        target="_self">
         {text}
     </a>;
 
