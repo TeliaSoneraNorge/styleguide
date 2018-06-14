2a226369f6636f2909523076a6e6c356688267ef
Alex York <alexyork1985@gmail.com>
2018-06-14 11:22:08 +0200

    Remove the last .html example

---
+++ src/client/examples/molecules/Header/[05]WithContentOverlap.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Header } from '@telia/styleguide';
+
+const WithContentOverlap = () => (
+    <div>
+        <Header
+            iconUrl="/public/icons/ico_favourites.svg"
+            pageTitle="Page Title"
+            withContentOverlap={true}>
+            Preamble; an introductory and <a className="link">expressionary</a> statement in a document that explains the document's purpose and
+            underlying philosophy.
+        </Header>
+        <div class="focus-box">
+            <div class="focus-box__content-outer">
+                <div class="container container--small container--no-margin">
+                    <p>If you add the <code>--with-content-overlap</code> CSS class then the next piece of content, such as this Focus Box, will overlap the header.</p>
+                    <p>When using the header with <code>--with-content-overlap</code> it is recommended to use a component like <code>Focus Box</code> as the next component on the page.</p>
+                    <p>This is recommended to keep the overlapping content tall enough to cover the mask.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default WithContentOverlap;
