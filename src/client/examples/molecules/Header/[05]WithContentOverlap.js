b08fdae9be04228ed8fee05ff7f821bee1f53297
Richard Vašek <richard.vasek@tieto.com>
2018-06-14 12:24:22 +0200

    Fix className

--- src/client/examples/molecules/Header/[05]WithContentOverlap.js
+++ src/client/examples/molecules/Header/[05]WithContentOverlap.js
@@ -10,9 +10,9 @@ const WithContentOverlap = () => (
             Preamble; an introductory and <a className="link">expressionary</a> statement in a document that explains the document's purpose and
             underlying philosophy.
         </Header>
-        <div class="focus-box">
-            <div class="focus-box__content-outer">
-                <div class="container container--small container--no-margin">
+        <div className="focus-box">
+            <div className="focus-box__content-outer">
+                <div className="container container--small container--no-margin">
                     <p>If you add the <code>--with-content-overlap</code> CSS class then the next piece of content, such as this Focus Box, will overlap the header.</p>
                     <p>When using the header with <code>--with-content-overlap</code> it is recommended to use a component like <code>Focus Box</code> as the next component on the page.</p>
                     <p>This is recommended to keep the overlapping content tall enough to cover the mask.</p>
