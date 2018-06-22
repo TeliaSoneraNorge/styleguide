0e5cf26b2b909abed78a064e9e38d6422820d240
Cathrine Holm <cathrine.holm@bouvet.no>
2018-06-22 16:35:51 +0200

    RadioButtonList with rich content

--- component-lib/src/molecules/RadioButtonList/RadioButtonList.jsx
+++ component-lib/src/molecules/RadioButtonList/RadioButtonList.jsx
@@ -3,22 +3,25 @@ import classnames from 'classnames';
 
 import RadioButtonWithLabel from './RadioButtonWithLabel';
 
-const getClassName = (type) =>
-    classnames('radio-button-list', { 'radio-button-list--horizontal' : type === 'horizontal' });
+const getClassName = (type, hasRichContent) =>
+    classnames('radio-button-list', { 'radio-button-list--horizontal' : type === 'horizontal' , 'radio-button-list--rich-content' : hasRichContent });
 
 /**
  * Status: *finished*.
  *
  */
-const RadioButtonList = ({ list, selectedIndex, name, type }) =>
-    <div className={getClassName(type)}>
-        {list.map((radio, index) =>
-            <RadioButtonWithLabel
-                checked={index === selectedIndex}
-                key={index}
-                label={radio.label}
-                name={name}
-                value={radio.value} />)}
+const RadioButtonList = ({ list, selectedIndex, name, type, hasRichContent, children }) =>
+    <div className={getClassName(type, hasRichContent)}>
+        {hasRichContent ?
+            children
+            : list.map((radio, index) =>
+                <RadioButtonWithLabel
+                    checked={index === selectedIndex}
+                    key={index}
+                    label={radio.label}
+                    name={name}
+                    value={radio.value}/>)
+        }
     </div>;
 
 export default RadioButtonList;
