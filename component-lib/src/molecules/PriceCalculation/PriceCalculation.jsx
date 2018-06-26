a627938b62c6b07e1848825bc3ac8699442fcb3f
Cathrine Holm <cathrine.holm@bouvet.no>
2018-06-26 12:54:47 +0200

    Changed PriceCalcluation html to be a table

--- component-lib/src/molecules/PriceCalculation/PriceCalculation.jsx
+++ component-lib/src/molecules/PriceCalculation/PriceCalculation.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
-import HorizontalRule from '../../atoms/HorizontalRule/HorizontalRule';
 
 const PriceCalculation = ({ productListWithPrice, totalTextWithPrice }) =>
-    <div className="price-calculation">
-        {productListWithPrice.map((product, index) =>
-            <div className="price-calculation__item" key={index}>
-                <div className="price-calculation__item-title">{product.title}
-                    <span className="price-calculation__item-price">{product.price}</span>
-                </div>
-                {product.subtitle &&
-                    <div className="price-calculation__item-subtitle">{product.subtitle}</div>
-                }
-            </div>)
-        }
-        <HorizontalRule/>
-        <div className="price-calculation__item price-calculation__total">
-            <div className="price-calculation__item-title">{totalTextWithPrice.title}
-                <span className="price-calculation__item-price">{totalTextWithPrice.price}</span>
-            </div>
-            {totalTextWithPrice.subtitle &&
-                <div className="price-calculation__item-subtitle">{totalTextWithPrice.subtitle}</div>
-            }
-        </div>
-    </div>;
+    <table className="price-calculation">
+        <tbody className="price-calculation__items">
+            {productListWithPrice.map((product, index) =>
+                <tr className="price-calculation__item" key={index}>
+                    <td className="price-calculation__item-cell">
+                        <div className="price-calculation__item-title">{product.title}</div>
+                        {product.subtitle &&
+                            <div className="price-calculation__item-subtitle">{product.subtitle}</div>
+                        }
+                    </td>
+                    <td className="price-calculation__item-cell price-calculation__item-price">{product.price}</td>
+                </tr>
+            )}
+        </tbody>
+        <tfoot>
+            <tr className="price-calculation__item price-calculation__total">
+                <td className="price-calculation__item-cell">
+                    <div className="price-calculation__item-title">{totalTextWithPrice.title}</div>
+                    {totalTextWithPrice.subtitle &&
+                        <div className="price-calculation__item-subtitle">{totalTextWithPrice.subtitle}</div>
+                    }
+                </td>
+                <td className="price-calculation__item-cell price-calculation__item-price">{totalTextWithPrice.price}</td>
+            </tr>
+        </tfoot>
+    </table>;
 
 export default PriceCalculation;
