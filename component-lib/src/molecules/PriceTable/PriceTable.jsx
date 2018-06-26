53210aa3490f43cb6531e077279b23cd7e7ff133
Alex York <alexyork1985@gmail.com>
2018-06-26 15:10:27 +0200

    Rename

---
+++ component-lib/src/molecules/PriceTable/PriceTable.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const PriceTable = ({ productListWithPrice, totalTextWithPrice }) =>
+    <table className="price-table">
+        <tbody className="price-table__items">
+            {productListWithPrice.map((product, index) =>
+                <tr className="price-table__item" key={index}>
+                    <td className="price-table__item-cell">
+                        <div className="price-table__item-title">{product.title}</div>
+                        {product.subtitle &&
+                            <div className="price-table__item-subtitle">{product.subtitle}</div>
+                        }
+                    </td>
+                    <td className="price-table__item-cell price-table__item-price">{product.price}</td>
+                </tr>
+            )}
+        </tbody>
+        <tfoot>
+            <tr className="price-table__item price-table__total">
+                <td className="price-table__item-cell">
+                    <div className="price-table__item-title">{totalTextWithPrice.title}</div>
+                    {totalTextWithPrice.subtitle &&
+                        <div className="price-table__item-subtitle">{totalTextWithPrice.subtitle}</div>
+                    }
+                </td>
+                <td className="price-table__item-cell price-table__item-price">{totalTextWithPrice.price}</td>
+            </tr>
+        </tfoot>
+    </table>;
+
+export default PriceTable;
