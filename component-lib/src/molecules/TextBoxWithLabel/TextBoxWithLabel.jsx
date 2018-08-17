650f22386acb386dbc9a8f91911096cc18fbfd68
Markus Kullberg <markus.kullberg@teliacompany.com>
2018-08-17 10:10:59 +0300

    Added ...rest props to input field

--- component-lib/src/molecules/TextBoxWithLabel/TextBoxWithLabel.jsx
+++ component-lib/src/molecules/TextBoxWithLabel/TextBoxWithLabel.jsx
@@ -10,17 +10,17 @@ import SvgIcon from '../../atoms/SvgIcon/SvgIcon';
  * the user to give correct input.
  *
 **/
-const TextBoxWithLabel = ({ labelText, type, placeholder, errorMessage, disabled, withIcon, iconName }) =>
+const TextBoxWithLabel = ({ labelText, type, placeholder, errorMessage, disabled, withIcon, iconName, ...rest }) =>
     <Label className={classnames('textbox-with-label', { 'textbox-with-label--with-error': errorMessage, 'textbox-with-label--with-icon': withIcon })} isUsingGrayText={true}>
         <span className="textbox-with-label__label-text">{labelText}</span>
         {withIcon
             ? <div className="textbox-with-label__input-icon-wrapper">
-                <input className="textbox-with-label__input" type={type} placeholder={placeholder} disabled={disabled} />
+                <input className="textbox-with-label__input" type={type} placeholder={placeholder} disabled={disabled} {...rest} />
                 <button className="textbox-with-label__button">
                     <SvgIcon className="textbox-with-label__icon" iconName={iconName} color="black" aria-label={labelText} />
                 </button>
             </div>
-            : <input className="textbox-with-label__input" type={type} placeholder={placeholder} disabled={disabled} />
+            : <input className="textbox-with-label__input" type={type} placeholder={placeholder} disabled={disabled} {...rest} />
         }
         {errorMessage &&
             <span className="input-error">{errorMessage}</span>}
