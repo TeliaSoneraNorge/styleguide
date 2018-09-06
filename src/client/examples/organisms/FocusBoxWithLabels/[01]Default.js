53b0c1fd4ce22d75abb3757c4d184c2919fe7176
Joakim Kristiansen <joakim.kristiansen@telia.no>
2018-09-06 16:47:35 +0200

    Added organism FocusBoxWithLabels

---
+++ src/client/examples/organisms/FocusBoxWithLabels/[01]Default.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FocusBoxWithLabels } from '@telia/styleguide';
+
+const content = {
+    upperLeft: {
+        title: 'Ordrenummer:',
+        description: '24320656'
+    },
+    upperRight: {
+        title: 'Bestillingsdato:',
+        description: '17.01.2018'
+    },
+    bottomLeft: {
+        title: 'Ditt mobilnummer',
+        description: '915 43 325'
+    },
+    bottomRight: {
+        title: 'Levering:',
+        description: 'Posten'
+    }
+};
+
+const Default = () => (
+    <FocusBoxWithLabels content={content} />
+);
+
+export default Default;
