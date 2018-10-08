c6a888e34d3250b32fb5964a0b6d61664196df5b
Joakim Kristiansen <joakim.kristiansen@telia.no>
2018-10-08 16:36:45 +0200

    Updated StepIndicator atom with new design

--- component-lib/src/atoms/StepIndicator/StepIndicator.jsx
+++ component-lib/src/atoms/StepIndicator/StepIndicator.jsx
@@ -7,26 +7,58 @@ import SvgIcon from '../SvgIcon/SvgIcon';
 /**
  * Status: *In progress*.
  */
-const StepIndicator = ({ index, numberOfSteps }) =>
+
+const renderLine = (number, numberOfSteps, index) => {
+    if (number >= numberOfSteps - 1) {
+        return null;
+    }
+
+    const isPassed = index > number;
+
+    return (
+        <span className={ classnames(
+            'step-indicator__line',
+            {
+                'step-indicator__line--passed': isPassed
+            })
+        } />
+    );
+};
+
+const renderStep = (index, number, numberOfSteps, label) => {
+    const isActive = index === number;
+    const isPassed = index >= number;
+
+    return (
+        <li className="step-indicator__wrapper" key={`step-indicator-step-${number}`}>
+            <div className={classnames(
+                'step-indicator__step',
+                {
+                    'step-indicator__step--active': isActive,
+                    'step-indicator__step--passed': isPassed
+                }
+            )}>
+                {index > number ?
+                    <span className="icon-wrapper">
+                        <SvgIcon className="icon" iconName="ico_check" />
+                    </span>
+                    : number + 1
+                }
+            </div>
+            <span className={
+                classnames('step-indicator__label', {
+                    'step-indicator__label--active': isActive })}>
+                { label }
+            </span>
+            { renderLine(number, numberOfSteps, index) }
+        </li>
+    );
+};
+
+const StepIndicator = ({ index, numberOfSteps, labels }) =>
     <div className="step-indicator">
         <ul className="step-indicator__list">
-            {_.range(numberOfSteps).map(number =>
-                <li key={`step-indicator-step-${number}`}
-                    className={classnames(
-                        'step-indicator__step',
-                        {
-                            'step-indicator__step--active': index === number,
-                            'step-indicator__step--passed': index >= number
-                        }
-                    )}>
-                    {index > number ?
-                        <span className="icon-wrapper">
-                            <SvgIcon className="icon" iconName="ico_check" />
-                        </span>
-                        : number + 1
-                    }
-                </li>
-            )}
+            {_.range(numberOfSteps).map(number => renderStep(index, number, numberOfSteps, labels[number]) )}
         </ul>
     </div>;
 
@@ -34,7 +66,9 @@ StepIndicator.propTypes = {
     /** Current active step (index starts at 0) */
     index: PropTypes.number,
     /** Total number of steps */
-    numberOfSteps: PropTypes.number
+    numberOfSteps: PropTypes.number,
+    /** Labels for steps */
+    labels: PropTypes.arrayOf(PropTypes.string)
 };
 
 export default StepIndicator;
