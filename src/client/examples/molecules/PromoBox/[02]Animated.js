4da4a8ab64c48ff61adb4763045231dd2035f3d1
Maren Parnas Gulnes <maren.gulnes@bouvet.no>
2018-10-11 14:44:40 +0200

    Removed of heading--pepple class

--- src/client/examples/molecules/PromoBox/[02]Animated.js
+++ src/client/examples/molecules/PromoBox/[02]Animated.js
@@ -6,7 +6,7 @@ const AnimatedPromoBox = () => (
         animated
         imgSrc="/public/website-images/ico_databoost_with_sparkels.svg"
         imgAlt="databoost icon">
-        <h2 className="heading heading--level-2 heading--pebble">Heading</h2>
+        <h2 className="heading heading--level-2">Heading</h2>
         <p className="paragraph">Lorem ipsum dolor sit amet.</p>
     </PromoBox>
 );
