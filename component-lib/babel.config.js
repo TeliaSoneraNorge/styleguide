3795fa851fa1da00e3b8fd7e3968540e725da555
Majid Hajian <mhadaily@gmail.com>
2018-11-15 15:04:03 +0100

    optimize lodash to export cjs, es and modular javascirpt which will be tree shakable

---
+++ component-lib/babel.config.js
@@ -0,0 +1,70 @@
+let defaultPresets;
+
+// We release a ES version of styleguide.
+// It's something that matches the latest official supported features of JavaScript.
+// Nothing more (stage-1, etc), nothing less (require, etc).
+
+if (process.env.BABEL_ENV === 'es') {
+    defaultPresets = [];
+}
+else {
+    defaultPresets = [
+        [
+            '@babel/preset-env',
+            {
+                modules: ['modules', 'production-umd'].includes(process.env.BABEL_ENV) ? false : 'commonjs',
+            },
+        ],
+    ];
+}
+
+const defaultAlias = {
+    '@telia/styleguide': './src',
+};
+
+module.exports = {
+    presets: defaultPresets.concat(['@babel/preset-react']),
+    plugins: [
+        ['@babel/plugin-proposal-class-properties', { loose: true }],
+        [
+            '@babel/plugin-proposal-object-rest-spread',
+            {
+                // Workaround for https://github.com/babel/babel/issues/8323
+                loose: process.env.BABEL_ENV !== 'es',
+            },
+        ],
+        '@babel/plugin-transform-object-assign',
+        '@babel/plugin-transform-runtime',
+    ],
+    env: {
+        test: {
+            sourceMaps: 'both',
+            plugins: [
+                [
+                    'babel-plugin-module-resolver',
+                    {
+                        root: ['./'],
+                        alias: defaultAlias,
+                    },
+                ],
+            ],
+        },
+        development: { },
+        es: {
+            plugins: [
+                'transform-react-constant-elements',
+            ],
+            // It's most likely a babel bug.
+            // We are using this ignore option in the CLI command but that has no effect.
+            ignore: ['**/*.test.js'],
+        },
+        production: {
+            plugins: [
+                'transform-react-constant-elements',
+            ],
+            // It's most likely a babel bug.
+            // We are using this ignore option in the CLI command but that has no effect.
+            ignore: ['**/*.test.js'],
+        },
+    },
+};
--- component-lib/rollup.build.js
+++ component-lib/rollup.build.js
@@ -1,78 +1,53 @@
-import babel from 'rollup-plugin-babel';
-import cjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
-import uglify from 'rollup-plugin-uglify';
-import eslint from 'rollup-plugin-eslint';
-import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 import nodeGlobals from 'rollup-plugin-node-globals';
+import uglify from 'rollup-plugin-uglify';
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 
-import pkg from './package.json';
-
+const input = './src/index.js';
+const name = 'style-guide';
 const globals = {
     react: 'React',
     'react-dom': 'ReactDOM',
-    'prop-types': 'propTypes',
-    'lodash-es': 'lodash-es',
 };
-export default [{
-    //perf: true, // Outputs performance information
-    input: 'src/index.js',
-    external: Object.keys(globals),
-    output: [
-        { file: pkg.main, format: 'cjs', sourcemap: 'inline', globals },
-        { file: pkg.module, format: 'es', sourcemap: 'inline', globals },
-    ],
-    plugins: [
-        eslint(),
-        nodeResolve({
-            jsnext: true,
-            main: true,
-            browser: true
-        }),
-        babel({
-            runtimeHelpers: true,
-            babelrc: false,
-            exclude: 'node_modules/**',
-            presets: ['@babel/preset-react', ['@babel/preset-env', {
-                modules: false,
-                targets: {
-                    browsers :['last 2 versions'],
-                }
-            }]],
-            plugins: [
-                'lodash',
-                '@babel/plugin-proposal-class-properties',
-                ['@babel/plugin-proposal-decorators', { 'legacy': true }],
-                '@babel/plugin-proposal-do-expressions',
-                '@babel/plugin-proposal-export-default-from',
-                '@babel/plugin-proposal-export-namespace-from',
-                '@babel/plugin-proposal-function-bind',
-                '@babel/plugin-proposal-function-sent',
-                '@babel/plugin-proposal-json-strings',
-                '@babel/plugin-proposal-logical-assignment-operators',
-                '@babel/plugin-proposal-nullish-coalescing-operator',
-                '@babel/plugin-proposal-numeric-separator',
-                '@babel/plugin-proposal-optional-chaining',
-                ['@babel/plugin-proposal-pipeline-operator', { 'proposal': 'minimal' }],
-                '@babel/plugin-proposal-throw-expressions',
-                '@babel/plugin-syntax-dynamic-import',
-                '@babel/plugin-syntax-import-meta']
-        }),
-        cjs({
-            ignoreGlobal: true,
-            include: 'node_modules/**',
-            extensions: ['.js', '.jsx']
-        }),
-        replace({
-            ENV: JSON.stringify(process.env.NODE_ENV || 'development')
-        }),
-        //(process.env.NODE_ENV === 'production' && uglify()),
-        nodeGlobals(), // Wait for https://github.com/cssinjs/jss/pull/893
-        sizeSnapshot(),
-    ],
-    watch: {
-        chokidar: true,
-        include: ['./**']
-    }
-}];
+const babelOptions = {
+    exclude: /node_modules/,
+    // We are using @babel/plugin-transform-runtime
+    runtimeHelpers: true,
+    configFile: './babel.config.js',
+};
+const commonjsOptions = {
+    ignoreGlobal: true,
+    include: /node_modules/,
+};
+
+export default [
+    {
+        input,
+        output: { file: `dist/umd/${name}.development.js`, format: 'umd', name, globals },
+        external: Object.keys(globals),
+        plugins: [
+            nodeResolve(),
+            babel(babelOptions),
+            commonjs(commonjsOptions),
+            nodeGlobals(), // Wait for https://github.com/cssinjs/jss/pull/893
+            replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+        ],
+    },
+    {
+        input,
+        output: { file: `dist/umd/${name}.production.min.js`, format: 'umd', name, globals },
+        external: Object.keys(globals),
+        plugins: [
+            nodeResolve(),
+            babel(babelOptions),
+            commonjs(commonjsOptions),
+            nodeGlobals(), // Wait for https://github.com/cssinjs/jss/pull/893
+            replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+            sizeSnapshot(),
+            uglify(),
+        ],
+    },
+];
