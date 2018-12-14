e93cc0f9a4632d96fb063aafa6de11f59b388283
Alex York <alexyork1985@gmail.com>
2018-12-14 15:18:38 +0100

    Fixed some linting

--- src/client/examples/molecules/StepByStep/[04]BasicWithoutDescription.js
+++ src/client/examples/molecules/StepByStep/[04]BasicWithoutDescription.js
@@ -6,14 +6,10 @@ const { Step, Description, Content } = StepByStep;
 const DefaultStepByStep = () => (
     <StepByStep>
         <Step>
-            <Description
-                iconName="ico_buy"
-                heading="Lorem"/>
+            <Description iconName="ico_buy" heading="Lorem" />
         </Step>
         <Step>
-            <Description
-                imageSrc="/public/website-images/boy-ipad.jpg"
-                heading="Ipsum"/>
+            <Description imageSrc="/public/website-images/boy-ipad.jpg" heading="Ipsum" />
             <Content>
                 Ullamcorper massa in dictum consectetur. Sed eu luctus velit. Pellentesque aliquam, lectus eu aliquet
                 suscipit, diam sem vulputate ex, posuere cursus mi arcu ac nunc. Vestibulum dictum metus nisi, non
@@ -26,9 +22,7 @@ const DefaultStepByStep = () => (
             </Content>
         </Step>
         <Step>
-            <Description
-                iconName="ico_buy"
-                heading="Sit dolor"/>
+            <Description iconName="ico_buy" heading="Sit dolor" />
             <Content>
                 Nullam suscipit risus eget lacus gravida sagittis. Suspendisse massa augue,
                 mollis a pretium nec, faucibus nec lorem. In hac habitasse platea dictumst. Integer rhoncus, massa at
