de3c9771154426dee44f18909d051c24d9e1d7b5
Majid Hajian <mhadaily@gmail.com>
2019-01-07 13:00:23 +0100

    fix allicons template typing

--- component-lib/generate-AllIcons-component.js
+++ component-lib/generate-AllIcons-component.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const rawSvg = fs.readFileSync('./assets/allicons/allicons.svg', 'utf8');
-const template = fs.readFileSync('./assets/allicons/AllIcons.jsx.template', 'utf8');
+const template = fs.readFileSync('./assets/allicons/AllIcons.jsx.Template', 'utf8');
 
 let reactComponent =
     template.replace('[ALL_ICONS_SVG_MARKUP]', rawSvg)
