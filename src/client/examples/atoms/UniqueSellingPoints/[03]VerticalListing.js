21fc6569b5d0b9de96e643a31238a7ab89ad6355
Amund Løchen <amund.lochen@bouvet.no>
2019-02-04 13:35:49 +0100

    Add vertical list display option for USPs

---
+++ src/client/examples/atoms/UniqueSellingPoints/[03]VerticalListing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { UniqueSellingPoints } from '@telia/styleguide';
+
+const { Item } = UniqueSellingPoints;
+
+const UniqueSellingPointsVerticalListing = () => (
+    <UniqueSellingPoints className="unique-selling-points__vertical-list">
+        <Item
+            iconName="ico_check_sharp"
+            iconColor="purple">
+            You can use this funtionality
+        </Item>
+        <Item
+            iconName="facebook_icon"
+            iconColor="purple">
+            To list items vertically
+        </Item>
+        <Item
+            iconName="ico_delete"
+            iconColor="purple">
+            And even with different icons
+        </Item>
+    </UniqueSellingPoints>
+);
+
+export default UniqueSellingPointsVerticalListing;
