cd1e6e6c72e8b3ecf3d45e024ff955e90ee90059
Anmer Seif <anmer.seif@bouvet.no>
2019-02-07 10:11:01 +0100

    Added InfoBox

--- component-lib/src/index.js
+++ component-lib/src/index.js
@@ -85,6 +85,7 @@ export { default as FunkyTabs } from './molecules/FunkyTabs';
 export { default as Video } from './molecules/Video';
 export { default as ArticleList } from './molecules/ArticleList';
 export { default as LightAlert } from './molecules/LightAlert';
+export { default as InfoBox } from './molecules/InfoBox';
 
 // Organisms
 export { default as ContentRating } from './organisms/ContentRating';
---
+++ component-lib/src/molecules/InfoBox/InfoBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Heading from '../../atoms/Heading/Heading';
+
+/**
+ * Status: *finished*.
+ * 
+ * This should be used in `small` or `medium` width containers. In a `large`, the text lines become too long to read comfortably.
+ */
+const InfoBox = ({ title, children }) => (
+    <div className="info-box">
+        <Heading level="2" text={title} />
+        {children}
+    </div>
+);
+
+InfoBox.propTypes = {
+    title: PropTypes.string,
+    children: PropTypes.node
+};
+
+export default InfoBox;
