9be0340d165bb32428a521fd2979753f87f694ae
Anmer Seif <anmer.seif@bouvet.no>
2019-02-21 13:12:48 +0100

    Added functionality block

---
+++ src/client/examples/molecules/DisplayFunctionality/Default.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { DisplayFunctionality, Container } from '@telia/styleguide';
+
+const imgs = [ 
+    '/public/website-images/screenshot1.jpg',
+    '/public/website-images/screenshot2.jpg'
+];
+
+const DefaultDisplayFunctionality = () => (
+    <DisplayFunctionality reverse={true} imgs={imgs}> 
+        <h3 className="heading heading--level-2">Bedrifter med behov for dokumentasjon</h3>
+        <p className="paragraph">
+            Mobilt Opptak fra Telia sørger for opptak og trygg lagring av samtaler og meldinger. 
+        </p>
+        <p className="paragraph">
+            Tjenesten er særlig egnet for bank og finans, men kan også benyttes av andre bedrifter med behov for dokumentasjon.
+        </p>
+        <br/>
+        <br/>
+        <br/>
+        <a className="button">Bestill nå</a>
+    </DisplayFunctionality>
+);
+
+export default DefaultDisplayFunctionality;
