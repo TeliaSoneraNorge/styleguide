d89f418d21ea5399a8f2d2ed6cceaf07bb88dbaf
Vegard Haugstvedt <it.vegard@gmail.com>
2019-02-27 13:09:56 +0100

    Revert to old step indicator design and make accessibility modifications
    - Make sure we don't use disabled and onClick on divs
    - Use links for clickable elements, but with onClicks to enable javascript-triggered navigation
    - Switched from an unordered to a ordered list
    - Remove background color on "next" steps, to better separate them from the current step
    - Remove hover-styling on "next" steps
    - Make focus-/hover-styling on previous steps more like styleguide standard

--- component-lib/src/atoms/StepIndicator/StepIndicator.jsx
+++ component-lib/src/atoms/StepIndicator/StepIndicator.jsx
@@ -2,6 +2,7 @@ import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
 import range from 'lodash/range';
+import SvgIcon from '../SvgIcon/SvgIcon';
 
 /**
  * Status: *In progress*.
@@ -18,40 +19,49 @@ const renderLine = (number, numberOfSteps, index) => {
     );
 };
 
+const StepElement = ({ children, isPassed, onClick }) => {
+    if (isPassed) {
+        return (
+            <a href="javascript:void(0)" className="step-indicator__element step-indicator__element__clickable" onClick={onClick}>
+                {children}
+            </a>
+        );
+    } else {
+        return (
+            <div className="step-indicator__element">
+                {children}
+            </div>
+        );
+    }
+};
+
 const renderStep = (index, number, numberOfSteps, label, link) => {
     const isActive = index === number;
-    const isPassed = index >= number;
-    const isPassedButNotActive = isPassed && !isActive;
-
-    const onClick = () => {
-        if (isPassedButNotActive) {
-            window.location.assign(link);
-        }
-    };
-
-    const Element = isPassedButNotActive ? 'button' : 'div';
+    const isPassed = index > number;
 
     return (
         <li className="step-indicator__wrapper" key={`step-indicator-step-${number}`}>
-            <Element
-                className={classnames('step-indicator__clickable', {
-                    'step-indicator__clickable--future': !isPassedButNotActive
-                })}
-                onClick={onClick}
-                disabled={isPassed || isActive ? 0 : -1}>
-                <span className={
-                    classnames('step-indicator__label', { 'step-indicator__label--active': isActive })}>
-                    {label}
-                    {index > number && <span className="sr-only">- fullført</span>}
-                </span>
+            <StepElement isPassed={isPassed} onClick={() => window.location.assign(link)}>
                 <div className={classnames(
                     'step-indicator__step',
                     {
                         'step-indicator__step--active': isActive,
                         'step-indicator__step--passed': isPassed
                     }
-                )} />
-            </Element>
+                )}>
+                    {index > number ?
+                        <span className="icon-wrapper">
+                            <SvgIcon className="icon" iconName="ico_check" role="presentation" title={`${number + 1}`} />
+                        </span>
+                        : number + 1
+                    }
+                </div>
+                <span className={
+                    classnames('step-indicator__label', { 'step-indicator__label--active': isActive })}>
+                    {label}
+                    {index > number && <span className="sr-only">- fullført</span>}
+                </span>
+            </StepElement>
             { renderLine(number, numberOfSteps, index) }
         </li>
     );
@@ -59,9 +69,9 @@ const renderStep = (index, number, numberOfSteps, label, link) => {
 
 const StepIndicator = ({ index, numberOfSteps, labels, links }) =>
     <div className="step-indicator">
-        <ul className="step-indicator__list">
+        <ol className="step-indicator__list">
             {range(numberOfSteps).map(number => renderStep(index, number, numberOfSteps, labels[number], links[number]))}
-        </ul>
+        </ol>
     </div>;
 
 StepIndicator.propTypes = {
