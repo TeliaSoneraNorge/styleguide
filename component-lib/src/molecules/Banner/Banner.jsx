2fa2269ae90102dd65b214162d3371896f8e298b
Anmer Seif <anmer.seif@bouvet.no>
2019-03-07 11:33:15 +0100

    updated banner block description and added grey background

--- component-lib/src/molecules/Banner/Banner.jsx
+++ component-lib/src/molecules/Banner/Banner.jsx
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
  * Status: *finished*.
  *
  * Should be placed in a medium or large container. Never small.
+ * Should always use white banner on grey background and grey banner on white background. 
  **/
 export default class Banner extends React.Component {
     static propTypes = {
@@ -24,7 +25,8 @@ export default class Banner extends React.Component {
             <div className={ classnames('banner',{
                 [this.props.className]: this.props.className,
                 'banner--reverse': this.props.reverse,
-                'banner--background-grey': this.props.grey
+                'banner--background-grey': this.props.grey,
+                'banner--background-white': !this.props.grey
             })}>
                 <div className="banner__col banner__content">
                     {this.props.children}
