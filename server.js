3f9fe268f0e1cd8677ef5db3541629ec8cdb2516
Alex York <alexyork1985@gmail.com>
2019-03-12 13:27:47 +0100

    Fixed the server/routing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const Bundler = require('parcel-bundler');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -9,12 +10,18 @@ if (process.env.LOCAL_DEV === 'true') {
     const bundler = new Bundler('./src/index.html', {});
     app.use(bundler.middleware());
 } else {
-    // Production: server the 'dist' folder
+    // Production: serve the 'dist' folder
     app.use(express.static('dist'));
+
+    // All page requests should serve the index.html file (client-side routing then handles the rest)
+    app.get('/*', (req, res) => {
+        const indexHtml = path.resolve(__dirname, './dist/index.html')
+        res.sendFile(indexHtml);
+    });
 }
 
 // Start server
-const port = (process.env.PORT || 3001);
+const port = (process.env.PORT || 3000);
 app.listen(port, () => {
     console.log(`Styleguide app listening on port ${port}!`);
 });
