e23d9448fba83054c7527fc4367458ade632cb69
Alex York <alexyork1985@gmail.com>
2019-03-12 14:26:44 +0100

    Fixed ZIP file & an npm script

--- scripts/generate-zip-file.js
+++ scripts/generate-zip-file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const fileName = 'TeliaStyleguide-Latest.zip';
-const outputPath = path.join(__dirname, '../dist/public/', fileName);
+const outputPath = path.join(__dirname, '../dist/', fileName);
 const output = fs.createWriteStream(outputPath);
 const archive = archiver('zip');
 
@@ -25,15 +25,15 @@ archive.on('error', (err) => {
 
 archive.pipe(output);
 
-const css = fs.readFileSync('./dist/public/css/bundle.components.css', 'utf8').replace(/\/public\//g, '');
+const css = fs.readFileSync(path.resolve(__dirname, '../component-lib/dist/index.css'), 'utf8').replace(/\.\/assets\//g, '');
 const packageJson = require('../package.json');
 
 archive
     .append(packageJson.version, { name: 'version.txt' })
     .append('Follow the instuctions on the Telia Styleguide for usage.', { name: 'README.txt' })
     .append(css, { name: 'telia-styleguide.css' })
-    .directory('./dist/public/fonts/', 'fonts')
-    .directory('./dist/public/allicons/', 'allicons')
-    .directory('./dist/public/icons/', 'icons')
-    .directory('./dist/public/pebbles/', 'pebbles')
+    .directory(path.resolve(__dirname, '../component-lib/dist/assets/fonts/'), 'fonts')
+    .directory(path.resolve('../component-lib/dist/assets/allicons/'), 'allicons')
+    .directory(path.resolve('../component-lib/dist/assets/icons/'), 'icons')
+    .directory(path.resolve('../component-lib/dist/assets/pebbles/'), 'pebbles')
     .finalize();
