9b523d0346bc4d9998cfab814b4594791c671eb1
Amund Løchen <amund.lochen@bouvet.no>
2019-03-15 13:46:17 +0100

    Expanded accordion styling

--- src/components/examples/molecules/AccordionList/ListOfAccordions.js
+++ src/components/examples/molecules/AccordionList/ListOfAccordions.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { AccordionList } from '@telia/styleguide';
 
+/**Status: Add possibility for passing objects to the right content area */
+
 const ListOfAccordions = () => (
     <AccordionList
         accordionItems={[
@@ -13,6 +15,20 @@ const ListOfAccordions = () => (
                 id: 'example-2',
                 title: 'An open accordion heading',
                 children: 'Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Proin eget tortor risus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Donec sollicitudin molestie malesuada.'
+            },
+            {
+                id: 'example-3',
+                title: 'Accordion with icon and preamble',
+                children: 'Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Proin eget tortor risus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Donec sollicitudin molestie malesuada.',
+                icon: 'ico_mobile_o',
+                text: 'Ønsker du å gi dine ansatte større frihet, og samtidig sikre selskapets…'
+            },
+            {
+                id: 'example-4',
+                title: 'Accordion with log in button',
+                children: 'Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Proin eget tortor risus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Donec sollicitudin molestie malesuada.',
+                text: 'Ønsker du å gi dine ansatte større frihet, og samtidig sikre selskapets…',
+                button: 'Log in'
             }
         ]}
         isExpandedAccordionIndex={1} />
