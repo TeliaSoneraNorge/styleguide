cf20f54cb2116ded19cbd5b840c549cfbc437fc8
Alex York <alexyork1985@gmail.com>
2019-03-18 22:15:56 +0100

    Deleted the consumer-app. It conflicts with the root app node_modules.

--- consumer-app/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './components/App';
-
-const mountElement = document.getElementById('app');
-ReactDOM.render(<App />, mountElement);
