de169bea7b267bf0398c6ccb9feebc6a81cfa781
Honza Smid <jansmidhonza@gmail.com>
2019-03-26 16:51:56 +0100

    added babel plugin to avoid having export/import ES6 syntax in the build files

--- component-lib/babel.config.js
+++ component-lib/babel.config.js
@@ -12,6 +12,7 @@ module.exports = {
         ['@babel/plugin-proposal-class-properties', { loose: true }],
         '@babel/plugin-proposal-object-rest-spread',
         '@babel/plugin-transform-object-assign',
+        '@babel/plugin-transform-modules-commonjs',
         'lodash'
     ],
     env: {
