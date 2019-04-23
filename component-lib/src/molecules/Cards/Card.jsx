0e894986074da6a6007f43f7ac91db263713fb29
Maren Parnas Gulnes <maren.gulnes@bouvet.no>
2019-04-23 14:01:16 +0200

    Updated props and css

--- component-lib/src/molecules/Cards/Card.jsx
+++ component-lib/src/molecules/Cards/Card.jsx
@@ -5,28 +5,27 @@ import classnames from 'classnames';
 /**
  * Status: *finished*.
  */
-const Card = ({ children, isGrey, img, alt, title, link }) => (
-    <div className="card__wrapper">
-        <a className={ classnames('card link', {
-            'card--background-grey' : isGrey
-        })} href={link}>
-            <img className="card__image" src={img} alt={alt} />
-            <div className="card__content">
-                <h2 className="heading heading--level-2">{title}</h2>
-                <span className="card__horizontal-line"></span>
-                {children}
-            </div>
-        </a>
-    </div>
-);
+const Card = ({ title, link, isGrey, img, children }) => (
+    <a className={ classnames('card link', {
+        'card--background-grey' : isGrey
+    })} href={link}>
+        <img className="card__image" src={img.src} alt={img.alt} />
+        <div className="card__content">
+            <h2 className="heading heading--level-2">{title}</h2>
+            <span className="card__horizontal-line"></span>
+            {children}
+        </div>
+    </a>
+    );
 
 Card.propTypes = {
-    title: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
     isGrey: PropTypes.bool,
-    img: PropTypes.string,
-    alt: PropTypes.string,
-    title: PropTypes.string,
-    link: PropTypes.string
+    img: PropTypes.shape({
+        src: PropTypes.string.isRequired,
+        alt: PropTypes.string.isRequired
+    })
 };
 
 export default Card;
--- src/components/examples/molecules/Cards/Default.js
+++ src/components/examples/molecules/Cards/Default.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import { Card, Cards } from 'component-lib';
 
-const img = require('../../../../assets/website-images/screenshot1.png');
+const imgSrc = require('../../../../assets/website-images/screenshot1.png');
 
 const DefaultCards = () => (
     <Cards isGrey={true}>
         <Card
-            img={img}
-            alt="Man and woman looking at an iPad."
+            img={{ src: imgSrc, alt: "Man and woman looking at an iPad." }}
             link="#"
             title="Dette er cardsblokk 1" />
         <Card
-            img={img}
-            alt="Man and woman looking at an iPad."
+            img={{ src: imgSrc, alt: "Man and woman looking at an iPad." }}
             link="#"
             title="Dette er cardsblokk 2" />
         <Card
-            img={img}
-            alt="Man and woman looking at an iPad."
+            img={{ src: imgSrc, alt: "Man and woman looking at an iPad." }}
             link="#"
             title="Dette er cardsblokkasd asdasdasdasd 3" />
     </Cards>
