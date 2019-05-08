cc1be24ec4899d802f9c10c082a7f986cdba0a53
Maren Parnas Gulnes <maren.gulnes@bouvet.no>
2019-05-08 09:40:10 +0200

    Forgot this file

---
+++ src/components/examples/molecules/Subscription/03-SmallMBWithFeatureOnExpanded.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button, Subscription } from 'component-lib';
+
+const features = {
+    button: <Button text="Gå videre" kind="primary" onClick={() => {}} />
+};
+
+const ThreeDigitsMBSubscription = () => (
+    <Subscription
+        name="SMART Mini"
+        id="smartMini"
+        dataAmount={200}
+        dataUnit="MB"
+        price={99}
+        priceInfo={['pr. md', 'Ingen bindingstid.']}
+        color="light-orange"
+        size="small"
+        features={features} />
+);
+
+export default ThreeDigitsMBSubscription;
