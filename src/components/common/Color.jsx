49b3e853cddcda3db8a3dac8e4d9e1b3acdb6147
Bjørn Even Egge <bjornevenegge@gmail.com>
2019-06-03 11:17:17 +0200

    Changing layout of color palette

--- src/components/common/Color.jsx
+++ src/components/common/Color.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import classnames from 'classnames'
 
-const Color = ({ name, hex, textColor }) =>
-    <div className="sg-color" style={{ backgroundColor: hex, color: textColor }}>
-        <div className="sg-color__name">{name}</div>
-        <div className="sg-color__hex">{hex}</div>
-    </div>;
+ 
+const Color = ({ name, hex, textColor, priority }) =>
+<div class="sg-color">
+    <span class={classnames("sg-color__circle", 
+        priority === "1" ? "sg-color__circle-large" : null, 
+        priority === "2" ? "sg-color__circle-medium" : null)} 
+        style={{ backgroundColor: hex }}></span>
+    <p align="center">{name}</p>
+    <p align="center">{hex}</p>
+    <p class="sg-color__text-color-hint" style={{ backgroundColor: hex, color:textColor }} align="center">Use black text</p>
+</div>
 
 export default Color;
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -26,8 +26,9 @@ const HomePage = () =>
             <Heading level={2} text="Color Palette" />
             <p className="paragraph">The color palette below was taken from the Telia Company design document (TODO: source) and shows the recommended font color to give the correct contrast.</p>
         </div>
-        <div className="sg-colors-wrapper container container--medium">
-            {_.map(colors, (color, name) => <Color key={name} name={name} {...color} />)}
+        <div className="container container--medium">
+            <div className="sg-colors-wrapper"> {_.map(_.filter(colors,{priority:"1"}), (color, name) => <Color key={name} name={name} {...color} />)}</div>
+            <div className="sg-colors-wrapper"> {_.map(_.filter(colors,{priority:"2"}), (color, name) => <Color key={name} name={name} {...color} />)}</div>
         </div>
 
         <div className="container container--small">
