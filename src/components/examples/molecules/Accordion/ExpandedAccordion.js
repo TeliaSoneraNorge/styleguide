05c0275eb795ae678b24d860bad578b499312b22
Marek Magdziak <marek.magdziak@hotmail.cz>
2019-06-03 10:06:39 +0200

    Expose accordion in component-lib (#400)

---
+++ src/components/examples/molecules/Accordion/ExpandedAccordion.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Accordion } from 'component-lib';
+
+const ExpandedAccordion = () => (
+    <Accordion title="Lorem ipsum" isExpanded={true}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia ipsum posuere blandit ornare. Phasellus consequat aliquet pellentesque. Curabitur est nulla, rhoncus non turpis in, dignissim consectetur nunc. Vestibulum fringilla dolor nulla, non blandit turpis aliquet vel. Suspendisse potenti.
+    </Accordion>
+);
+
+export default ExpandedAccordion;
