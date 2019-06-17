6ea4b6f26d281c481b773853689c37ea9ac20182
Anmer Seif <anmer.seif@bouvet.no>
2019-06-17 08:04:42 +0200

    Added Gallery component

--- src/utils/componentUtil.js
+++ src/utils/componentUtil.js
@@ -111,6 +111,7 @@ export function isFullWidthComponent(componentName) {
         'Menu',
         'DisplayFunctionality',
         'Cards',
+        'Gallery',
 
         // Organisms
         'FocusSubscription',
