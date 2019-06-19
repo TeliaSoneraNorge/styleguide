d433c82433f4a5aa0557a4bc4e2807f2c97ec9a0
Tomas Vanicek <tomas.vanicek@telia.no>
2019-06-19 10:15:56 +0200

    Added disabled state for CheckBoxWithLabel component.

--- component-lib/src/molecules/CheckBoxWithLabel/CheckBoxWithLabel.jsx
+++ component-lib/src/molecules/CheckBoxWithLabel/CheckBoxWithLabel.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import classnames from 'classnames';
 
 import Label from '../../atoms/Label/Label';
 
@@ -9,12 +10,15 @@ import Label from '../../atoms/Label/Label';
  *
  * Waiting on the Photoshop files to get the exact checkmark SVG.
  */
-const CheckBoxWithLabel = ({ label, defaultChecked, onChange, checked }) => (
+const CheckBoxWithLabel = ({ label, defaultChecked, onChange, checked, disabled }) => (
     <Label className="check-box-with-label">
-        <input onChange={onChange} checked={checked} className="check-box-with-label__input" type="checkbox" defaultChecked={defaultChecked} />
+        <input onChange={onChange} checked={checked} className="check-box-with-label__input" type="checkbox" defaultChecked={defaultChecked} disabled={disabled} />
         <span className="check-box-with-label__svg-container">
             <svg className="check-box-with-label__svg" width="20px" height="20px" viewBox="0 0 20 20">
-                <path className="check-box-with-label__frame" d="M16,19H4c-1.7,0-3-1.4-3-3V4c0-1.7,1.3-3,3-3h12c1.6,0,3,1.3,3,3v12C19,17.6,17.6,19,16,19z" />
+                <path
+                    className={classnames('check-box-with-label__frame', disabled && 'check-box-with-label__frame--disabled')}
+                    d="M16,19H4c-1.7,0-3-1.4-3-3V4c0-1.7,1.3-3,3-3h12c1.6,0,3,1.3,3,3v12C19,17.6,17.6,19,16,19z"
+                />
                 <path className="check-box-with-label__check-mark" d="M5,7.7c1.5,2.4,3.1,4.8,4.6,7.3c2.1-3.3,4.3-6.7,6.4-10" />
             </svg>
         </span>
@@ -26,7 +30,8 @@ CheckBoxWithLabel.propTypes = {
     label: PropTypes.string.isRequired,
     defaultChecked: PropTypes.bool,
     checked: PropTypes.bool,
-    onChange: PropTypes.func
+    onChange: PropTypes.func,
+    disabled: PropTypes.bool
 };
 
 export default CheckBoxWithLabel;
---
+++ src/components/examples/molecules/CheckBoxWithLabel/Disabled.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { CheckBoxWithLabel } from '@telia/styleguide';
+
+const Disabled = () => (
+    <CheckBoxWithLabel label="Checkbox is disabled" disabled />
+);
+
+export default Disabled;
