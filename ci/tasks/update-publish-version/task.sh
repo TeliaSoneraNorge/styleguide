9e65fec070b63962a22156f9c2c0d619daa23ac9
Maren Parnas Gulnes <maren.gulnes@bouvet.no>
2019-08-02 13:17:16 +0200

    separate builds

---
+++ ci/tasks/update-publish-version/task.sh
@@ -0,0 +1,37 @@
+set -ueo pipefail
+
+DIR="${PWD}"
+export GREEN='\033[1;32m'
+export NC='\033[0m'
+export CHECK="√"
+
+echo "Installing git"
+apk add --update --no-cache git
+git config --global user.email "concourse-ci@localhost"
+git config --global user.name "concourse-ci"
+
+echo "Update styleguide version:"
+
+cd styleguide
+npm version ${update_type} --no-git-tag
+
+echo "Update component lib version:"
+
+cd component-lib
+npm version ${update_type} --no-git-tag
+
+ls compontent-lib/dist
+cd component-lib/dist
+npm version ${update_type} --no-git-tag
+
+cd ../..
+git commit -a -m ":airplane: Updating styleguide version type: ${update_type}"
+
+git clone "${DIR}/styleguide" "${DIR}/updated-version"
+
+echo -e "${GREEN}${CHECK} Package version updated"
+
+cd component-lib
+npm publish dist
+
+echo -e "${GREEN}${CHECK} Latest version deployed"
