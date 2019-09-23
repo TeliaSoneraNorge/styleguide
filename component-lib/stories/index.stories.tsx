62a4fd5cb6d62992bf484cea24cf6dd3d7d75709
Nils Magnus Englund <nme@stepone.no>
2019-09-23 09:13:13 +0200

    Changed Storybook config to use TypeScript

---
+++ component-lib/stories/index.stories.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
+
+import Button from "../src/business/Button";
+import { Welcome } from "@storybook/react/demo";
+import "!style-loader!postcss-loader!../src/atoms/Button/Button.pcss";
+
+storiesOf("Welcome", module).add("to Storybook", () => (
+    <Welcome showApp={linkTo("Button")} />
+));
+
+storiesOf("Button", module)
+    .add("with text", () => (
+        <Button text="Hello Button" onClick={action("clicked")} />
+    ))
+    .add("with some emoji", () => (
+        <Button onClick={action("clicked")} text="😀 😎 👍 💯" />
+    ));
