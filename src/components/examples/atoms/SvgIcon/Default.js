dcc4168085ddef35f0cafe567f686f2235834829
Maren Parnas Gulnes <maren.gulnes@bouvet.no>
2019-10-01 13:35:02 +0200

    remove title from icons and aria hide icons without title

--- src/components/examples/atoms/SvgIcon/Default.js
+++ src/components/examples/atoms/SvgIcon/Default.js
@@ -3,7 +3,7 @@ import { SvgIcon } from '@telia/styleguide';
 
 const DefaultSvgIcon = () => (
     <div>
-        <SvgIcon iconName="ico_heart" color="black" />
+        <SvgIcon iconName="ico_heart" color="black" title="Black heart" />
         <SvgIcon iconName="ico_heart" color="core-purple" />
         <SvgIcon iconName="ico_heart" color="light-grey" />
         <SvgIcon iconName="ico_heart" color="white" style={{background: '#990AE3'}} />
