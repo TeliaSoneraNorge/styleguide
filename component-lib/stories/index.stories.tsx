9bf48b82a41d6fb9278847077d6b1dd07711ff10
Nils Magnus Englund <nme@stepone.no>
2019-10-02 12:30:58 +0200

    Remove original Button from business area

--- component-lib/stories/index.stories.tsx
+++ component-lib/stories/index.stories.tsx
@@ -4,7 +4,6 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
 
-import Button from "../src/business/Button";
 import Toggle from "../src/business/Toggle";
 import { Welcome } from "@storybook/react/demo";
 import "!style-loader!postcss-loader!../src/business.pcss";
@@ -12,14 +11,6 @@ import "!style-loader!postcss-loader!../src/business.pcss";
 storiesOf("Welcome", module).add("to Storybook", () => (
     <Welcome showApp={linkTo("Button")} />
 ));
-
-storiesOf("Button", module)
-    .add("with text", () => (
-        <Button text="Hello Button" onClick={action("clicked")} />
-    ))
-    .add("with some emoji", () => (
-        <Button onClick={action("clicked")} text="😀 😎 👍 💯" />
-    ));
 
 storiesOf("Toggle", module)
     .add('default', () => (
