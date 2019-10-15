1d26a299186c1f2ca8d27e2f9e16964148dfcd43
Nils Magnus Englund <nme@stepone.no>
2019-10-15 14:17:16 +0200

    Added business/Icon component

---
+++ component-lib/assets/business-icons/README.md
@@ -0,0 +1,5 @@
+# Business icons
+
+These icons were imported from Zeplin at 2019-10-15.
+
+Contact: Bjørn Even Egge, Jonathan Esbjug
---
+++ component-lib/generate-AllBusinessIcons-component.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const rawSvg = fs.readFileSync('./assets/business-allicons/business-allicons.svg', 'utf8');
+const template = fs.readFileSync('./src/business/Icon/AllBusinessIcons.tsx.template', 'utf8');
+
+let reactComponent = template
+    .replace('[ALL_ICONS_LAST_GENERATED_AT]', new Date().toISOString())
+    .replace('[ALL_ICONS_SVG_MARKUP]', rawSvg)
+    .replace(/\<symbol/gi, '        <symbol')
+    .replace(/\<\/svg/gi, '    </svg');
+
+fs.writeFileSync('./src/business/Icon/AllBusinessIcons.tsx', reactComponent);
