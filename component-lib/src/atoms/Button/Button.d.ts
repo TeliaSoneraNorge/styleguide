f28b48e7c71f731e7638b526d1ed316ec09e047e
Nils Magnus Englund <nme@stepone.no>
2019-10-18 09:11:49 +0200

    Added TS definition to atoms/Button

---
+++ component-lib/src/atoms/Button/Button.d.ts
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface Props {
+    text?: string;
+    type?: 'button' | 'reset' | 'submit';
+    kind?: 'primary' | 'cancel' | 'link' | 'inverted' | 'negative';
+    margin?: 'top' | 'bottom';
+    size?: 'small';
+    onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+    className?: string;
+    processingText?: string;
+    isProcessing?: boolean;
+    isDisabled?: boolean;
+}
+
+declare function Button(props: Props): React.ReactElement | null;
+
+export default Button;
