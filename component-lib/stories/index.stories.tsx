5f421efce024eb744316a5ecece12344fc527e3f
Nils Magnus Englund <nme@stepone.no>
2019-10-18 09:12:04 +0200

    Added sample Storybook story for atoms/Button

--- component-lib/stories/index.stories.tsx
+++ component-lib/stories/index.stories.tsx
@@ -1,5 +1,7 @@
 import "!style-loader!postcss-loader!../src/business.pcss";
 
+require('./atoms/Button.stories.tsx');
+
 require('./business/Icon.stories.tsx');
 require('./business/Toggle.stories.tsx');
 
