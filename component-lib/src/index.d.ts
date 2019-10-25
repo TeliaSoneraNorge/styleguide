6b39c0bb49b0eaee7ada36eceaec37f7d75eacd3
Nils Magnus Englund <nme@stepone.no>
2019-10-25 16:49:17 +0200

    Work in progress, TS type definitions

---
+++ component-lib/src/index.d.ts
@@ -0,0 +1,5 @@
+// Fra index.tsx
+export { default as AlertText } from "./atoms/AlertText/AlertText";
+export { default as Button } from "./atoms/Button/Button";
+export { default as Heading } from "./atoms/Heading/Heading";
+export { default as Container } from "./atoms/Container";
