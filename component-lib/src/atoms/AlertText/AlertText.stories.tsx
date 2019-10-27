196bb4fb52523ea642cb9f46c2f1e8e755f29967
Nils Magnus Englund <nme@stepone.no>
2019-10-27 08:21:40 +0100

    Remove AlertText stories in anticipation of new types

--- component-lib/src/atoms/AlertText/AlertText.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-
-import AlertText from "./AlertText";
-
-storiesOf("Atoms|AlertText", module).add("AlertText", () => (
-    <AlertText>Chat is closed</AlertText>
-));
