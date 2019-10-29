56831936951089ce49e02396eddd5b89c1845e6f
Marek Magdziak <marek.magdziak@hotmail.cz>
2019-10-29 14:40:08 +0100

    Allow modal dialog templating (#461)

---
+++ src/components/examples/molecules/ModalDialog/WithCustomHeader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ModalDialog } from '@telia/styleguide';
+
+const WithCustomHeader = () => (
+    <ModalDialog
+        name="info-dialog"
+        submitText="Ok"
+        headerElement={<section id={`${name}-heading`}>
+            <h2 className="modal-dialog__heading">Custom header</h2>
+            <p>Some subtitle</p>
+        </section>}
+        standalone>
+        <p>This dialog can be associated with actions where it's necessary to give the user more information.</p>
+    </ModalDialog>
+);
+
+export default WithCustomHeader;
