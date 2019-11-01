5eb8f855d73050d5f8a715a245287ebab0a05fa5
Nils Magnus Englund <nme@stepone.no>
2019-11-01 15:55:45 +0100

    Added Toggle component in business area (#465)

--- component-lib/stories/index.stories.tsx
+++ component-lib/stories/index.stories.tsx
@@ -7,6 +7,7 @@ import { storiesOf } from "@storybook/react";
  * loader in the storybook-specific .storybook/webpack.config.js.
  */
 import "../src/index.pcss";
+import "../src/business.pcss";
 
 import HomePage from "./home/HomePage";
 import ColorPalette from "./home/ColorPalette";
