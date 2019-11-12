a501dac833df037541a0a933f160f6fe0568f0b5
bogsti <christoffer.bogsti@telia.no>
2019-11-12 14:15:22 +0100

    rebase master and changed to displayLoader

--- VERSIONS.md
+++ VERSIONS.md
@@ -15,6 +15,10 @@ For Major and Minor changes in version you must notify everyone in the #stylegui
 
 Only noteworthy versions shown (minor changes are omitted).
 
+## 1.29.6
+- Lint and Prettier fix
+- Menu search: Shows spinner when loading and input field are disabled
+
 ## 1.29.3
 - Menu & ModalDialog: IE compatibility fixes
 
--- component-lib/src/molecules/Menu/MenuSearch.jsx
+++ component-lib/src/molecules/Menu/MenuSearch.jsx
@@ -7,96 +7,87 @@ import FocusTrap from './FocusTrap';
 
 const KEY_ESC = 27;
 
-export default function MenuSearch({
-    onSubmit,
-    searchLabel,
-    searchButtonLabel,
-    searchButtonAbortText,
-}) {
-    const [focused, setFocused] = useState(false);
-    const [searchQuery, setSearchQuery] = useState('');
-    const [loading, setLoading] = useState(false);
+export default function MenuSearch({ onSubmit, searchLabel, searchButtonLabel, searchButtonAbortText }) {
+  const [focused, setFocused] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [loading, setLoading] = useState(false);
 
-    const searchBoxRef = useRef();
-    const openSearchButton = useRef();
+  const searchBoxRef = useRef();
+  const openSearchButton = useRef();
 
-    useEffect(() => {
-        document.addEventListener('keydown', handleKeyDown);
-        return () => document.removeEventListener('keydown', handleKeyDown);
-    });
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    return () => document.removeEventListener('keydown', handleKeyDown);
+  });
 
-    const handleKeyDown = (event) => {
-        if (event.keyCode === KEY_ESC && focused)
-            closeSeachBox();
-    };
+  const handleKeyDown = event => {
+    if (event.keyCode === KEY_ESC && focused) closeSeachBox();
+  };
 
-    const closeSeachBox = () => {
-        setFocused(false);
+  const closeSeachBox = () => {
+    setFocused(false);
 
-        if (openSearchButton.current)
-            openSearchButton.current.focus();
-    };
+    if (openSearchButton.current) openSearchButton.current.focus();
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (onSubmit) onSubmit(e, searchQuery);
-        setLoading(true);
-    };
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (onSubmit) onSubmit(e, searchQuery);
+    setLoading(true);
+  };
 
-    const renderSearchField = (Component, additionalProps = {}) => (
-        <Component
-            className="menu__search--open"
-            ref={searchBoxRef}
-            {...additionalProps}>
-            <form onSubmit={handleSubmit} className="menu__search--open-form">
-                <TextBoxWithLabel
-                    type="search"
-                    autoFocus
-                    withIcon={true}
-                    iconName="ico_search"
-                    iconLabel={searchButtonLabel}
-                    disabled={loading}
-                    iconIsButton
-                    onIconClick={onSubmit}
-                    searchIsLoading={loading}
-                    small
-                    hideLabel={true}
-                    labelText={searchLabel}
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)} />
-            </form>
-            <button className="menu__search--open-abort-button button button--small"
-                onClick={() => closeSeachBox()}
-                aria-label={searchButtonAbortText ? searchButtonAbortText : 'Lukk'} >
-                {searchButtonAbortText ? searchButtonAbortText : 'Lukk'}
-            </button>
-        </Component>
-    );
+  const renderSearchField = (Component, additionalProps = {}) => (
+    <Component className="menu__search--open" ref={searchBoxRef} {...additionalProps}>
+      <form onSubmit={handleSubmit} className="menu__search--open-form">
+        <TextBoxWithLabel
+          type="search"
+          autoFocus
+          withIcon={true}
+          iconName="ico_search"
+          iconLabel={searchButtonLabel}
+          disabled={loading}
+          iconIsButton
+          onIconClick={onSubmit}
+          displayLoader={loading}
+          small
+          hideLabel={true}
+          labelText={searchLabel}
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+        />
+      </form>
+      <button
+        className="menu__search--open-abort-button button button--small"
+        onClick={() => closeSeachBox()}
+        aria-label={searchButtonAbortText ? searchButtonAbortText : 'Lukk'}
+      >
+        {searchButtonAbortText ? searchButtonAbortText : 'Lukk'}
+      </button>
+    </Component>
+  );
 
-    return (
-        <div className={classnames('menu__search', { 'menu__search--focused': focused })} >
-            <button
-                ref={openSearchButton}
-                className="menu__search button--stripped"
-                onClick={() => setFocused(true)}
-                aria-label={searchButtonLabel ? searchButtonLabel : 'Søk'} >
-
-                <SvgIcon className="menu__search--icon" iconName="ico_search-menu" color="none" />
-                <div className="menu__search--label" aria-label={searchButtonLabel ? searchButtonLabel : 'Søk'}>
-                    {searchButtonLabel ? searchButtonLabel : 'Søk'}
-                </div>
-            </button>
-
-            {focused &&
-                renderSearchField(FocusTrap, { as: 'div' })
-            }
+  return (
+    <div className={classnames('menu__search', { 'menu__search--focused': focused })}>
+      <button
+        ref={openSearchButton}
+        className="menu__search button--stripped"
+        onClick={() => setFocused(true)}
+        aria-label={searchButtonLabel ? searchButtonLabel : 'Søk'}
+      >
+        <SvgIcon className="menu__search--icon" iconName="ico_search-menu" color="none" />
+        <div className="menu__search--label" aria-label={searchButtonLabel ? searchButtonLabel : 'Søk'}>
+          {searchButtonLabel ? searchButtonLabel : 'Søk'}
         </div>
-    );
+      </button>
+
+      {focused && renderSearchField(FocusTrap, { as: 'div' })}
+    </div>
+  );
 }
 
 MenuSearch.propTypes = {
-    onSubmit: PropTypes.func,
-    searchLabel: PropTypes.string,
-    searchButtonLabel: PropTypes.string,
-    searchButtonAbortText: PropTypes.string,
+  onSubmit: PropTypes.func,
+  searchLabel: PropTypes.string,
+  searchButtonLabel: PropTypes.string,
+  searchButtonAbortText: PropTypes.string,
 };
