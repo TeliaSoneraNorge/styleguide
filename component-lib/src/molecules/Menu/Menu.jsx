781c6837bcddc5686804f1c760251005fcfb34bb
Maren Parnas Gulnes <maren.gulnes@bouvet.no>
2019-11-15 16:22:55 +0100

    Focus trap mobile menu

--- component-lib/src/molecules/Menu/Menu.jsx
+++ component-lib/src/molecules/Menu/Menu.jsx
@@ -1,184 +1,182 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import MenuTop from './MenuTop';
 import MenuContent from './MenuContent';
 import MobileMenu from './MobileMenu';
+import FocusTrap from '../../atoms/FocusTrap/FocusTrap';
 
-const defaultLinkTemplate = ({ url, ...otherProps }) =>
-    <a href={url} {...otherProps} />;
+const Menu = ({
+  menuLinks,
+  className,
+  logoImageDesktopPath,
+  logoImageInverseDesktopPath,
+  logoTitle,
+  logoUrl,
+  activeIndex,
+  activeLinkIndex,
+  onSearchSubmit,
+  searchLabel,
+  searchButtonLabel,
+  searchButtonAbortText,
+  mobileMenuCloseButtonLabel,
+  linkTemplate,
+  lockBodyOnMenuOpen,
+  isLoggedIn,
+  loginUrl,
+  myPageUrl,
+  isLoading,
+}) => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [openedSubmenuIndex, setOpenedSubmenuIndex] = useState(-1);
+  const mobileMenuRef = useRef();
 
+  useEffect(() => {
+    document.addEventListener('click', onClickaway);
+    document.addEventListener('keydown', onGlobalKeyDown);
+    return () => {
+      document.removeEventListener('click', onClickaway);
+      document.removeEventListener('keydown', onGlobalKeyDown);
+    };
+  });
 
-const isDescendant = (parent, child) => {
+  const defaultLinkTemplate = ({ url, ...otherProps }) => <a href={url} {...otherProps} />;
+  const LinkTemplate = linkTemplate || defaultLinkTemplate;
+  let openedSubmenu = null;
+
+  const isDescendant = (parent, child) => {
     while (child) {
-        if (child === parent) {
-            return true;
-        }
-        child = child.parentNode;
+      if (child === parent) {
+        return true;
+      }
+      child = child.parentNode;
     }
     return false;
+  };
+
+  const onGlobalKeyDown = e => {
+    const key = e.which || e.keyCode;
+
+    if (key === 27 && openedSubmenuIndex !== -1) {
+      // escape key
+      toggleSubmenu(openedSubmenuIndex, e);
+    }
+  };
+
+  const onClickaway = e => {
+    if (!openedSubmenu) return;
+    if (isDescendant(openedSubmenu, e.target)) return;
+
+    setOpenedSubmenuIndex(-1);
+    openedSubmenu = null;
+  };
+
+  const toggleMobileMenu = () => {
+    if (!mobileMenuOpen && lockBodyOnMenuOpen) {
+      document.body.classList.add('body--locked');
+    } else {
+      document.body.classList.remove('body--locked');
+    }
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
+  const toggleSubmenu = (submenuIndex, e) => {
+    e.stopPropagation();
+
+    if (openedSubmenuIndex === submenuIndex) {
+      setOpenedSubmenuIndex(-1);
+      openedSubmenu = null;
+    } else {
+      setOpenedSubmenuIndex(submenuIndex);
+      openedSubmenu = e.target;
+    }
+  };
+
+  const renderMobileMenu = (Component, additionalProps = {}) => (
+    <Component ref={mobileMenuRef} {...additionalProps}>
+      <MobileMenu
+        isOpen={mobileMenuOpen}
+        LinkTemplate={LinkTemplate}
+        onMobileMenuToggle={toggleMobileMenu}
+        menuLinks={menuLinks}
+        selectedHeaderIndex={activeIndex}
+        onMenuItemSelected={toggleMobileMenu}
+        isLoading={isLoading}
+        mobileMenuCloseButtonLabel={mobileMenuCloseButtonLabel}
+      />
+    </Component>
+  );
+
+  return (
+    <div className={classnames('menu', { [className]: className })}>
+      {menuLinks && menuLinks.length > 1 && (
+        <MenuTop activeIndex={activeIndex} menuLinks={menuLinks} LinkTemplate={LinkTemplate} />
+      )}
+
+      <MenuContent
+        logo={{
+          image: logoImageDesktopPath,
+          imageInverted: logoImageInverseDesktopPath,
+          title: logoTitle,
+          url: logoUrl,
+        }}
+        LinkTemplate={LinkTemplate}
+        menuLink={menuLinks && menuLinks[activeIndex]}
+        onToggleSubmenu={toggleSubmenu}
+        openedSubmenuIndex={openedSubmenuIndex}
+        activeIndex={activeLinkIndex}
+        loginUrl={loginUrl}
+        onMobileMenuToggle={toggleMobileMenu}
+        onSearchSubmit={onSearchSubmit}
+        searchLabel={searchLabel}
+        searchButtonLabel={searchButtonLabel}
+        searchButtonAbortText={searchButtonAbortText}
+        isLoggedIn={isLoggedIn}
+        myPageUrl={myPageUrl}
+        isLoading={isLoading}
+      />
+      {mobileMenuOpen && renderMobileMenu(FocusTrap, { as: 'div' })}
+    </div>
+  );
+};
+
+Menu.propTypes = {
+  menuLinks: PropTypes.arrayOf(
+    PropTypes.shape({
+      heading: PropTypes.shape({
+        text: PropTypes.string,
+        url: PropTypes.string,
+      }),
+      links: PropTypes.arrayOf(
+        PropTypes.shape({
+          text: PropTypes.string,
+          url: PropTypes.string,
+        })
+      ),
+    })
+  ),
+  logoImageDesktopPath: PropTypes.string,
+  logoImageInverseDesktopPath: PropTypes.string,
+  logoTitle: PropTypes.string,
+  logoUrl: PropTypes.string,
+  activeIndex: PropTypes.number,
+  activeLinkIndex: PropTypes.number,
+  onSearchSubmit: PropTypes.func,
+  searchLabel: PropTypes.string,
+  searchButtonLabel: PropTypes.string,
+  searchButtonAbortText: PropTypes.string,
+  mobileMenuCloseButtonLabel: PropTypes.string,
+  linkTemplate: PropTypes.func,
+  lockBodyOnMenuOpen: PropTypes.bool,
+  isLoggedIn: PropTypes.bool,
+  loginUrl: PropTypes.string,
+  myPageUrl: PropTypes.string,
+  cartUrl: PropTypes.string,
+  isLoading: PropTypes.bool,
 };
 
 /**
  * Status: *In progress*.
  * Category: PageElements
  **/
-export default class Menu extends React.Component {
-    static propTypes = {
-        menuLinks: PropTypes.array,
-        logoImageDesktopPath: PropTypes.string,
-        logoImageInverseDesktopPath: PropTypes.string,
-        logoTitle: PropTypes.string,
-        logoUrl: PropTypes.string,
-        activeIndex: PropTypes.number,
-        activeLinkIndex: PropTypes.number,
-        onSearchSubmit: PropTypes.func,
-        searchLabel: PropTypes.string,
-        searchButtonLabel: PropTypes.string,
-        searchButtonAbortText: PropTypes.string,
-        mobileMenuCloseButtonLabel: PropTypes.string,
-        linkTemplate: PropTypes.func,
-        lockBodyOnMenuOpen: PropTypes.bool,
-        isLoggedIn: PropTypes.bool,
-        loginUrl: PropTypes.string,
-        myPageUrl: PropTypes.string,
-        cartUrl: PropTypes.string,
-        isLoading: PropTypes.bool
-    };
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            open: false,
-            mobileMenuOpen: false,
-            openedSubmenuIndex: -1
-        };
-
-        this.closeMobileMenu = this.toggleMobileMenu.bind(this);
-        this.toggleMobileMenu = this.toggleMobileMenu.bind(this);
-        this.toggleSubmenu = this.toggleSubmenu.bind(this);
-        this.onClickaway = this.onClickaway.bind(this);
-        this.onGlobalKeyDown = this.onGlobalKeyDown.bind(this);
-    }
-
-    componentDidMount() {
-        document.addEventListener('click', this.onClickaway);
-        document.addEventListener('keydown', this.onGlobalKeyDown);
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener('click', this.onClickaway);
-        document.removeEventListener('keydown', this.onGlobalKeyDown);
-    }
-
-    onGlobalKeyDown(e) {
-        const key = e.which || e.keyCode;
-
-        if (key === 27 && this.state.openedSubmenuIndex !== -1) { // escape key
-            this.toggleSubmenu(this.state.openedSubmenuIndex, e);
-        }
-    }
-
-    onClickaway(event) {
-        if (!this.openedSubmenu) return;
-        if (isDescendant(this.openedSubmenu, event.target)) return;
-
-        this.setState({ openedSubmenuIndex: -1 });
-        this.openedSubmenu = null;
-    }
-
-    toggleMobileMenu() {
-        if (!this.state.mobileMenuOpen && this.props.lockBodyOnMenuOpen) {
-            document.body.classList.add('body--locked');
-        } else {
-            document.body.classList.remove('body--locked');
-        }
-
-        this.setState({ mobileMenuOpen: !this.state.mobileMenuOpen });
-    }
-
-    toggleSubmenu(submenuIndex, event) {
-        event.stopPropagation();
-
-        if (this.state.openedSubmenuIndex === submenuIndex) {
-            this.setState({ openedSubmenuIndex: -1 });
-            this.openedSubmenu = null;
-        } else {
-            this.setState({ openedSubmenuIndex: submenuIndex });
-            this.openedSubmenu = event.target;
-        }
-    }
-
-    render() {
-        const LinkTemplate = this.props.linkTemplate || defaultLinkTemplate;
-        const {
-            openedSubmenuIndex,
-            mobileMenuOpen
-        } = this.state;
-
-        const {
-            menuLinks,
-            logoUrl = '/',
-            logoImageDesktopPath,
-            logoImageInverseDesktopPath,
-            logoTitle,
-            onSearchSubmit,
-            searchLabel,
-            searchButtonLabel,
-            searchButtonAbortText,
-            mobileMenuCloseButtonLabel,
-            isLoggedIn,
-            loginUrl,
-            myPageUrl,
-            activeIndex = 0,
-            activeLinkIndex = -1,
-            isLoading
-        } = this.props;
-
-        const logo = {
-            image: logoImageDesktopPath,
-            imageInverted: logoImageInverseDesktopPath,
-            title: logoTitle,
-            url: logoUrl
-        };
-
-        return (
-            <div className={classnames('menu', { [this.props.className]: this.props.className })}>
-                {menuLinks && menuLinks.length > 1 &&
-                <MenuTop
-                    activeIndex={activeIndex}
-                    menuLinks={menuLinks}
-                    LinkTemplate={LinkTemplate} />
-                }
-
-                <MenuContent
-                    logo={logo}
-                    LinkTemplate={LinkTemplate}
-                    menuLink={menuLinks && menuLinks[activeIndex]}
-                    onToggleSubmenu={this.toggleSubmenu}
-                    openedSubmenuIndex={openedSubmenuIndex}
-                    activeIndex={activeLinkIndex}
-                    loginUrl={loginUrl}
-                    onMobileMenuToggle={this.toggleMobileMenu}
-                    onSearchSubmit={onSearchSubmit}
-                    searchLabel={searchLabel}
-                    searchButtonLabel={searchButtonLabel}
-                    searchButtonAbortText={searchButtonAbortText}
-                    isLoggedIn={isLoggedIn}
-                    myPageUrl={myPageUrl}
-                    isLoading={isLoading} />
-
-                <MobileMenu
-                    isOpen={mobileMenuOpen}
-                    LinkTemplate={LinkTemplate}
-                    onMobileMenuToggle={this.toggleMobileMenu}
-                    menuLinks={menuLinks}
-                    selectedHeaderIndex={activeIndex}
-                    onMenuItemSelected={this.toggleMobileMenu}
-                    isLoading={isLoading}
-                    mobileMenuCloseButtonLabel={mobileMenuCloseButtonLabel} />
-            </div>
-        );
-    }
-}
+export default Menu;
--- component-lib/src/molecules/Menu/MobileMenu.jsx
+++ component-lib/src/molecules/Menu/MobileMenu.jsx
@@ -1,148 +1,163 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classnames from 'classnames';
+import PropTypes from 'prop-types';
 import Spinner from '../../atoms/Spinner';
 import SvgIcon from '../../atoms/SvgIcon/SvgIcon';
 
 const MobileMenuCloseButton = ({ onClick, mobileMenuCloseButtonLabel }) => (
-    <button className="menu__mobile-close-button"
-        onClick={onClick}
-        aria-label={mobileMenuCloseButtonLabel ? mobileMenuCloseButtonLabel : 'Lukk'} >
-        <SvgIcon
-            iconName="ico_delete"
-            color="black"
-            className="menu__mobile-close-button-icon" />
-    </button>
+  <button
+    className="menu__mobile-close-button"
+    onClick={onClick}
+    aria-label={mobileMenuCloseButtonLabel ? mobileMenuCloseButtonLabel : 'Lukk'}
+  >
+    <SvgIcon iconName="ico_delete" color="black" className="menu__mobile-close-button-icon" />
+  </button>
 );
 
 const MobileMenuHeaderItem = ({ index, onHeaderItemSelected, menuLink, isActive }) => (
-    <li>
-        <div onClick={() => onHeaderItemSelected(index)} tabIndex="0"
-            className={classnames(
-                'menu__mobile-heading-item', {
-                    'menu__mobile-heading-item--active': isActive,
-                })}>
-            <span className="menu__mobile-heading-item-text">{menuLink.heading && menuLink.heading.text}</span>
-        </div>
-    </li>
+  <li>
+    <div
+      onClick={() => onHeaderItemSelected(index)}
+      tabIndex="0"
+      className={classnames('menu__mobile-heading-item', {
+        'menu__mobile-heading-item--active': isActive,
+      })}
+    >
+      <span className="menu__mobile-heading-item-text">{menuLink.heading && menuLink.heading.text}</span>
+    </div>
+  </li>
 );
 
 const MobileMenuHeader = ({
-    onMobileMenuToggle,
-    menuLinks,
-    LinkTemplate,
-    selectedIndex,
-    onHeaderItemSelected,
-    mobileMenuCloseButtonLabel }) => (
-    <div className="menu__mobile-header">
-        {menuLinks &&
-        <ul className="menu__mobile-heading-links">
-            {menuLinks.map((menuLink, index) => <MobileMenuHeaderItem
-                key={menuLink.heading ? menuLink.heading.text : index}
-                index={index}
-                LinkTemplate={LinkTemplate}
-                menuLink={menuLink}
-                isActive={selectedIndex === index}
-                onHeaderItemSelected={onHeaderItemSelected} />)}
-        </ul>
-        }
-        <MobileMenuCloseButton onClick={onMobileMenuToggle} mobileMenuCloseButtonLabel={mobileMenuCloseButtonLabel} />
-    </div>
+  onMobileMenuToggle,
+  menuLinks,
+  LinkTemplate,
+  selectedIndex,
+  onHeaderItemSelected,
+  mobileMenuCloseButtonLabel,
+}) => (
+  <div className="menu__mobile-header">
+    {menuLinks && (
+      <ul className="menu__mobile-heading-links">
+        {menuLinks.map((menuLink, index) => (
+          <MobileMenuHeaderItem
+            key={menuLink.heading ? menuLink.heading.text : index}
+            index={index}
+            LinkTemplate={LinkTemplate}
+            menuLink={menuLink}
+            isActive={selectedIndex === index}
+            onHeaderItemSelected={onHeaderItemSelected}
+          />
+        ))}
+      </ul>
+    )}
+    <MobileMenuCloseButton onClick={onMobileMenuToggle} mobileMenuCloseButtonLabel={mobileMenuCloseButtonLabel} />
+  </div>
 );
 
 const MobileSubmenu = ({ link, LinkTemplate, onItemSelected }) => (
-    <div className="menu__mobile-submenu">
-        <span className="menu__mobile-item-with-children">{link.text}</span>
-        <div className="menu__mobile-submenu-container">
-            {link.subLinks.map((sublink, index) =>
-                <LinkTemplate
-                    onClick={() => onItemSelected(index, sublink)}
-                    key={sublink.text}
-                    className="menu__mobile-subitem link"
-                    url={sublink.url}>
-                    <span className="link__content">{sublink.text}</span>
-                </LinkTemplate>
-            )}
-        </div>
+  <div className="menu__mobile-submenu">
+    <span className="menu__mobile-item-with-children">{link.text}</span>
+    <div className="menu__mobile-submenu-container">
+      {link.subLinks.map((sublink, index) => (
+        <LinkTemplate
+          onClick={() => onItemSelected(index, sublink)}
+          key={sublink.text}
+          className="menu__mobile-subitem link"
+          url={sublink.url}
+        >
+          <span className="link__content">{sublink.text}</span>
+        </LinkTemplate>
+      ))}
     </div>
+  </div>
 );
 
 const MobileMenuItem = ({ link, onItemSelected, LinkTemplate }) => (
-    <React.Fragment>
-        {link.url &&
-            <LinkTemplate
-                onClick={onItemSelected}
-                className="menu__mobile-item link"
-                url={link.url}>
-                <span className="link__content">{link.text}</span>
-            </LinkTemplate>}
-        {!link.url && <MobileSubmenu link={link} onItemSelected={onItemSelected} LinkTemplate={LinkTemplate} />}
-    </React.Fragment>
+  <React.Fragment>
+    {link.url && (
+      <LinkTemplate onClick={onItemSelected} className="menu__mobile-item link" url={link.url}>
+        <span className="link__content">{link.text}</span>
+      </LinkTemplate>
+    )}
+    {!link.url && <MobileSubmenu link={link} onItemSelected={onItemSelected} LinkTemplate={LinkTemplate} />}
+  </React.Fragment>
 );
 
 const MobileMenuItemSection = ({ menuLink, onItemSelected, LinkTemplate }) => (
-    <section id={`${menuLink.heading && menuLink.heading.text}-mobile-panel`} className="menu__mobile-panel">
-        {menuLink.heading && menuLink.heading.url &&
-            <LinkTemplate
-                onClick={onItemSelected}
-                className="menu__mobile-item link"
-                url={menuLink.heading.url}>
-                <span className="link__content">Forside</span>
-            </LinkTemplate>
-        }
+  <section id={`${menuLink.heading && menuLink.heading.text}-mobile-panel`} className="menu__mobile-panel">
+    {menuLink.heading && menuLink.heading.url && (
+      <LinkTemplate onClick={onItemSelected} className="menu__mobile-item link" url={menuLink.heading.url}>
+        <span className="link__content">Forside</span>
+      </LinkTemplate>
+    )}
 
-        {menuLink.links.map((link, index) =>
-            <MobileMenuItem
-                index={index}
-                key={link.text}
-                link={link}
-                onItemSelected={onItemSelected}
-                LinkTemplate={LinkTemplate} />)}
-
-    </section>
+    {menuLink.links.map((link, index) => (
+      <MobileMenuItem
+        index={index}
+        key={link.text}
+        link={link}
+        onItemSelected={onItemSelected}
+        LinkTemplate={LinkTemplate}
+      />
+    ))}
+  </section>
 );
 
-class MobileMenu extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedHeaderIndex: 0
-        };
-    }
+const MobileMenu = ({
+  isOpen,
+  LinkTemplate,
+  onMobileMenuToggle,
+  menuLinks,
+  onMenuItemSelected,
+  isLoading,
+  mobileMenuCloseButtonLabel,
+}) => {
+  const [selectedHeaderIndex, setSelectedHeaderIndex] = useState(0);
 
-    render() {
-        const {
-            selectedHeaderIndex
-        } = this.state;
+  return (
+    <div className={classnames('menu__mobile', { 'menu__mobile--open': isOpen })}>
+      <MobileMenuHeader
+        onMobileMenuToggle={onMobileMenuToggle}
+        menuLinks={menuLinks}
+        LinkTemplate={LinkTemplate}
+        selectedIndex={selectedHeaderIndex}
+        onHeaderItemSelected={index => setSelectedHeaderIndex(index)}
+        mobileMenuCloseButtonLabel={mobileMenuCloseButtonLabel}
+      />
+      {isLoading && <Spinner />}
+      {menuLinks && (
+        <MobileMenuItemSection
+          menuLink={menuLinks[selectedHeaderIndex]}
+          onItemSelected={onMenuItemSelected}
+          LinkTemplate={LinkTemplate}
+        />
+      )}
+    </div>
+  );
+};
 
-        const {
-            isOpen,
-            LinkTemplate,
-            onMobileMenuToggle,
-            menuLinks,
-            onMenuItemSelected,
-            isLoading,
-            mobileMenuCloseButtonLabel
-        } = this.props;
-
-        return (
-            <div className={classnames('menu__mobile', { 'menu__mobile--open': isOpen })}>
-                <MobileMenuHeader
-                    onMobileMenuToggle={onMobileMenuToggle}
-                    menuLinks={menuLinks}
-                    LinkTemplate={LinkTemplate}
-                    selectedIndex={selectedHeaderIndex}
-                    onHeaderItemSelected={index => this.setState({ selectedHeaderIndex: index })}
-                    mobileMenuCloseButtonLabel={mobileMenuCloseButtonLabel} />
-                {isLoading && <Spinner />}
-                {menuLinks &&
-                <MobileMenuItemSection
-                    menuLink={menuLinks[selectedHeaderIndex]}
-                    onItemSelected={onMenuItemSelected}
-                    LinkTemplate={LinkTemplate} />
-                }
-            </div>
-        );
-    }
-}
+MobileMenu.propTypes = {
+  isOpen: PropTypes.bool,
+  LinkTemplate: PropTypes.func,
+  onMobileMenuToggle: PropTypes.func,
+  menuLinks: PropTypes.arrayOf(
+    PropTypes.shape({
+      heading: PropTypes.shape({
+        text: PropTypes.string,
+        url: PropTypes.string,
+      }),
+      links: PropTypes.arrayOf(
+        PropTypes.shape({
+          text: PropTypes.string,
+          url: PropTypes.string,
+        })
+      ),
+    })
+  ),
+  onMenuItemSelected: PropTypes.func,
+  isLoading: PropTypes.bool,
+  mobileMenuCloseButtonLabel: PropTypes.string,
+};
 
 export default MobileMenu;
