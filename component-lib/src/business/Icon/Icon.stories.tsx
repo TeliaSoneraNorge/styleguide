60a6887cb7c94b25597ba64553d2af6c39358125
Nils Magnus Englund <nme@stepone.no>
2019-11-19 14:47:45 +0100

    Icon stories: Run prettier, refactored icon into a map to fix select() bug

--- component-lib/src/business/Icon/Icon.stories.tsx
+++ component-lib/src/business/Icon/Icon.stories.tsx
@@ -1,73 +1,60 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
+import React from 'react';
+import { storiesOf } from '@storybook/react';
 
-import { withKnobs, color, select } from "@storybook/addon-knobs";
+import { withKnobs, color, select } from '@storybook/addon-knobs';
 
-import { AllBusinessIcons } from "./AllBusinessIcons";
-import { Icon } from "./Icon";
-import { IconDefinition } from "./IconDefinition";
+import { AllBusinessIcons } from './AllBusinessIcons';
+import { Icon } from './Icon';
+import { IconDefinition } from './IconDefinition';
 
-const stories = storiesOf("Business|Icon", module);
+const stories = storiesOf('Business|Icon', module);
 
 stories.addDecorator(withKnobs);
 
-stories.add("Icon", () => (
-    <div style={{ padding: "24px" }}>
-        <AllBusinessIcons />
+stories.add('Icon', () => (
+  <div style={{ padding: '24px' }}>
+    <AllBusinessIcons />
 
-        <h1>Icon</h1>
+    <h1>Icon</h1>
 
-        <p>
-            The component <code>{"<AllBusinessIcons />"}</code> (which itself is
-            invisible) must be present in order for the SVGs to be available to
-            the <code>{"<Icon />"}</code> component.
-        </p>
+    <p>
+      The component <code>{'<AllBusinessIcons />'}</code> (which itself is invisible) must be present in order for the
+      SVGs to be available to the <code>{'<Icon />'}</code> component.
+    </p>
 
-        <p>
-            The icon inherits its color from the surrounding element (
-            <code>{"fill: currentColor;"}</code>).
-        </p>
+    <p>
+      The icon inherits its color from the surrounding element (<code>{'fill: currentColor;'}</code>).
+    </p>
 
-        <h2>Sample icon</h2>
+    <h2>Sample icon</h2>
 
-        <div
-            style={{
-                transition: "color 0.15s ease-in-out",
-                color: color("Color", "red")
-            }}
-        >
-            <Icon
-                icon={
-                    select(
-                        "Sample icon",
-                        Object.keys(availableIcons),
-                        "ai-robot"
-                    ) as IconDefinition
-                }
-            />
-        </div>
-
-        <h2>All icons</h2>
-
-        <ul style={{ listStyleType: "none", margin: "0", padding: "0" }}>
-            {availableIcons.map((key: IconDefinition) => (
-                <li
-                    key={key}
-                    style={{
-                        padding: "8px",
-                        display: "inline-block",
-                        color: color("Color for all icons", "red")
-                    }}
-                    title={key}
-                >
-                    <Icon
-                        style={{ width: "32px", height: "32px" }}
-                        icon={key}
-                    />
-                </li>
-            ))}
-        </ul>
+    <div
+      style={{
+        transition: 'color 0.15s ease-in-out',
+        color: color('Color', 'red'),
+      }}
+    >
+      <Icon icon={select('Sample icon', availableIcons, 'ai-robot') as IconDefinition} />
     </div>
+
+    <h2>All icons</h2>
+
+    <ul style={{ listStyleType: 'none', margin: '0', padding: '0' }}>
+      {availableIcons.map((key: IconDefinition) => (
+        <li
+          key={key}
+          style={{
+            padding: '8px',
+            display: 'inline-block',
+            color: color('Color for all icons', 'red'),
+          }}
+          title={key}
+        >
+          <Icon style={{ width: '32px', height: '32px' }} icon={key} />
+        </li>
+      ))}
+    </ul>
+  </div>
 ));
 
 /**
@@ -76,126 +63,126 @@ stories.add("Icon", () => (
  * if the list does not match the list of icons in IconDefinition.
  */
 // const availableIcons: Record<IconDefinition, IconDefinition> = {
-const availableIconMap: { [key in IconDefinition]: true } = {
-    add: true,
-    "ai-robot": true,
-    alarm: true,
-    alert: true,
-    "arrow-down": true,
-    "arrow-large-down": true,
-    "arrow-large-left": true,
-    "arrow-large-right": true,
-    "arrow-large-up": true,
-    "arrow-left": true,
-    "arrow-right": true,
-    "arrow-small-down": true,
-    "arrow-small-left": true,
-    "arrow-small-right": true,
-    "arrow-small-up": true,
-    "arrow-up": true,
-    attach: true,
-    battery: true,
-    "best-in-test": true,
-    broadband: true,
-    "business-intelligence": true,
-    buy: true,
-    calendar: true,
-    camera: true,
-    "card-view": true,
-    chat: true,
-    "check-mark": true,
-    close: true,
-    cloud: true,
-    delivery: true,
-    devices: true,
-    "document-doc": true,
-    "document-pdf": true,
-    "document-ppt": true,
-    document: true,
-    "document-xls": true,
-    download: true,
-    email: true,
-    facebook: true,
-    filter: true,
-    "folder-new": true,
-    folder: true,
-    games: true,
-    gift: true,
-    graph: true,
-    "grid-view": true,
-    "heart-filled": true,
-    heart: true,
-    home: true,
-    image: true,
-    infinite: true,
-    info: true,
-    invoice: true,
-    ipad: true,
-    laptop: true,
-    level: true,
-    link: true,
-    "list-view": true,
-    lock: true,
-    login: true,
-    logout: true,
-    menu: true,
-    minus: true,
-    "mobile-recycle": true,
-    mobile: true,
-    "money-euro": true,
-    money: true,
-    more: true,
-    open: true,
-    orgchart: true,
-    payment: true,
-    pen: true,
-    "person-new": true,
-    person: true,
-    phone: true,
-    "pie-chart": true,
-    place: true,
-    play: true,
-    position: true,
-    question: true,
-    refresh: true,
-    return: true,
-    router: true,
-    search: true,
-    server: true,
-    services: true,
-    settings: true,
-    shopping_2: true,
-    shopping: true,
-    shuffle: true,
-    signal: true,
-    "sim-card": true,
-    sms: true,
-    "star-filled": true,
-    statistics: true,
-    summary: true,
-    support: true,
-    sync: true,
-    "table-view": true,
-    tags: true,
-    time: true,
-    "time-waiting": true,
-    trash: true,
-    tv: true,
-    twitter: true,
-    undo: true,
-    upload: true,
-    video: true,
-    "visibility-filled": true,
-    visibility: true,
-    volume: true,
-    warning: true,
-    "wireless-off": true,
-    wireless: true,
-    "world-alert": true,
-    "world-off": true,
-    "world-question": true,
-    world: true,
-    wrench: true
+const availableIconMap: { [key in IconDefinition]: key } = {
+  add: 'add',
+  'ai-robot': 'ai-robot',
+  alarm: 'alarm',
+  alert: 'alert',
+  'arrow-down': 'arrow-down',
+  'arrow-large-down': 'arrow-large-down',
+  'arrow-large-left': 'arrow-large-left',
+  'arrow-large-right': 'arrow-large-right',
+  'arrow-large-up': 'arrow-large-up',
+  'arrow-left': 'arrow-left',
+  'arrow-right': 'arrow-right',
+  'arrow-small-down': 'arrow-small-down',
+  'arrow-small-left': 'arrow-small-left',
+  'arrow-small-right': 'arrow-small-right',
+  'arrow-small-up': 'arrow-small-up',
+  'arrow-up': 'arrow-up',
+  attach: 'attach',
+  battery: 'battery',
+  'best-in-test': 'best-in-test',
+  broadband: 'broadband',
+  'business-intelligence': 'business-intelligence',
+  buy: 'buy',
+  calendar: 'calendar',
+  camera: 'camera',
+  'card-view': 'card-view',
+  chat: 'chat',
+  'check-mark': 'check-mark',
+  close: 'close',
+  cloud: 'cloud',
+  delivery: 'delivery',
+  devices: 'devices',
+  'document-doc': 'document-doc',
+  'document-pdf': 'document-pdf',
+  'document-ppt': 'document-ppt',
+  document: 'document',
+  'document-xls': 'document-xls',
+  download: 'download',
+  email: 'email',
+  facebook: 'facebook',
+  filter: 'filter',
+  'folder-new': 'folder-new',
+  folder: 'folder',
+  games: 'games',
+  gift: 'gift',
+  graph: 'graph',
+  'grid-view': 'grid-view',
+  'heart-filled': 'heart-filled',
+  heart: 'heart',
+  home: 'home',
+  image: 'image',
+  infinite: 'infinite',
+  info: 'info',
+  invoice: 'invoice',
+  ipad: 'ipad',
+  laptop: 'laptop',
+  level: 'level',
+  link: 'link',
+  'list-view': 'list-view',
+  lock: 'lock',
+  login: 'login',
+  logout: 'logout',
+  menu: 'menu',
+  minus: 'minus',
+  'mobile-recycle': 'mobile-recycle',
+  mobile: 'mobile',
+  'money-euro': 'money-euro',
+  money: 'money',
+  more: 'more',
+  open: 'open',
+  orgchart: 'orgchart',
+  payment: 'payment',
+  pen: 'pen',
+  'person-new': 'person-new',
+  person: 'person',
+  phone: 'phone',
+  'pie-chart': 'pie-chart',
+  place: 'place',
+  play: 'play',
+  position: 'position',
+  question: 'question',
+  refresh: 'refresh',
+  return: 'return',
+  router: 'router',
+  search: 'search',
+  server: 'server',
+  services: 'services',
+  settings: 'settings',
+  shopping_2: 'shopping_2',
+  shopping: 'shopping',
+  shuffle: 'shuffle',
+  signal: 'signal',
+  'sim-card': 'sim-card',
+  sms: 'sms',
+  'star-filled': 'star-filled',
+  statistics: 'statistics',
+  summary: 'summary',
+  support: 'support',
+  sync: 'sync',
+  'table-view': 'table-view',
+  tags: 'tags',
+  time: 'time',
+  'time-waiting': 'time-waiting',
+  trash: 'trash',
+  tv: 'tv',
+  twitter: 'twitter',
+  undo: 'undo',
+  upload: 'upload',
+  video: 'video',
+  'visibility-filled': 'visibility-filled',
+  visibility: 'visibility',
+  volume: 'volume',
+  warning: 'warning',
+  'wireless-off': 'wireless-off',
+  wireless: 'wireless',
+  'world-alert': 'world-alert',
+  'world-off': 'world-off',
+  'world-question': 'world-question',
+  world: 'world',
+  wrench: 'wrench',
 };
 
-const availableIcons = Object.keys(availableIconMap) as IconDefinition[];
+const availableIcons = Object.values(availableIconMap);
