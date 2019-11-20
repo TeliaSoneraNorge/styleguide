117d0bbf75e54dd5f71c54f7bfdf1daaf2282062
Nils Magnus Englund <nme@stepone.no>
2019-11-20 15:08:57 +0100

    Run prettier on scripts, config, utils

--- scripts/generate-static-data.js
+++ scripts/generate-static-data.js
@@ -5,12 +5,12 @@ const packageJson = require('../package.json');
 const colorsJson = require('../src/colors.json');
 
 const staticData = {
-    colors: colorsJson,
-    variablesCss: fs.readFileSync('./component-lib/src/variables.pcss', 'utf8'),
-    versionsHtml: marked(fs.readFileSync('./VERSIONS.md', 'utf8')),
-    icons: fs.readdirSync('./component-lib/assets/icons').filter(f => f.match(/\.svg$/)),
-    pebbles: fs.readdirSync('./component-lib/assets/pebbles'),
-    version: packageJson.version
+  colors: colorsJson,
+  variablesCss: fs.readFileSync('./component-lib/src/variables.pcss', 'utf8'),
+  versionsHtml: marked(fs.readFileSync('./VERSIONS.md', 'utf8')),
+  icons: fs.readdirSync('./component-lib/assets/icons').filter(f => f.match(/\.svg$/)),
+  pebbles: fs.readdirSync('./component-lib/assets/pebbles'),
+  version: packageJson.version,
 };
 
 fs.writeFileSync('./src/static-data.json', JSON.stringify(staticData, null, 2));
--- src/utils/componentUtil.js
+++ src/utils/componentUtil.js
@@ -4,120 +4,124 @@ import componentCategories from '../component-categories';
 import { getComponentDocumentation, getComponentCategory } from './staticDataUtil';
 
 function getComponentsByType(type, componentsInFolder) {
-    const examples = require('../components/examples/**/*.js');
+  const examples = require('../components/examples/**/*.js');
 
-    return _.reduce(componentsInFolder, (result, value, componentName) => {
-        if (!value[componentName]) {
-            return result;
-        }
-
-        const component = value[componentName].default;
-
-        result[componentName] = {
-            name: componentName,
-            type: type,
-            component,
-            docs: getComponentDocumentation(type, componentName) || '*No documentation found*',
-            examples: examples[type][componentName]
-        };
+  return _.reduce(
+    componentsInFolder,
+    (result, value, componentName) => {
+      if (!value[componentName]) {
         return result;
-    }, {});
+      }
+
+      const component = value[componentName].default;
+
+      result[componentName] = {
+        name: componentName,
+        type: type,
+        component,
+        docs: getComponentDocumentation(type, componentName) || '*No documentation found*',
+        examples: examples[type][componentName],
+      };
+      return result;
+    },
+    {}
+  );
 }
 
 function getComponentsByPredicate(predicate, allComponents, heading = '', category) {
-    const examples = require('../components/examples/**/*.js');
+  const examples = require('../components/examples/**/*.js');
 
-    const componentsInCategory = _.map(allComponents, (components, type) => {
-        const filtered = _.filter(components, (_, componentName) => predicate(type, componentName));
+  const componentsInCategory = _.map(allComponents, (components, type) => {
+    const filtered = _.filter(components, (_, componentName) => predicate(type, componentName));
 
-        return filtered.map(c =>
-            _.map(c, (component, name) => ({
-                name,
-                type,
-                component,
-                docs: getComponentDocumentation(type, name) || '*No documentation found*',
-                examples: examples[type][name]
-            }))
-        );
-    });
+    return filtered.map(c =>
+      _.map(c, (component, name) => ({
+        name,
+        type,
+        component,
+        docs: getComponentDocumentation(type, name) || '*No documentation found*',
+        examples: examples[type][name],
+      }))
+    );
+  });
 
-    const flattenedCompontents = _.flattenDeep(componentsInCategory);
-    return {
-        heading: heading || category,
-        components: flattenedCompontents
-    };
+  const flattenedCompontents = _.flattenDeep(componentsInCategory);
+  return {
+    heading: heading || category,
+    components: flattenedCompontents,
+  };
 }
 
 export function getComponents() {
-    return {
-        atoms: getComponentsByType('atoms', require('../../component-lib/src/atoms/**/*.jsx')),
-        molecules: getComponentsByType('molecules', require('../../component-lib/src/molecules/**/*.jsx')),
-        organisms: getComponentsByType('organisms', require('../../component-lib/src/organisms/**/*.jsx'))
-    };
+  return {
+    atoms: getComponentsByType('atoms', require('../../component-lib/src/atoms/**/*.jsx')),
+    molecules: getComponentsByType('molecules', require('../../component-lib/src/molecules/**/*.jsx')),
+    organisms: getComponentsByType('organisms', require('../../component-lib/src/organisms/**/*.jsx')),
+  };
 }
 
 export function getComponentsByCategory() {
-    const allComponents = require('../../component-lib/src/**/**/*.jsx');
+  const allComponents = require('../../component-lib/src/**/**/*.jsx');
 
-    const matchesCategory = (type, componentName, category) =>
-        getComponentCategory(type, componentName).toLowerCase() === category.toLowerCase();
+  const matchesCategory = (type, componentName, category) =>
+    getComponentCategory(type, componentName).toLowerCase() === category.toLowerCase();
 
-    const matchesNoCategory = (type, componentName) =>
-        componentCategories.filter(category => matchesCategory(type, componentName, category.key)).length === 0;
+  const matchesNoCategory = (type, componentName) =>
+    componentCategories.filter(category => matchesCategory(type, componentName, category.key)).length === 0;
 
-    const componentsByCategory = componentCategories
-        .reduce((result, category) => ({
-            ...result,
-            [category.key]: getComponentsByPredicate(
-                (type, componentName) => matchesCategory(type, componentName, category.key),
-                allComponents,
-                category.name)
-        }), {});
-
-    const uncategorizedComponents = getComponentsByPredicate(
-        matchesNoCategory,
+  const componentsByCategory = componentCategories.reduce(
+    (result, category) => ({
+      ...result,
+      [category.key]: getComponentsByPredicate(
+        (type, componentName) => matchesCategory(type, componentName, category.key),
         allComponents,
-        'Without category');
+        category.name
+      ),
+    }),
+    {}
+  );
 
-    return {
-        ...componentsByCategory,
-        misc: uncategorizedComponents,
-    };
+  const uncategorizedComponents = getComponentsByPredicate(matchesNoCategory, allComponents, 'Without category');
+
+  return {
+    ...componentsByCategory,
+    misc: uncategorizedComponents,
+  };
 }
 
 export function isFullWidthComponent(componentName) {
-    const fullWidthComponents = [
-        // Atoms
-        'Container',
-        'FullWidthImage',
-        'PagePebbles',
+  const fullWidthComponents = [
+    // Atoms
+    'Container',
+    'FullWidthImage',
+    'PagePebbles',
 
-        // Molecules
-        'Alert',
-        'ArticleMetaData',
-        'BoxGrid',
-        'FactBox',
-        'FunkyTabs',
-        'Header',
-        'HeaderWithImage',
-        'Hero',
-        'PageHeader',
-        'PageFooter',
-        'PopUpLine',
-        'RecommendedProducts',
-        'RelatedArticles',
-        'MiniDashboard',
-        'Menu',
-        'DisplayFunctionality',
-        'Cards',
-        'Gallery',
+    // Molecules
+    'Alert',
+    'ArticleMetaData',
+    'BoxGrid',
+    'FactBox',
+    'FunkyTabs',
+    'Header',
+    'HeaderWithImage',
+    'Hero',
+    'PageHeader',
+    'PageFooter',
+    'PopUpLine',
+    'RecommendedProducts',
+    'RelatedArticles',
+    'MiniDashboard',
+    'Menu',
+    'DisplayFunctionality',
+    'Cards',
+    'Gallery',
 
-        // Organisms
-        'FocusSubscription',
-        'ThemeBoxes',
-        'ThemeBoxesWithImage',
-        'ProductListGrid',
-        'FeatureBoxes'
-    ];
-    return _.includes(fullWidthComponents, componentName);
+    // Organisms
+    'FocusSubscription',
+    'ThemeBoxes',
+    'ThemeBoxesWithImage',
+    'ProductListGrid',
+    'FeatureBoxes',
+  ];
+  return _.includes(fullWidthComponents, componentName);
 }
