d0a1de4ab1b4663d3c8e4e91681919aa5e6d76e1
Nils Magnus Englund <nme@stepone.no>
2019-11-20 14:56:22 +0100

    Run prettier on all .ts,.tsx files

--- component-lib/stories/home/HomePage.tsx
+++ component-lib/stories/home/HomePage.tsx
@@ -1,31 +1,30 @@
-import _ from "lodash";
-import React from "react";
+import _ from 'lodash';
+import React from 'react';
 
-import Heading from "../../src/atoms/Heading/Heading";
-import Header from "../../src/molecules/Header/Header";
-import Color from "../lib/Color";
+import Heading from '../../src/atoms/Heading/Heading';
+import Header from '../../src/molecules/Header/Header';
+import Color from '../lib/Color';
 
-import colors from "../colors";
+import colors from '../colors';
 
 const HomePage = () => (
-    <div>
-        <Header
-            iconName="ico_code-2"
-            runningTitle="Greetings, Earthlings!"
-            pageTitle="Telia Norge Styleguide"
-            withMask={true}
-        >
-            <p>
-                This styleguide offers color schemes, components (both plain
-                HTML and React) and CSS to be used in Telia Norway's digital
-                channels.
-            </p>
-            <p>
-                Talk with us in the #styleguide Slack channel and let us know if
-                and how you intend to use the styleguide. Enjoy :)
-            </p>
-        </Header>
-    </div>
+  <div>
+    <Header
+      iconName="ico_code-2"
+      runningTitle="Greetings, Earthlings!"
+      pageTitle="Telia Norge Styleguide"
+      withMask={true}
+    >
+      <p>
+        This styleguide offers color schemes, components (both plain HTML and React) and CSS to be used in Telia
+        Norway's digital channels.
+      </p>
+      <p>
+        Talk with us in the #styleguide Slack channel and let us know if and how you intend to use the styleguide. Enjoy
+        :)
+      </p>
+    </Header>
+  </div>
 );
 
 export default HomePage;
--- component-lib/stories/index.stories.tsx
+++ component-lib/stories/index.stories.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react';
 
-import { storiesOf } from "@storybook/react";
+import { storiesOf } from '@storybook/react';
 
 /**
  * We can import the .pcss file directly here thanks to the .pcss
  * loader in the storybook-specific .storybook/webpack.config.js.
  */
-import "../src/index.pcss";
-import "../src/business.pcss";
+import '../src/index.pcss';
+import '../src/business.pcss';
 
-import HomePage from "./home/HomePage";
-import ColorPalette from "./home/ColorPalette";
-import Typography from "./home/Typography";
+import HomePage from './home/HomePage';
+import ColorPalette from './home/ColorPalette';
+import Typography from './home/Typography';
 
-storiesOf("Introduction|Home", module)
-    .add("Greetings", () => <HomePage />)
-    .add("Color palette", () => <ColorPalette />)
-    .add("Typography", () => <Typography />);
+storiesOf('Introduction|Home', module)
+  .add('Greetings', () => <HomePage />)
+  .add('Color palette', () => <ColorPalette />)
+  .add('Typography', () => <Typography />);
