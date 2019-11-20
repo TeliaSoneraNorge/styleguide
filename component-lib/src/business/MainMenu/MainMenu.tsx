8480e84cc1c5933a079e004b5b116627bb60c9ea
Nils Magnus Englund <nme@stepone.no>
2019-11-20 16:38:52 +0100

    Run prettier on MainMenu files

--- component-lib/src/business/MainMenu/MainMenu.tsx
+++ component-lib/src/business/MainMenu/MainMenu.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import cs from "classnames";
+import React from 'react';
+import cs from 'classnames';
 
-import { MainMenuItemProps } from "./MainMenuItem";
+import { MainMenuItemProps } from './MainMenuItem';
 
 interface Props {
-    className?: string;
-    children?: Array<React.ReactElement<MainMenuItemProps>>;
+  className?: string;
+  children?: Array<React.ReactElement<MainMenuItemProps>>;
 }
 
 export const MainMenu: React.FC<Props> = props => (
-    <nav className={cs("Business-MainMenu", props.className)}>
-        <ul className="Business-MainMenu-list">{props.children}</ul>
-    </nav>
+  <nav className={cs('Business-MainMenu', props.className)}>
+    <ul className="Business-MainMenu-list">{props.children}</ul>
+  </nav>
 );
