8158594690d45d15356b1817676b36862058e6a2
Nils Magnus Englund <nme@stepone.no>
2019-11-20 15:06:09 +0100

    Run prettier on component-lib scripts, config, utils

--- component-lib/scripts/copy-type-files.js
+++ component-lib/scripts/copy-type-files.js
@@ -13,37 +13,31 @@ const fse = require('fs-extra');
 const glob = require('glob');
 
 async function filesAreEqual(src, dst) {
-    const b1 = await fse.readFile(src);
-    const b2 = await fse.readFile(dst);
-    return b1.compare(b2) === 0;
+  const b1 = await fse.readFile(src);
+  const b2 = await fse.readFile(dst);
+  return b1.compare(b2) === 0;
 }
 
 async function run() {
-    const typeFiles = glob.sync('**/*.d.ts', {
-        cwd: path.resolve(process.cwd(), 'src')
-    });
+  const typeFiles = glob.sync('**/*.d.ts', {
+    cwd: path.resolve(process.cwd(), 'src'),
+  });
 
-    await Promise.all(
-        typeFiles.map(async file => {
-            const src = path.resolve(__dirname, '../src', file);
-            const dst = path.resolve(__dirname, '../dist', file);
-            if ((await fse.exists(dst)) && !(await filesAreEqual(src, dst))) {
-                console.error(
-                    `ERROR: dist/${file} already exists in another shape -- aborting.`
-                );
-                console.log(
-                    `       This can be due to tsc emitting a file with the same`
-                );
-                console.log(
-                    `       name as a file already defined in src/${file}.`
-                );
-                process.exit(1);
-            } else {
-                await fse.copy(src, dst);
-                console.log(`Copied src/${file} to dist/${file}`);
-            }
-        })
-    );
+  await Promise.all(
+    typeFiles.map(async file => {
+      const src = path.resolve(__dirname, '../src', file);
+      const dst = path.resolve(__dirname, '../dist', file);
+      if ((await fse.exists(dst)) && !(await filesAreEqual(src, dst))) {
+        console.error(`ERROR: dist/${file} already exists in another shape -- aborting.`);
+        console.log(`       This can be due to tsc emitting a file with the same`);
+        console.log(`       name as a file already defined in src/${file}.`);
+        process.exit(1);
+      } else {
+        await fse.copy(src, dst);
+        console.log(`Copied src/${file} to dist/${file}`);
+      }
+    })
+  );
 }
 
 run();
