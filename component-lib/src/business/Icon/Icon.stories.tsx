8412f1f76c6c78bebedf69667a822413841da850
Nils Magnus Englund <nme@stepone.no>
2019-11-29 10:37:18 +0100

    Add <Icon/> to business area (#469)
    * Added `<Icon/>` component to business area
    * Added missing generator script for AllBusinessIcons
    * Update Icon CSS, set prefix to Business-Icon--
    * Add color setting for all icons in storybook
    * Replaced signal icon
    * Icon stories: Run prettier, refactored icon into a map to fix select() bug
    * Run prettier on Icon files
    * Added script to add and optimise business icons
    * Ran existing business icons through new pipeline
    * Added alarm-off
    * Replace all icons
    * Updated generator script to create IconDefinition, availableIcons
    * Regenerate icon files
    * Use generated icon list in Storybook
    * Add currentColor to stroke

---
+++ component-lib/src/business/Icon/Icon.stories.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { withKnobs, color, select } from '@storybook/addon-knobs';
+
+import { AllBusinessIcons } from './AllBusinessIcons';
+import { Icon } from './Icon';
+import { IconDefinition } from './IconDefinition';
+import { availableIcons } from './availableIcons';
+
+const stories = storiesOf('Business|Icon', module);
+
+stories.addDecorator(withKnobs);
+
+stories.add('Icon', () => (
+  <div style={{ padding: '24px' }}>
+    <AllBusinessIcons />
+
+    <h1>Icon</h1>
+
+    <p>
+      The component <code>{'<AllBusinessIcons />'}</code> (which itself is invisible) must be present in order for the
+      SVGs to be available to the <code>{'<Icon />'}</code> component.
+    </p>
+
+    <p>
+      The icon inherits its color from the surrounding element (<code>{'fill: currentColor;'}</code>).
+    </p>
+
+    <h2>Sample icon</h2>
+
+    <div
+      style={{
+        transition: 'color 0.15s ease-in-out',
+        color: color('Color', 'red'),
+      }}
+    >
+      <Icon icon={select('Sample icon', availableIcons, 'ai-robot') as IconDefinition} />
+    </div>
+
+    <h2>All icons</h2>
+
+    <ul style={{ listStyleType: 'none', margin: '0', padding: '0' }}>
+      {availableIcons.map((key: IconDefinition) => (
+        <li
+          key={key}
+          style={{
+            padding: '8px',
+            display: 'inline-block',
+            color: color('Color for all icons', 'red'),
+          }}
+          title={key}
+        >
+          <Icon style={{ width: '32px', height: '32px' }} icon={key} />
+        </li>
+      ))}
+    </ul>
+  </div>
+));
--- component-lib/src/business/index.tsx
+++ component-lib/src/business/index.tsx
@@ -1,1 +1,2 @@
+export { Icon, IconDefinition, AllBusinessIcons } from './Icon';
 export { Toggle } from './Toggle';
