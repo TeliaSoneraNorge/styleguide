73b3625e14271f2869335cfe0cabe041817722ad
Joakim Kristiansen <joakikr@gmail.com>
2019-12-04 14:45:36 +0100

    Add AllIcons to stories

--- component-lib/.storybook/config.tsx
+++ component-lib/.storybook/config.tsx
@@ -1,12 +1,20 @@
+import React from 'react';
 import { addParameters, addDecorator, configure, setAddon } from '@storybook/react';
 import { withKnobs } from "@storybook/addon-knobs";
 import teliaTheme from './teliaTheme';
 import JSXAddon from 'storybook-addon-jsx';
+import AllIcons from '../assets/allicons/AllIcons';
 
 setAddon(JSXAddon);
 
 // Default decorators
 addDecorator(withKnobs);
+addDecorator(s => (
+  <>
+    <AllIcons />
+    {s()}
+  </>
+));
 
 addParameters({
   options: {
