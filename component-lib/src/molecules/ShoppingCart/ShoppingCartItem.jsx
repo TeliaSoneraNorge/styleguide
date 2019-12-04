322e1b708578fe138b744a6c2c199660576135af
alfredbirk <alfredbirk@gmail.com>
2019-12-04 15:26:29 +0100

    Fix isAnyCartItemsRemovable (#498)

--- component-lib/src/molecules/ShoppingCart/ShoppingCartItem.jsx
+++ component-lib/src/molecules/ShoppingCart/ShoppingCartItem.jsx
@@ -68,7 +68,7 @@ const ShoppingCartItem = ({
   const { id, leaseMonths, name, subtitle, price, discount, type, indent } = cartItem;
   const quantity = _.get(cartItem, 'quantity.value', 1);
   const isQuantityModifiable = _.get(cartItem, 'quantity.modifiable');
-  const isRemovable = _.get(cartItem, 'quantity.removable');
+  const isRemovable = _.get(cartItem, 'quantity.removable') || isAnyCartItemsRemovable;
   const shouldShowPricePerUnit = (!!price.upfront || !!price.firstInvoice) && quantity > 1;
   const discountValueUpfront = _.get(discount, 'value.upfront', 0);
   const discountValueMonthly = _.get(discount, 'value.monthly', 0);
@@ -152,7 +152,7 @@ const ShoppingCartItem = ({
         )}
       </ShoppingCartCell>
       <ShoppingCartCell className="shopping-cart__item__delete">
-        {isRemovable && !hasPaid && isAnyCartItemsRemovable && (
+        {isRemovable && !hasPaid && (
           <button className="button button--small shopping-cart__item__button" onClick={() => onRemoveItem(cartItem)}>
             <SvgIcon
               className="shopping-cart__item__button__icon"
