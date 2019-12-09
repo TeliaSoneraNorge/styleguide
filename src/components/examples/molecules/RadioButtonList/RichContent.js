24c17c02f5c6ac476f1a1cfd5c88b92330d82054
Gaute Tessand Baalsrud <gaute.baalsrud@bouvet.no>
2019-12-09 15:48:04 +0100

    initial radiobutton with label fix and styling

--- src/components/examples/molecules/RadioButtonList/RichContent.js
+++ src/components/examples/molecules/RadioButtonList/RichContent.js
@@ -1,32 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { RadioButtonList, RadioButtonWithLabel, Heading, Paragraph } from '@telia/styleguide';
 
-const RichContentRadioButtonList = () => (
-  <RadioButtonList type="vertical" hasRichContent>
-    <RadioButtonWithLabel checked hasRichContent value="0" name="radiobutton-test-3" onChange={() => {}}>
-      <div style={{ display: 'inline-block', width: '80%' }}>
-        <Heading level="3" text="Option 1" />
+const RichContentRadioButtonList = () => {
+  const [value, setValue] = useState(0);
+
+  const onClick = e => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <RadioButtonList type="vertical" hasRichContent onChange={onClick}>
+      <RadioButtonWithLabel checked={value == 0} hasRichContent value="0" name="radiobutton-test-3" onChange={onClick}>
+        <div style={{ display: 'inline-block', width: '80%' }}>
+          <Heading level="3" text="Option 1" />
+          <Paragraph>
+            A longer description text here. Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat, accumsan
+            id imperdiet et, porttitor at sem.
+          </Paragraph>
+          <Paragraph kind="fineprint">A fineprint text here</Paragraph>
+        </div>
+        <img
+          style={{ display: 'inline-block', float: 'right', width: '20%' }}
+          src={require('assets/website-images/zens-charger.png')}
+        />
+      </RadioButtonWithLabel>
+      <RadioButtonWithLabel checked={value == 1} hasRichContent value="1" name="radiobutton-test-3" onChange={onClick}>
+        <Heading level="3" text="Option 2" />
         <Paragraph>
-          A longer description text here. Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat, accumsan id
-          imperdiet et, porttitor at sem.
+          Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
         </Paragraph>
-        <Paragraph kind="fineprint">A fineprint text here</Paragraph>
-      </div>
-      <img
-        style={{ display: 'inline-block', float: 'right', width: '20%' }}
-        src={require('assets/website-images/zens-charger.png')}
-      />
-    </RadioButtonWithLabel>
-    <RadioButtonWithLabel hasRichContent value="1" name="radiobutton-test-3">
-      <Heading level="3" text="Option 2" />
-      <Paragraph>
-        Nulla quis lorem ut libero malesuada feugiat. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
-      </Paragraph>
-    </RadioButtonWithLabel>
-    <RadioButtonWithLabel hasRichContent value="2" name="radiobutton-test-3">
-      <Heading level="3" text="Option 3" />
-    </RadioButtonWithLabel>
-  </RadioButtonList>
-);
+      </RadioButtonWithLabel>
+      <RadioButtonWithLabel checked={value == 2} hasRichContent value="2" name="radiobutton-test-3" onChange={onClick}>
+        <Heading level="3" text="Option 3" />
+      </RadioButtonWithLabel>
+    </RadioButtonList>
+  );
+};
 
 export default RichContentRadioButtonList;
