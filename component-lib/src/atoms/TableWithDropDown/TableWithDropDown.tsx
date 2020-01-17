55d15fd2a28ceb44d94201acaa49f62818b1cabd
Lars Erik Skraastad <lars.erik.skraastad@get.no>
2020-01-17 13:30:27 +0100

    add status comment

--- component-lib/src/atoms/TableWithDropDown/TableWithDropDown.tsx
+++ component-lib/src/atoms/TableWithDropDown/TableWithDropDown.tsx
@@ -2,16 +2,10 @@ import React, { ReactNode, useState } from 'react';
 import classnames from 'classnames';
 import SvgIcon from '../SvgIcon';
 /**
- * Status: *work in progress*
- * Category: Tables
- *
- * This table component use negative margins to achieve full width rows in the table,
- * and padding to add back the negative margin value. To avoid horizontal scrolling
- * it is important to style the nearest full width element with <code>overflow-x: hidden;</code>.
- *
- * It's also recommended to use this component within an element with a styling that centers-, and
- * sets the content to a max-width.
+ * Status: *Finish*
+ * Category: Table with expandable rows
  */
+
 interface Props {
     className?: string;
     children: ReactNode;
