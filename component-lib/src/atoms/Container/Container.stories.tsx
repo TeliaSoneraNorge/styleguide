782762fce103bfb3d575846a719f9125313d5b73
bogsti <christoffer.bogsti@telia.no>
2020-01-27 14:27:34 +0100

    Container

---
+++ component-lib/src/atoms/Container/Container.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Container from './Container';
+
+
+export default {
+  title: 'Component library|Atoms/Container',
+  // component: Container,
+};
+
+
+
+export const DefaultContainer = () => {
+
+  const exampleStyles = {
+    backgroundColor: 'rgb(153, 10, 227)',
+    color: 'rgb(255, 255, 255)',
+    paddingTop: '1rem',
+  };
+  
+  return (
+    <>
+      <Container size="small" style={exampleStyles}>
+        This is some content inside a small block.
+      </Container>
+      <Container size="medium" style={exampleStyles}>
+        This is some content inside a medium block.
+      </Container>
+      <Container size="large" style={exampleStyles}>
+        This is some content inside a large block.
+      </Container>
+    </>
+  )
+};
