8571a1fb53294d65d730004749df957682914a62
bogsti <christoffer.bogsti@telia.no>
2020-01-27 13:56:11 +0100

    AlertText

---
+++ component-lib/src/atoms/AlertText/AlertText.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import AlertText from './AlertText';
+
+export default {
+  title: 'Component library|Atoms/Alert Text',
+  component: AlertText,
+};
+
+export const Default = () => <AlertText>Chat is closed</AlertText>;
