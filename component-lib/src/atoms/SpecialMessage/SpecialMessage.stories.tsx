3aee81c51be4a45651b7487184f06481f8a6f28d
bogsti <christoffer.bogsti@telia.no>
2020-01-28 13:59:49 +0100

    SpecialMessage

---
+++ component-lib/src/atoms/SpecialMessage/SpecialMessage.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SpecialMessage from './SpecialMessage';
+
+export default {
+  title: 'Component library|Atoms/Special Message',
+  component: SpecialMessage,
+};
+
+export const Default = () => (
+
+  <SpecialMessage>
+    This is a small block of text that should be used to highlight some important content on the page.
+  </SpecialMessage>
+  
+);
