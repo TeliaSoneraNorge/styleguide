22ca46a69ab9867d9e7a6f3e58f06f2ae8c9f2f7
bogsti <christoffer.bogsti@telia.no>
2020-01-28 13:37:36 +0100

    IconLink

---
+++ component-lib/src/atoms/IconLink/IconLink.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import IconLink from './IconLink';
+
+
+export default {
+  title: 'Component library|Atoms/Icon Link',
+  component: IconLink,
+};
+
+export const IconLinks = () => (
+
+  <div>
+    <IconLink iconName="ico_download" href="#" />
+    <IconLink iconName="linkedin_icon" href="#" />
+    <IconLink iconName="twitter_icon" href="#" />
+  </div>
+  
+);
