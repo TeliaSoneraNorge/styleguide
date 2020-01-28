b6257b016e7073d8d6121e0f9474c63ef45def8a
bogsti <christoffer.bogsti@telia.no>
2020-01-28 15:00:06 +0100

    Tags

---
+++ component-lib/src/atoms/Tags/Tags.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Tags from './Tags';
+
+export default {
+  title: 'Component library|Atoms/Tags',
+  component: Tags,
+};
+
+export const Default = () => {
+
+  const { Tag } = Tags;
+  
+  return (
+    <Tags>
+      <Tag>Technology</Tag>
+      <Tag>Music</Tag>
+      <Tag>Streaming</Tag>
+    </Tags>
+  )
+
+};
+
+export const Color = () => {
+
+  const { Tag } = Tags;
+  
+  return (
+    <Tags color="dark-grey">
+      <Tag>Technology</Tag>
+      <Tag>Music</Tag>
+      <Tag>Streaming</Tag>
+    </Tags>
+  )
+  
+};
