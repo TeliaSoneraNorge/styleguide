21d9b599c92ff15b9c155c4de56d908a96f38a25
bogsti <christoffer.bogsti@telia.no>
2020-01-28 14:08:19 +0100

    tepIndicator

---
+++ component-lib/src/atoms/StepIndicator/StepIndicator.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import StepIndicator from './StepIndicator';
+
+export default {
+  title: 'Component library|Atoms/Step Indicator',
+  component: StepIndicator,
+};
+
+
+
+export const StepIndicators = () => {
+
+  const wrapperStyle = {
+    width: '700px',
+    display: 'flex',
+    justifyContent: 'flexStart',
+  };
+  
+  return (
+    <div>
+      <div style={wrapperStyle}>
+        <StepIndicator
+          index={0}
+          numberOfSteps={3}
+          labels={['Nummer', 'Se over', 'Ferdig']}
+          links={['#StepIndicatorTest', '#StepIndicatorTest1', '#StepIndicatorTest2']}
+        />
+      </div>
+      <div style={wrapperStyle}>
+        <StepIndicator
+          index={2}
+          numberOfSteps={5}
+          labels={['One', 'Two', 'Three', 'Four', 'Five']}
+          links={[
+            '#StepIndicatorTest',
+            '#StepIndicatorTest1',
+            '#StepIndicatorTest2',
+            '#StepIndicatorTest3',
+            '#StepIndicatorTest4',
+          ]}
+        />
+      </div>
+    </div>
+  )
+
+};
