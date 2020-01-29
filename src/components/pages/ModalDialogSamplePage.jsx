f37219af303212184edb68ae29c84429b4946730
Marek Magdziak <marek.magdziak@hotmail.cz>
2020-01-29 09:59:14 +0100

    Fix: ability to scroll the dalog window (#528)

--- src/components/pages/ModalDialogSamplePage.jsx
+++ src/components/pages/ModalDialogSamplePage.jsx
@@ -2,6 +2,39 @@ import React from 'react';
 
 import { Header, ModalDialog } from '../../../component-lib/src/index';
 
+const LongText = () => (
+  <>
+    <p>
+      Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat. Sed porttitor
+      lectus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit
+      neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus. Praesent sapien massa,
+      convallis a pellentesque nec, egestas non nisi. Proin eget tortor risus. Cras ultricies ligula sed magna dictum
+      porta. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Proin eget tortor risus.
+    </p>
+    <p>
+      Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat. Sed porttitor
+      lectus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit
+      neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus. Praesent sapien massa,
+      convallis a pellentesque nec, egestas non nisi. Proin eget tortor risus. Cras ultricies ligula sed magna dictum
+      porta. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Proin eget tortor risus.
+    </p>
+    <p>
+      Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat. Sed porttitor
+      lectus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit
+      neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus. Praesent sapien massa,
+      convallis a pellentesque nec, egestas non nisi. Proin eget tortor risus. Cras ultricies ligula sed magna dictum
+      porta. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Proin eget tortor risus.
+    </p>
+    <p>
+      Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat. Sed porttitor
+      lectus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit
+      neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus. Praesent sapien massa,
+      convallis a pellentesque nec, egestas non nisi. Proin eget tortor risus. Cras ultricies ligula sed magna dictum
+      porta. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Proin eget tortor risus.
+    </p>
+  </>
+);
+
 export default class ModalDialogSamplePage extends React.Component {
   constructor(props) {
     super(props);
@@ -10,11 +43,16 @@ export default class ModalDialogSamplePage extends React.Component {
       infoDialogActive: false,
       confirmDialogActive: false,
       nonClosableDialogActive: false,
+      toggleLongTextDialogActive: false,
     };
 
     this.toggleInfoModalDialog = this.toggleInfoModalDialog.bind(this);
     this.toggleConfirmModalDialog = this.toggleConfirmModalDialog.bind(this);
     this.toggleNonClosableDialog = this.toggleNonClosableDialog.bind(this);
+    this.toggleLongTextDialogActive = this.toggleLongTextDialogActive.bind(this);
+  }
+  toggleLongTextDialogActive() {
+    this.setState({ longTextDialogActive: !this.state.longTextDialogActive });
   }
   toggleInfoModalDialog() {
     this.setState({ infoDialogActive: !this.state.infoDialogActive });
@@ -30,38 +68,7 @@ export default class ModalDialogSamplePage extends React.Component {
       <article>
         <Header iconName="ico_info" pageTitle="Modal Dialog Sample" withMask={false} withContentOverlap={false}>
           <p>See how a modal dialog works by clicking the button below!</p>
-          <p>
-            Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat. Sed
-            porttitor lectus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-            Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus.
-            Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Proin eget tortor risus. Cras
-            ultricies ligula sed magna dictum porta. Vivamus magna justo, lacinia eget consectetur sed, convallis at
-            tellus. Proin eget tortor risus.
-          </p>
-          <p>
-            Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat. Sed
-            porttitor lectus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-            Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus.
-            Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Proin eget tortor risus. Cras
-            ultricies ligula sed magna dictum porta. Vivamus magna justo, lacinia eget consectetur sed, convallis at
-            tellus. Proin eget tortor risus.
-          </p>
-          <p>
-            Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat. Sed
-            porttitor lectus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-            Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus.
-            Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Proin eget tortor risus. Cras
-            ultricies ligula sed magna dictum porta. Vivamus magna justo, lacinia eget consectetur sed, convallis at
-            tellus. Proin eget tortor risus.
-          </p>
-          <p>
-            Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat. Sed
-            porttitor lectus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-            Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus.
-            Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Proin eget tortor risus. Cras
-            ultricies ligula sed magna dictum porta. Vivamus magna justo, lacinia eget consectetur sed, convallis at
-            tellus. Proin eget tortor risus.
-          </p>
+          <LongText />
         </Header>
         <section className="container container--small" style={{ textAlign: 'center' }}>
           <button className="button button-default button--margin-top" onClick={this.toggleInfoModalDialog}>
@@ -105,6 +112,20 @@ export default class ModalDialogSamplePage extends React.Component {
               disableCloseByEscKey
             >
               <p>This dialog can&apos;t be closed by escape key or clickout</p>
+            </ModalDialog>
+          )}
+          <button className="button button-default button--margin-top" onClick={this.toggleLongTextDialogActive}>
+            Dialog with very long text
+          </button>
+          {this.state.longTextDialogActive && (
+            <ModalDialog
+              name="nonclosable-dialog"
+              heading="Info dialog"
+              onSubmit={this.toggleLongTextDialogActive}
+              submitText="Ok"
+            >
+              <LongText />
+              <LongText />
             </ModalDialog>
           )}
         </section>
