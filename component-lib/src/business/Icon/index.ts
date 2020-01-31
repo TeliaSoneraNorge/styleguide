4bca7b8ce39e8cba767a0d73da21734d2d355caf
Nils Magnus Englund <nme@stepone.no>
2020-01-31 15:23:22 +0100

    Remove Icon from business area

--- component-lib/src/business/Icon/index.ts
+++
@@ -1,4 +0,0 @@
-export { Icon } from './Icon';
-export { IconDefinition } from './IconDefinition';
-export { availableIcons } from './availableIcons';
-export { AllBusinessIcons } from './AllBusinessIcons';
