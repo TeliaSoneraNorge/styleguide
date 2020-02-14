3f181cb0127e064ac2e7911e36008a00e4b781bf
Nils Magnus Englund <nme@stepone.no>
2020-02-14 11:13:20 +0100

    Ran prettier on IconLink stories, added IconLink component reference

--- component-lib/src/atoms/IconLink/IconLink.stories.tsx
+++ component-lib/src/atoms/IconLink/IconLink.stories.tsx
@@ -2,33 +2,27 @@ import React from 'react';
 import IconLink from './IconLink';
 
 export default {
-  title: 'Component library|Atoms/Icon Link',
+  title: 'Component library|Atoms/IconLink',
+  component: IconLink,
 };
 
 export const Default = () => (
   <>
-
     <p>Default Link Icons</p>
     <IconLink icon="alarm" href="#" />
-
 
     <p>Black Link Icons</p>
     <IconLink icon="download" iconColor="black" href="#" />
 
-
     <p>Core Purple Link Icon</p>
     <IconLink icon="place" iconColor="core-purple" href="#" />
 
-
     <p>White Link Icons on Background</p>
     <div style={{ background: '#380354', padding: '1rem', display: 'inline-block' }}>
-
       <IconLink icon="twitter" iconColor="white" href="#" />
       <IconLink icon="like" iconColor="white" href="#" />
       <IconLink icon="heart-filled" iconColor="white" href="#" />
-
     </div>
-
   </>
 );
 
@@ -38,4 +32,4 @@ export const Deprecated = () => (
     <IconLink iconName="linkedin_icon" href="#" />
     <IconLink iconName="twitter_icon" href="#" />
   </>
-)
+);
