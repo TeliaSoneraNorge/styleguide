f5338711a67d70494c31eaff3c417e35e6ee2fbd
Nils Magnus Englund <nme@stepone.no>
2020-02-14 11:13:00 +0100

    Refactored IconLink a bit

--- component-lib/src/atoms/IconLink/IconLink.tsx
+++ component-lib/src/atoms/IconLink/IconLink.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
-import classnames from 'classnames';
+import React, { CSSProperties } from 'react';
+import cs from 'classnames';
+
 import SvgIcon from '../../atoms/SvgIcon/SvgIcon';
 import upperFirst from 'lodash/upperFirst';
 
 import { Icon } from '../Icon';
 import { IconDefinition } from '../Icon/Icon.generated';
 
-interface IconLinkProps {
-  className?: any;
+export interface IconLinkProps {
+  /**
+   * Any additional CSS classes.
+   */
+  className?: string;
+
+  /**
+   * Any additional CSS properties.
+   */
+  style?: CSSProperties;
+
+  /**
+   * The icon to show. Must be a valid `IconDefinition`.
+   */
   icon?: IconDefinition;
+
+  /**
+   * @deprecated Use `icon` prop to use the new icons.
+   */
   iconName?: string;
+
+  /**
+   * The icon color. Also affects state colors (e.g. _hover_).
+   */
   iconColor?: 'black' | 'core-purple' | 'white';
+
   href: string;
 }
 
@@ -18,33 +40,21 @@ interface IconLinkProps {
  * Status: *finished*.
  * Category: Links
  */
-const IconLink: React.FC<IconLinkProps> = ({ className, iconName, icon, iconColor, ...rest }) => (
-  <>
-    {icon && (
-      <a
-        className={classnames('icon-link', {
-          [className]: className,
-        })}
-        aria-label={upperFirst(icon)}
-        {...rest}
-      >
-        <Icon icon={icon} className={`icon-link__${iconColor ? iconColor : 'black'}`} />
-      </a>
-    )}
-
-    {/* DEPRECATED */}
-    {iconName && (
-      <a
-        className={classnames('icon-link-deprecated', {
-          [className]: className,
-        })}
-        aria-label={upperFirst(iconName)}
-        {...rest}
-      >
+const IconLink: React.FC<IconLinkProps> = ({ className, style, iconName, icon, iconColor, ...rest }) => {
+  // Deprecated, old iconName style
+  if (iconName) {
+    return (
+      <a className={cs('icon-link-deprecated', className)} aria-label={upperFirst(iconName)} {...rest}>
         <SvgIcon iconName={iconName} color="black" className="icon-link-deprecated__icon" />
       </a>
-    )}
-  </>
-);
+    );
+  }
+
+  return (
+    <a className={cs('icon-link', className)} aria-label={upperFirst(icon)} {...rest}>
+      <Icon icon={icon} className={`icon-link__${iconColor ? iconColor : 'black'}`} />
+    </a>
+  );
+};
 
 export default IconLink;
