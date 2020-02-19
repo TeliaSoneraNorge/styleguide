deecdcfda679726e109612b6b595301446ba0637
Nils Magnus Englund <nme@stepone.no>
2020-02-19 14:21:14 +0100

    Refactor definition of colors and color aliases

--- component-lib/src/utils/colors.ts
+++ component-lib/src/utils/colors.ts
@@ -1,135 +1,73 @@
-interface colors {
-  corePurple100: '#F9EEFE';
-  corePurple200: '#F2DBFD';
-  corePurple300: '#C86AF8';
-  corePurple400: '#AF3CE8';
-  corePurple500: '#990AE3';
-  corePurple600: '#7907B3';
-  corePurple700: '#570580';
-  corePurple800: '#380354';
-  corePurple900: '#1F012F'; 
-
-  grey100: '#FBFBFB';
-  grey200: '#F2F2F2';
-  grey300: '#CCCCCC';
-  grey400: '#A0A0A0';
-  grey500: '#6E6E6E';
-  grey600: '#5E5E5E';
-  grey700: '#4E4E4E';
-  grey800: '#303030';
-  grey900: '#222222'; 
-
-  green100: '#F3FFF9';
-  green200: '#D9FFEB';
-  green300: '#40FF9F';
-  green400: '#00F37A';
-  green500: '#00D066';
-  green600: '#00A552';
-
-  red100: '#FFF5F9';
-  red200: '#FFE0EB';
-  red300: '#FF6699';
-  red400: '#FF3377';
-  red500: '#E4175C';
-  red600: '#BF134D';
-
-  orange100: '#FFF7EB';
-  orange200: '#FFEDD0';
-  orange300: '#FFD06B';
-  orange400: '#FFB84D';
-  orange500: '#FF9B00';
-  orange600: '#DD8804';
-
-  blue100: '#EDF8FF';
-  blue200: '#D4EEFF';
-  blue300: '#73C7FF';
-  blue400: '#40B2FF';
-  blue500: '#0099FF';
-  blue600: '#0082D9';
-}
-
-interface alias {
-  corePurple: colors["corePurple500"];
-  deepPurple: colors["corePurple800"];
-  blackPurple: colors["corePurple900"];
-
-  lightGrey: colors["grey200"];
-  darkGrey: colors["grey400"];
-  black: colors["grey900"];
-
-  green: colors["green500"];
-  red: colors["red500"];
-  orange: colors["orange500"];
-  blue: colors["blue500"];
-}
-
-export type colorTypes =  colors & alias;
-
-
-
-export const colors: colorTypes = {
-  
-  // PURPLE
-  corePurple100: '#F9EEFE',
-  corePurple200: '#F2DBFD',
-  corePurple300: '#C86AF8',
-  corePurple400: '#AF3CE8',
-  corePurple500: '#990AE3',
-  corePurple600: '#7907B3',
+export const colorDefinitions = {
+  // Purple
+  corePurple100: '#f9eefe',
+  corePurple200: '#f2dbfd',
+  corePurple300: '#c86af8',
+  corePurple400: '#af3ce8',
+  corePurple500: '#990ae3',
+  corePurple600: '#7907b3',
   corePurple700: '#570580',
   corePurple800: '#380354',
-  corePurple900: '#1F012F',
-  corePurple: '#990AE3',
-  deepPurple: '#380354',
-  blackPurple: '#1F012F',
+  corePurple900: '#1f012f',
 
-  // GREY
-  grey100: '#FBFBFB',
-  grey200: '#F2F2F2',
-  grey300: '#CCCCCC',
-  grey400: '#A0A0A0',
-  grey500: '#6E6E6E',
-  grey600: '#5E5E5E',
-  grey700: '#4E4E4E',
+  // Grey
+  grey100: '#fbfbfb',
+  grey200: '#f2f2f2',
+  grey300: '#cccccc',
+  grey400: '#a0a0a0',
+  grey500: '#6e6e6e',
+  grey600: '#5e5e5e',
+  grey700: '#4e4e4e',
   grey800: '#303030',
   grey900: '#222222',
-  lightGrey: '#F2F2F2',
-  darkGrey: '#A0A0A0',
-  black: '#222222',
 
-  // GREEN
-  green100: '#F3FFF9',
-  green200: '#D9FFEB',
-  green300: '#40FF9F',
-  green400: '#00F37A',
-  green500: '#00D066',
-  green600: '#00A552',
-  green: '#00D066',
+  // Green
+  green100: '#f3fff9',
+  green200: '#d9ffeb',
+  green300: '#40ff9f',
+  green400: '#00f37a',
+  green500: '#00d066',
+  green600: '#00a552',
 
-  // RED
-  red100: '#FFF5F9',
-  red200: '#FFE0EB',
-  red300: '#FF6699',
-  red400: '#FF3377',
-  red500: '#E4175C',
-  red600: '#BF134D',
-  red: '#E4175C',
+  // Red
+  red100: '#fff5f9',
+  red200: '#ffe0eb',
+  red300: '#ff6699',
+  red400: '#ff3377',
+  red500: '#e4175c',
+  red600: '#bf134d',
 
-  // ORANGE
-  orange100: '#FFF7EB',
-  orange200: '#FFEDD0',
-  orange300: '#FFD06B',
-  orange400: '#FFB84D',
-  orange500: '#FF9B00',
-  orange600: '#DD8804',
-  orange: '#FF9B00',
+  // Orange
+  orange100: '#fff7eb',
+  orange200: '#ffedd0',
+  orange300: '#ffd06b',
+  orange400: '#ffb84d',
+  orange500: '#ff9b00',
+  orange600: '#dd8804',
 
-  // BLUE
-  blue100: '#EDF8FF',
-  blue200: '#D4EEFF',
-  blue300: '#73C7FF',
-  blue400: '#40B2FF',
-  blue500: '#0099FF',
-  blue600: '#0082D9',
-  blue: '#0099FF',
-}
+  // Blue
+  blue100: '#edf8ff',
+  blue200: '#d4eeff',
+  blue300: '#73c7ff',
+  blue400: '#40b2ff',
+  blue500: '#0099ff',
+  blue600: '#0082d9',
+};
+
+export const colorAliases = {
+  corePurple: colorDefinitions.corePurple500,
+  deepPurple: colorDefinitions.corePurple800,
+  blackPurple: colorDefinitions.corePurple900,
+  lightGrey: colorDefinitions.grey200,
+  darkGrey: colorDefinitions.grey400,
+  black: colorDefinitions.grey900,
+  green: colorDefinitions.green500,
+  red: colorDefinitions.red500,
+  orange: colorDefinitions.orange500,
+  blue: colorDefinitions.blue500,
+};
+
+export const colors = {
+  ...colorDefinitions,
+  ...colorAliases,
+};
