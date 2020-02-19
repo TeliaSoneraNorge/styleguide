64fa1c0138b3ad14aaf8dd67aae938e15aa96953
Nils Magnus Englund <nme@stepone.no>
2020-02-19 14:21:37 +0100

    Ran prettier

--- component-lib/stories/home/ColorPalette.stories.tsx
+++ component-lib/stories/home/ColorPalette.stories.tsx
@@ -13,48 +13,78 @@ export const Default = () => (
     <div style={{ width: '30px', height: '30px', background: colors.blue }}></div>
     <Icon icon="like" style={{ width: '100px', height: '100px', color: colors.red }} />
   </>
-) 
-
+);
 
 export const ThePalette = () => {
   const style = {
     minWidth: '12rem',
-  }
+  };
 
   return (
     <>
-     {colorsJSON.map(color => (
+      {colorsJSON.map(color => (
         <div style={{ display: 'flex', alignItems: 'center' }}>
-          <div style={{ 
-            padding: '.3rem 1rem',
-            marginRight: '1rem',
-            flexBasis: '8rem',
-            minWidth: '8rem',
-            color: color.textColor, 
-            background: color.hex,
-            textAlign: 'center',
-            overflowWrap: 'normal',
-          }}>
+          <div
+            style={{
+              padding: '.3rem 1rem',
+              marginRight: '1rem',
+              flexBasis: '8rem',
+              minWidth: '8rem',
+              color: color.textColor,
+              background: color.hex,
+              textAlign: 'center',
+              overflowWrap: 'normal',
+            }}
+          >
             Text Color
           </div>
           <p style={{ minWidth: style.minWidth }}>{color.name}</p>
           <p style={{ minWidth: style.minWidth }}>
-            <span style={{ color: 'rgba(0,0,0,.4)', background: '#f8f8f8', padding: '.2rem', marginRight: '.5rem', fontSize: '12px' }}>HEX</span>
+            <span
+              style={{
+                color: 'rgba(0,0,0,.4)',
+                background: '#f8f8f8',
+                padding: '.2rem',
+                marginRight: '.5rem',
+                fontSize: '12px',
+              }}
+            >
+              HEX
+            </span>
             {color.hex}
           </p>
           <p style={{ minWidth: style.minWidth }}>
-            <span style={{ color: 'rgba(0,0,0,.4)', background: '#f8f8f8', padding: '.2rem', marginRight: '.5rem', fontSize: '12px' }}>RGB</span>
+            <span
+              style={{
+                color: 'rgba(0,0,0,.4)',
+                background: '#f8f8f8',
+                padding: '.2rem',
+                marginRight: '.5rem',
+                fontSize: '12px',
+              }}
+            >
+              RGB
+            </span>
             {color.rgb}
           </p>
-          {color.alias ? 
+          {color.alias ? (
             <p style={{ minWidth: style.minWidth }}>
-              <span style={{ color: 'rgba(0,0,0,.4)', background: '#f8f8f8', padding: '.2rem', marginRight: '.5rem', fontSize: '12px' }}>ALIAS</span>
+              <span
+                style={{
+                  color: 'rgba(0,0,0,.4)',
+                  background: '#f8f8f8',
+                  padding: '.2rem',
+                  marginRight: '.5rem',
+                  fontSize: '12px',
+                }}
+              >
+                ALIAS
+              </span>
               {color.alias}
             </p>
-            : null
-          }
+          ) : null}
         </div>
-     ))}
+      ))}
     </>
-  )
-}
+  );
+};
