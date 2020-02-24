613ff718b65cc0aeac4792b08e2b2c372298dcec
Nils Magnus Englund <nme@stepone.no>
2020-02-24 12:21:11 +0100

    Added assets used by the page examples

---
+++ component-lib/stories/pages/sampleImages.ts
@@ -0,0 +1,23 @@
+import boyIpad from '../assets/boy-ipad.jpg';
+import forAlle from '../assets/for-alle.png';
+import icoInfo from '../assets/ico_info.svg';
+import icoShare from '../assets/ico_share.svg';
+import kidsIpad from '../assets/kids-ipad.jpg';
+import kontantkort from '../assets/kontantkort.png';
+import under15 from '../assets/under15.png';
+import under28 from '../assets/under28.png';
+import womanHoldingIpad from '../assets/woman-holding-ipad.jpg';
+import womanMobile from '../assets/woman-mobile.jpg';
+
+export default {
+  boyIpad,
+  forAlle,
+  icoInfo,
+  icoShare,
+  kidsIpad,
+  kontantkort,
+  under15,
+  under28,
+  womanHoldingIpad,
+  womanMobile,
+};
