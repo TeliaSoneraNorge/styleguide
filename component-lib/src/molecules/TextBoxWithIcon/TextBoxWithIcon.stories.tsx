7a1eaaaffcd06f7dc35b4efddc8365a27f2b3fd1
bogsti <christoffer.bogsti@telia.no>
2020-03-05 15:41:46 +0100

    TextBoxWithIcon

---
+++ component-lib/src/molecules/TextBoxWithIcon/TextBoxWithIcon.stories.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useRef } from 'react';
+import { TextBoxWithIcon } from '../../index';
+
+export default {
+  title: 'Component library|Molecules/TextBoxWithIcon',
+  component: TextBoxWithIcon,
+};
+
+export const Default = () => <TextBoxWithIcon type="search" iconName="ico_search" iconLabel="Search" iconIsButton />;
+
+export const TypeColoredIcon = () => <TextBoxWithIcon type="text" iconName="ico_check" iconColor="purple" />;
+
+export const SmallTextBoxWithIcon = () => (
+  <TextBoxWithIcon type="search" iconName="ico_search" iconLabel="Search" iconIsButton small />
+);
