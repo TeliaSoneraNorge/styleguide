6a7d49eba75a1454a80323396c7e0280d2600bb7
Alfred Birketvedt <alfredbirk@gmail.com>
2020-03-13 15:08:03 +0100

    Change design of delete icon in ShoppingCartItem

--- component-lib/src/molecules/ShoppingCart/ShoppingCartItem.jsx
+++ component-lib/src/molecules/ShoppingCart/ShoppingCartItem.jsx
@@ -158,7 +158,8 @@ const ShoppingCartItem = ({
       <ShoppingCartCell className="shopping-cart__item__delete">
         {isRemovable && !hasPaid && (
           <button className="button button--small shopping-cart__item__button" onClick={() => onRemoveItem(cartItem)}>
-            <Icon className="shopping-cart__item__icon-close" icon="close" title={`Fjern ${name} fra handlekurven`} />
+            <div className="shopping-cart__item__close">x</div>
+            <div className="shopping-cart__item__close__text">Slett</div>
           </button>
         )}
       </ShoppingCartCell>
