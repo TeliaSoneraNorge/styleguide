32447c88ba4666644f8899153698a56a634c506d
bogsti <christoffer.bogsti@telia.no>
2020-03-24 00:02:31 +0100

    added bubble-link

--- component-lib/src/atoms/Link/Link.stories.tsx
+++ component-lib/src/atoms/Link/Link.stories.tsx
@@ -1,13 +1,97 @@
 import React from 'react';
-import Link from './Link';
+import { Link, colors } from '../../index';
 
 export default {
   title: 'Component library|Atoms/Link',
   component: Link,
 };
 
-export const Default = () => <Link href="#" text="Standard text link" />
+export const Default = () => (
+  <>
+    <Link href="#">
+      Standard text link
+    </Link>
+    <br />
+    <br />
+    <div style={{ background: colors.deepPurple }}>
+      <Link href="#" negative={true}>
+        Standard text link on dark background
+      </Link>
+    </div>
+  </>
+);
 
-export const LinkWithIconAfter = () => <Link href="#" text="Link with icon after" icon="forward-icon" iconPosition="after" />
+export const LinkWithIconOnPositive = () => (
+  <>
+    <Link href="#" icon="arrow-left" iconPosition="before">
+      Link with icon before
+    </Link>
+    <br />
+    <Link href="#" icon="arrow-left" iconPosition="before" iconColor="core-purple">
+      Link with icon before
+    </Link>
+    <br />
+    <Link href="#" icon="arrow-left" iconPosition="before" iconColor="black">
+      Link with icon before
+    </Link>
+    <br />
+    <Link href="#" icon="arrow-right" iconPosition="after">
+      Link with icon after
+    </Link>
+    <br />
+    <Link href="#" icon="arrow-right" iconPosition="after" iconColor="core-purple">
+      Link with icon after
+    </Link>
+    <br />
+    <Link href="#" icon="arrow-right" iconPosition="after" iconColor="black">
+      Link with icon after
+    </Link>
+  </>
+);
 
-export const LinkWithIconBefore = () => <Link href="#" text="Link with icon before" icon="back-icon" iconPosition="before" />
+export const LinkWithIconOnNegative = () => (
+  <div style={{ background: colors.deepPurple, padding: '1rem' }}>
+    <Link href="#" negative={true} icon="arrow-left" iconPosition="before" iconColor="white">
+      Link with icon before
+    </Link>
+    <br />
+    <Link href="#" negative={true} icon="arrow-right" iconPosition="after" iconColor="white">
+      Link with icon after
+    </Link>
+  </div>
+);
+
+export const BubbleLink = () => (
+  <>
+    <Link type="bubble-link" href="#" icon="play" >
+      Core-Purple BubbleLink
+    </Link>
+    <br /><br />
+
+    <Link type="bubble-link" href="#" icon="settings" iconColor="black">
+      Black BubbleLink
+    </Link>
+    <br /><br />
+
+    <div style={{ background: colors.deepPurple, padding: '1rem' }}>
+      <Link type="bubble-link" href="#" icon="product-svitsj" iconColor="white">
+        BubbleLink on dark background
+      </Link>
+    </div>
+    
+    <br />
+  </>
+);
+
+export const DeprecatedIcons = () => (
+  <>
+    <Link href="#" icon="back-icon" iconPosition="before">
+      Link with icon before
+    </Link>
+    <br />
+    <Link href="#" icon="forward-icon" iconPosition="after">
+      Link with icon after
+    </Link>
+    <br />
+  </>
+);
--- component-lib/src/atoms/Link/Link.tsx
+++ component-lib/src/atoms/Link/Link.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import classnames from 'classnames';
+import cs from 'classnames';
+import { Icon, IconDefinition } from '../../index';
 
 /**
  * Status: *finished*.
@@ -12,10 +12,28 @@ import classnames from 'classnames';
  * Our links have _two visual means_ of being identified: an underline and bolder font.
  */
 
-
-export interface LinkProps {
+interface isLink {
+  type?: 'link';
   /**
-   * Content of this link.
+   * Choose between 'arrow-left' and 'arrow-right' icon.
+   * 
+   * 'back-icon' & 'forward-icon' are  deprecated!
+   * 
+   *  look on Icon component when using bubble-link
+   */
+  icon?: 'arrow-left' | 'arrow-right' | 'back-icon' | 'forward-icon';
+};
+
+interface isBubbleLink {
+  type?: 'bubble-link';
+  icon?: IconDefinition;
+}
+
+export interface CommonLinkProps {
+  className?: string;
+  /**
+    * The text to display in the component. We prefer to use `children` instead.
+    * @deprecated Use `children` instead.
    */
   text?: React.ReactNode;
 
@@ -24,39 +42,101 @@ export interface LinkProps {
    */
   children?: React.ReactNode;
 
-  className?: any;
-  href: string;
-
   target?: string;
 
-  /**
-   * Choose between forward arrow (forward-icon) and backward arrow (back-icon) icon
-   */
-  icon?: 'forward-icon' | 'back-icon';
+  href: string;
+  style?: React.CSSProperties;
 
   /**
    * Set position of icon (before or after)
    */
   iconPosition?: 'before' | 'after';
-};
+  iconColor?: 'core-purple' | 'black' | 'white';
 
-const Link = (props: LinkProps) => {
-  const { className, text, children, href, icon, iconPosition, target = '_self', ...rest } = props;
+  /**
+   * Only true if Link are on a dark background
+   */
+  negative?: boolean;
 
+  type?: 'link' | 'bubble-link';
+}
+
+export type LinkProps = (isLink | isBubbleLink) & CommonLinkProps;
+
+export const Link = (props: LinkProps) => {
+  const {
+    className,
+    text,
+    children,
+    href,
+    iconPosition,
+    iconColor = 'core-purple',
+    negative,
+    target = undefined,
+    type = 'link',
+    ...rest
+  } = props;
+
+  let icon;
+
+  // Support for deprecated icons
+  function checkForDeprecatedIcons(): string | undefined {
+    if(props.icon === 'back-icon') return icon = 'arrow-left';
+    if(props.icon === 'forward-icon') return icon = 'arrow-right';
+    return icon = props.icon;
+  }; checkForDeprecatedIcons();
+  
   return (
-    <a
-      className={classnames('link', {
-        [`link--with-icon link--${icon}`]: icon,
-        [`link--icon-${iconPosition}`]: icon && iconPosition,
-        [className]: className,
-      })}
-      href={href}
-      target={target}
-      {...rest}
-    >
-      {text}
-      {children}
-    </a>
+    <>
+
+    {type === 'link' && 
+      <a
+        className={cs(
+          negative ? 'link--negative' : 'link',
+          icon && iconColor !== 'white' ? 'link--icon' : '',
+          icon && iconColor === 'white' ? 'link--negative--icon' : '',
+          className ? className : undefined
+        )}
+        href={href}
+        target={target}
+        {...rest}
+      >
+        {icon && iconPosition === 'before' ? (
+          <Icon
+            icon={icon}
+            className={`link--icon--before link--${iconColor === 'white' ? 'negative--' : ''}icon--${iconColor}`}
+          />
+        ) : null}
+
+        {text}
+        {children}
+
+        {icon && iconPosition === 'after' ? (
+          <Icon
+            icon={icon}
+            className={`link--icon--after link--${iconColor === 'white' ? 'negative--' : ''}icon--${iconColor}`}
+          />
+        ) : null}
+      </a>
+    }
+
+    {type === 'bubble-link' &&
+      <a
+        href={href}
+        {...rest}
+        className={`bubble-link bubble-link--${iconColor}`}
+      > 
+        <div className="bubble-link--flex-container">
+          <span className={`bubble-link--icon-circle bubble-link--circle-background--${iconColor}`}>
+            <Icon icon={icon} className={`bubble-link--icon bubble-link--icon-color--${iconColor}`}/>
+          </span>
+          <span className={`bubble-link--text`}>
+            {children}
+          </span>
+        </div>
+      </a>
+    }
+    </>
   );
 };
 
