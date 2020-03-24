752fefbf90e9daf6cff6aca0be5ce64fb43f5629
Håkon Antonsen <hakon.antonsen@telia.no>
2020-03-24 12:11:58 +0100

    Sorting, selecting and paging

--- component-lib/src/business/Table/Table.stories.tsx
+++ component-lib/src/business/Table/Table.stories.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Table, TableBodyCell, TableBodyRow } from './Table';
+import { Table, TableBodyRow, TableBodyCell, TablePagingControls } from './Table';
+import _ from 'lodash';
 
 import { storiesOf } from '@storybook/react';
 
 const subscribers = require('./sampledata.json');
 
 storiesOf('Business|Table', module).add('default', () => {
+    const [selected, setSelected] = React.useState<Array<string>>([]);
+    const data = subscribers.slice(0, 10);
+
     return <Table headings={[
         "Navn",
         "Navn2",
@@ -23,11 +27,108 @@ storiesOf('Business|Table', module).add('default', () => {
         "Et annet objekt",
         "Profil",
         "Profil2"
-    ]}>
-        {subscribers.map((subscriber: any, index: number) =>
-            <TableBodyRow key={index}>
-                {Object.values(subscriber).map((item: any, index: number) =>
-                    <TableBodyCell key={index}>{item ? item.toString() : ""}</TableBodyCell>)}
-            </TableBodyRow>)}
-    </Table>;
-  });
+    ]}
+        onSelectAll={() => selected.length === data.length ? setSelected([]) : setSelected(data.reduce((acc: Array<any>, subscriber: any) => [...acc, subscriber["subscription_id"]], []))}
+        onSelectRow={(id: string) => selected.includes(id) ? setSelected(_.filter(selected, (elem) => elem !== id)) : setSelected([...selected, id])}
+        rowIdPropertyName="subscription_id"
+        selectedRows={selected}
+        data={data} />;
+});
+
+const initialState = {
+    selected: [],
+    allSelected: false,
+    sortKey: "subscription_id",
+    sortDirection: "ASC",
+    currentPerPage: 10,
+    currentPage: 1,
+    currentFrom: 0,
+    currentTo: 9,
+    data: []
+}
+
+
+function reducer(oldState: typeof initialState, action: {type: String, payload: any}): typeof initialState {
+    switch(action.type) {
+        case "SET_DATA": return { ...oldState, data: action.payload.data };
+        case "SELECT_ALL": return { ...oldState, selected: oldState.data.reduce((acc, elem) => [...acc, elem.subscription_id], []), allSelected: true };
+        case "DESELECT_ALL": return { ...oldState, selected: [], allSelected: false };
+        case "SELECT_ROW": return { ...oldState, selected: [...oldState.selected, action.payload] };
+        case "UNSELECT_ROW": return { ...oldState, selected: oldState.selected.filter((elem) => elem !== action.payload), allSelected: false};
+
+        case "SORT_COLUMN":
+        const newSortKey = action.payload;
+        const newSortDirection = newSortKey !== oldState.sortKey ? "ASC" : oldState.sortDirection === "ASC" ? "DESC" : "ASC";
+        return { ...oldState, sortKey: newSortKey, sortDirection: newSortDirection }
+
+        case "SET_PER_PAGE":
+        const newPerPage = action.payload;
+        const possibleNewPage = oldState.currentPage > Math.floor(subscribers.length/newPerPage) ? Math.floor(subscribers.length/newPerPage) >= 1 ? Math.floor(subscribers.length/newPerPage) : 1 : oldState.currentPage;
+        const newFrom = (newPerPage*possibleNewPage)-newPerPage;
+        const newTo = (newPerPage*possibleNewPage)-1 > subscribers.length-1 ? subscribers.length-1 : (newPerPage*possibleNewPage)-1;
+        return { ...oldState, currentPerPage: newPerPage, currentFrom: newFrom, currentTo: newTo, currentPage: possibleNewPage };
+
+        case "SET_PAGE":
+        const newPage = action.payload < 1 ? 1 : action.payload > Math.ceil(subscribers.length/oldState.currentPerPage) ? Math.ceil(subscribers.length/oldState.currentPerPage) : action.payload;
+        const newFrom2 = (oldState.currentPerPage*newPage)-oldState.currentPerPage;
+        const newTo2 = (oldState.currentPerPage*newPage)-1 > subscribers.length-1 ? subscribers.length-1 : (oldState.currentPerPage*newPage)-1;
+        return { ...oldState, currentPage: newPage, currentFrom: newFrom2, currentTo: newTo2 };
+
+        default: return {...oldState};
+    }
+}
+
+storiesOf('Business|Table', module).add('default', () => {
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+
+    React.useEffect(() => {
+       const newData = state.sortDirection === "ASC" ? _.sortBy(subscribers, state.sortKey).slice(state.currentFrom, state.currentTo+1) : _.sortBy(subscribers, state.sortKey).reverse().slice(state.currentFrom, state.currentTo+1);
+       dispatch({ type: "SET_DATA", payload: { data: newData }});
+    }, [state.sortKey, state.currentFrom, state.currentTo, state.sortDirection]);
+
+    return <React.Fragment>
+        <Table headings={{
+            formal_name: "Navn",
+            full_name: "Navn2",
+            subscription_id: "Telefonnummer",
+            account_id: "Avdelingsnummer",
+            account_name: "Avdeling",
+            first_name: "Fornavn",
+            last_name: "Etternavn",
+            info: "Ettellerannet",
+            resource_type: "Enhetstype",
+            subsciption_status: "Abonnementsstatus",
+            subscription_status_reason: "Statusårsak",
+            subscription_type: "Abonnement",
+            telephone_number: "Et objekt",
+            tags: "Et annet objekt",
+            profile_id: "Profil",
+            profile_name: "Profil2"
+        }}
+            onSelectAll={() => state.allSelected ? dispatch({ type: "DESELECT_ALL", payload: {} }) : dispatch({ type: "SELECT_ALL", payload: {} })}
+            allSelected={state.allSelected}
+            sortedColumnId={state.sortKey}
+            sortedColumnDirection={state.sortDirection}
+            onClickColumnHeader={(sortId) => dispatch({ type: "SORT_COLUMN", payload: sortId})}
+        >
+            {state.data.map((subscriber: any, index: number) =>
+                <TableBodyRow
+                    key={index}
+                    onSelect={() => state.selected.includes(subscriber.subscription_id) ? dispatch({ type: "UNSELECT_ROW", payload: subscriber.subscription_id }) : dispatch({ type: "SELECT_ROW", payload: subscriber.subscription_id })}
+                    selected={state.selected.includes(subscriber.subscription_id)}>
+                    {Object.values(subscriber).map((field: any, index: number) =>
+                        <TableBodyCell key={index}>{field ? field.toString() : ""}</TableBodyCell>)}
+                </TableBodyRow>)}
+        </Table>
+        <TablePagingControls
+            from={state.currentFrom+1}
+            to={state.currentTo+1}
+            dataLength={subscribers.length}
+            perPage={state.currentPerPage}
+            page={state.currentPage}
+            maxPage={Math.ceil(subscribers.length/state.currentPerPage)}
+            onPageChange={(page: number) => dispatch({ type: "SET_PAGE", payload: page })}
+            onPerPageChange={(perPage: number) => dispatch({ type: "SET_PER_PAGE", payload: perPage })}
+        />
+    </React.Fragment>
+});
--- component-lib/src/business/Table/Table.tsx
+++ component-lib/src/business/Table/Table.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import _ from 'lodash';
+
+export type SortDirection = "ASC" | "DESC" | "NONE";
 
 export const TableBodyCell: React.FC<any> = (props) => {
     const { children } = props;
@@ -8,11 +11,20 @@ export const TableBodyCell: React.FC<any> = (props) => {
     )
 }
 
-export const TableBodyRow: React.FC<any> = (props) => {
-    const { children } = props;
+interface TableBodyRowProps {
+    children: React.ReactNode,
+    onSelect?: () => void,
+    selected?: boolean
+}
+
+export const TableBodyRow: React.FC<TableBodyRowProps> = ({children, onSelect, selected}) => {
 
     return (
-        <tr>{children}</tr>
+        <tr>
+            {onSelect &&
+                <TableBodyCell><input type="checkbox" onChange={() => onSelect()} checked={selected} /></TableBodyCell>}
+            {children}
+        </tr>
     )
 }
 
@@ -20,28 +32,78 @@ const TableHeadCell: React.FC<any> = (props) => {
     const { children } = props;
 
     return (
-        <th>{children}</th>
+        <th onClick={props.onClick ? () => props.onClick() : null
+        }>{children}</th>
     )
 }
 
-interface TableProps {
-    children: React.ReactChildren,
-    headings: Array<String>
+type SortedTableHeadCellProps = {
+    children: React.ReactNode,
+    sortDirection: SortDirection,
+    onClick: () => void
+}
+const SortedTableHeadCell: React.FC<SortedTableHeadCellProps> = (props) => {
+    return (
+        <TableHeadCell onClick={props.onClick}>
+            {props.children}
+            {props.sortDirection === "ASC" ? "v" : null}
+            {props.sortDirection === "DESC" ? "^" : null}
+            {props.sortDirection === "NONE" ? "|" : null}
+        </TableHeadCell>
+    )
+}
+
+type TableProps = {
+    headings: Array<String> | Map<String | number, string>,
+    children: React.ReactNode,
+    onSelectAll?: () => void,
+    allSelected?: boolean,
+    onClickColumnHeader?: (columnId: string | number) => void,
+    sortedColumnId?: string | number,
+    sortedColumnDirection?: SortDirection
 }
 
 export const Table: React.FC<TableProps> = (props) => {
-    const { children, headings } = props;
-
     return (
         <table>
             <thead>
                 <tr>
-                    {headings.map((heading, index) => <TableHeadCell key={index}>{heading}</TableHeadCell>)}
+                    {props.onSelectAll &&
+                        <TableHeadCell><input type="checkbox" checked={props.allSelected} onChange={() => props.onSelectAll()} /></TableHeadCell>}
+                    {_.map(props.headings, (heading, key) =>
+                        props.sortedColumnId ?
+                        <SortedTableHeadCell key={key} sortDirection={props.sortedColumnId === key ? props.sortedColumnDirection : "NONE"} onClick={() => props.onClickColumnHeader(key)}>{heading}</SortedTableHeadCell> :
+                        <TableHeadCell key={key}>{heading}</TableHeadCell>)}
                 </tr>
             </thead>
             <tbody>
-                {children}
+                {props.children}
             </tbody>
         </table>
+    )
+}
+
+
+export const TablePagingControls: React.FC<any> = (props) => {
+    return (
+        <form onSubmit={(e) => e.preventDefault()}>
+            <label>
+            {props.perPageString || "Rader per side: "}
+            <select value={props.perPage} onChange={(e) => props.onPerPageChange(parseInt(e.target.value))}>
+                {(props.selectOptions || [10, 25, 50, 100, 1000]).map((option: number, key: number) => <option value={option} key={key}>{option}</option>)}
+            </select>
+            </label>
+            <span>
+                {props.fromToString || `${props.from}-${props.to} av ${props.dataLength}`}
+            </span>
+            <button disabled={props.page === 1} onClick={(e) => {
+                e.preventDefault();
+                props.onPageChange(props.page-1);
+            }}>&lt;</button>
+            <button disabled={props.page === props.maxPage} onClick={(e) => {
+                e.preventDefault();
+                props.onPageChange(props.page+1);
+            }}>&gt;</button>
+        </form>
     )
 }
