777cc22705faab9c9ab3aa4e4b858e023105fc65
Alfred Birketvedt <alfredbirk@gmail.com>
2020-04-07 10:27:33 +0200

    Remove knobs in subscription stories

--- component-lib/src/molecules/Subscription/Subscription.stories.tsx
+++ component-lib/src/molecules/Subscription/Subscription.stories.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
-import { text, number, select, array, boolean } from '@storybook/addon-knobs';
 
 import Subscription from './Subscription';
 import Container from '../../atoms/Container/Container';
+import {
+  allSubscriptions,
+} from './consts';
+import BoxGrid from '../BoxGrid/BoxGrid';
 
 const SubscriptionForm = ({ postToUrl, id }) => (
   <div className="subscription__button-container">
@@ -56,73 +59,41 @@ const featureMapper = {
 
 const stories = storiesOf('Component library|Molecules/Subscription', module);
 stories.addWithJSX('Subscription', () => {
-  const [isExpanded, setIsExpanded] = useState(false);
-  const name = text('Name', 'Telia');
-  const dataAmount = text('Data Amount', 'X');
-  const dataUnit = text('Data Unit', 'GB');
-  const price = number('Price', 579);
-  const priceInfo = array('PriceInfo', ['pr. md']);
-  const color = select('Color', ['purple', 'pink', 'light-orange', 'green', 'blue', 'teal', 'grey', 'black'], 'black');
-  const size = select('Size', ['small', 'medium'], 'medium');
-  const showFeatures = select('Number of features to show', [0, 1, 2], 0);
-  const scrollToOnOpen = boolean('Scroll to on open', false);
+  const [selectedSubscription, setSelectedSubscription] = useState(null);
 
   return (
-    <Container size="medium">
-      <Subscription
-        name={name}
-        id="smartMini"
-        dataAmount={dataAmount}
-        dataUnit={dataUnit}
-        price={price}
-        priceInfo={priceInfo}
-        color={color}
-        size={size}
-        features={featureMapper[showFeatures]}
-        isShowingFeatures={showFeatures > 0}
-        isExpanded={isExpanded}
-        onSelect={() => setIsExpanded(true)}
-        onClose={() => setIsExpanded(false)}
-        scrollToOnOpen={scrollToOnOpen}
-      >
-        <div className="rich-text">
-          <h2 className="heading heading--level-2">Om abonnement</h2>
-          <h3 className="heading heading--level-3">Skanning, texting og MMS</h3>
-          <p>
-            Lorem Ipsum best not make any more threats to your website. It will be met with fire and fury like the world
-            has never seen. Does everybody know that pig named Lorem Ipsum? She&apos;s a disgusting pig, right?
-          </p>
-          <h3 className="heading heading--level-3">Data</h3>
-          <p>
-            I was going to say something extremely rough to Lorem Ipsum, to its family, and I said to myself, &quot;I
-            can&apos;t do it. I just can&apos;t do it. It&apos;s inappropriate. It&apos;s not nice.&quot; When other
-            websites give you text, they’re not sending the best. They’re not sending you, they’re sending words that
-            have lots of problems and they’re bringing those problems with us. They’re bringing mistakes. They’re
-            bringing misspellings. They’re typists… And some, I assume, are good words. I think the only card she has is
-            the Lorem card.
-          </p>
-          <h3 className="heading heading--level-3">Utland</h3>
-          <p>
-            We have so many things that we have to do better... and certainly ipsum is one of them. Despite the constant
-            negative ipsum covfefe. Lorem Ipsum is the single greatest threat. We are not - we are not keeping up with
-            other websites.
-          </p>
-          <ul className="list list--links">
-            <li className="list__item">
-              <a className="list__link" href="#" target="_self">
-                Last ned abonnementsvilkår
-              </a>
-            </li>
-          </ul>
-        </div>
-      </Subscription>
-    </Container>
+    <Container size="medium" noMargin noPadding>
+    <BoxGrid>
+        { allSubscriptions.map((sub) => (
+            <Subscription
+                key={sub.id}
+                id={sub.id}
+                name={sub.name}
+                dataAmount={sub.dataAmount}
+                dataUnit={sub.dataUnit}
+                price={sub.price}
+                priceInfo={sub.priceInfo}
+                color={sub.color}
+                size={sub.size}
+                isExpanded={sub.id === selectedSubscription}
+                isShowingFeatures={sub.isShowingFeatures}
+                onClose={() => setSelectedSubscription(null)}
+                onSelect={(e, subscriptionId) => setSelectedSubscription(subscriptionId)}
+                scrollToOnOpen
+                features={{
+                    speechBubbleText: sub.speechBubbleText,
+                    highlightedFeature: sub.highlightedFeature,
+                    specialMessageText: sub.specialMessageText,
+                }}
+            >
+            </Subscription>
+        )) }
+    </BoxGrid>
+</Container>
   );
 });
 
 stories.addWithJSX('SubscriptionWithSelect', () => {
-  const showFeaturesInTeaser = boolean("ShowFeaturesInTeaser", true);
-  const priceInfo = array('PriceInfo', ['pr. md']);
   const transferSubscriptions = [
     {
         name: "Telia",
@@ -131,7 +102,7 @@ stories.addWithJSX('SubscriptionWithSelect', () => {
         dataAmount: 'X',
         dataUnit: "GB",
         price: 579,
-        priceInfo,
+        priceInfo: ['pr. md'],
         groupSubscriptionDiscount: 180,
         color: "purple",
         size: "small",
@@ -144,7 +115,7 @@ stories.addWithJSX('SubscriptionWithSelect', () => {
         dataAmount: 20,
         dataUnit: "GB",
         price: 529,
-        priceInfo,
+        priceInfo: ['pr. md'],
         color: "purple",
         size: "small",
         hasGroupSubscriptionDiscount: false,
@@ -156,7 +127,7 @@ stories.addWithJSX('SubscriptionWithSelect', () => {
         dataAmount: 15,
         dataUnit: "GB",
         price: 479,
-        priceInfo,
+        priceInfo: ['pr. md'],
         color: "purple",
         size: "small",
         hasGroupSubscriptionDiscount: false,
@@ -168,7 +139,7 @@ stories.addWithJSX('SubscriptionWithSelect', () => {
         dataAmount: 10,
         dataUnit: "GB",
         price: 429,
-        priceInfo,
+        priceInfo: ['pr. md'],
         color: "purple",
         size: "small",
         hasGroupSubscriptionDiscount: false,
@@ -180,7 +151,7 @@ stories.addWithJSX('SubscriptionWithSelect', () => {
         dataAmount: 6,
         dataUnit: "GB",
         price: 379,
-        priceInfo,
+        priceInfo: ['pr. md'],
         color: "purple",
         size: "small",
         hasGroupSubscriptionDiscount: false,
@@ -192,7 +163,7 @@ stories.addWithJSX('SubscriptionWithSelect', () => {
         dataAmount: 4,
         dataUnit: "GB",
         price: 329,
-        priceInfo,
+        priceInfo: ['pr. md'],
         color: "purple",
         size: "small",
         hasGroupSubscriptionDiscount: false,
@@ -204,7 +175,7 @@ stories.addWithJSX('SubscriptionWithSelect', () => {
         dataAmount: 2,
         dataUnit: "GB",
         price: 249,
-        priceInfo,
+        priceInfo: ['pr. md'],
         color: "purple",
         size: "small",
         hasGroupSubscriptionDiscount: false,
---
+++ component-lib/src/molecules/Subscription/consts.js
@@ -0,0 +1,291 @@
+export const FEATURES = {
+    DATA_BOOST: 'DATA_BOOST',
+    MUSIC_FREEDOM: 'MUSIC_FREEDOM',
+    TRY_AND_BUY: 'TRY_AND_BUY',
+    CONTROL: 'CONTROL',
+    GIFT_KONTANT: 'GIFT_KONTANT',
+    X: 'X'
+};
+
+export const FEATURES_CONTENT = {
+    [FEATURES.DATA_BOOST]: {
+        iconName: 'product-data-boost',
+        name: 'Data Boost'
+    },
+    [FEATURES.MUSIC_FREEDOM]: {
+        iconName: 'product-music-freedom',
+        name: 'Music Freedom inkludert'
+    },
+    [FEATURES.CONTROL]: {
+        iconName: 'money',
+        name: 'Kostnadskontroll'
+    },
+    [FEATURES.TRY_AND_BUY]: {
+        iconName: 'ico_interest_free_down_payment_icon',
+        name: 'Ny kunde? Prøv oss nå og få halv pris i 3 md.'
+    },
+    [FEATURES.GIFT_KONTANT]: {
+        iconName: '',
+        name: 'Nyhet: Music Freedom inkludert i Smart Kontant 10, 15 og 20 GB!'
+    },
+    [FEATURES.X]: {
+        iconName: 'ico_data_freedom',
+        name: 'Surf og stream uten å gå tom for data.',
+        secondIconName: 'group',
+        secondName: 'Flere sammen gir lavere pris',
+        secondSize: 'large'
+    }
+};
+
+export const HIGHLIGHTS = {
+    [FEATURES.TRY_AND_BUY]: {
+        title: 'Prøv oss til halv pris i 3 måneder – fornøyd eller pengene tilbake',
+        content: [
+            'Som ny kunde med valgfritt SMART- eller UNG-abonnement får du halv pris på månedsavgiften i 3 måneder. Rabatten vises i kassen når du har fylt inn telefonnummer. ',
+            'Etter prøveperioden løper abonnementet til ordinær pris. Er du da ikke fornøyd og velger å si opp, får du tilbake månedsavgiften.'
+        ]
+    }
+};
+
+export const BULLET_POINTS = {
+    FREE_VOICE_SMS_AND_MMS: 'FREE_VOICE_SMS_AND_MMS',
+    DATA_SHARING: 'DATA_SHARING',
+    MUSIC_FREEDOM: 'MUSIC_FREEDOM',
+    ROAM_LIKE_HOME: 'ROAM_LIKE_HOME',
+    MINUTES_TO_CALL: 'MINUTES_TO_CALL',
+    TELIA_X: 'TELIA_X'
+};
+
+export const BULLET_POINTS_CONTENT = {
+    [BULLET_POINTS.FREE_VOICE_SMS_AND_MMS]: 'Fri bruk av samtaler, SMS og MMS',
+    [BULLET_POINTS.DATA_SHARING]: 'Nyhet! Data sharing - <a class="link" href="https://www.telia.no/data-sharing/" target="_blank">Del Rollover-data med andre Telia-kunder</a>',
+    [BULLET_POINTS.MUSIC_FREEDOM]: 'Music Freedom - <a class="link" href="https://www.telia.no/music-freedom/" target="_blank">Hør så mye musikk du vil - uten å bruke dataen din</a>',
+    [BULLET_POINTS.ROAM_LIKE_HOME]: 'Roam Like Home – Bruk mobilen som hjemme i EU, EØS og Sveits',
+    [BULLET_POINTS.TELIA_X]: 'Med Telia X kan du surfe og streame i vei uten å gå tom for data'
+};
+
+export const DISCLAIMER_CONTENT = {
+    OPTION_ONE: '<p class="paragraph paragraph--small-text">Egne priser fra <a class="link" href="https://www.telia.no/priser-i-utlandet/" target="_blank">Norge til utlandet</a>, <a class="link" href="https://www.telia.no/priser-bat-fly-satellitt/" target="_blank">fly/båt/satellitt</a>, <a class="link" href="https://www.telia.no/privat/priser/spesialnummer/" target="_blank">spesialnumre</a> og <a class="link" href="https://www.telia.no/priser-i-utlandet/" target="_blank">bruk utenfor EU/EØS</a>. Estimert høyeste hastighet opptil 220 Mbit/s ned, og 35 Mbit/s opp. Hastighet varierer etter kapasitet og andre forhold.  <a class="link" href="https://www.telia.no/hastighet/" target="_blank">Mer om fart og dekning</a>. Music Freedom gjelder Spotify, Apple Music, Tidal, Deezer og Beat.  <a class="link" href="https://www.telia.no/data-sharing/" target="_blank">Data Sharing</a> gjelder deling av Rollover mellom BARN-, UNG- og SMART-abonnement. Du må være over 18 år for å dele data.</p>',
+    OPTION_TWO: '<p class="paragraph paragraph--small-text">Egne priser fra <a class="link" href="https://www.telia.no/priser-i-utlandet/" target="_blank">Norge til utlandet</a>, <a class="link" href="https://www.telia.no/priser-bat-fly-satellitt/" target="_blank">fly/båt/satellitt</a>, <a class="link" href="https://www.telia.no/privat/priser/spesialnummer/" target="_blank">spesialnumre</a> og <a class="link" href="https://www.telia.no/priser-i-utlandet/" target="_blank">bruk utenfor EU/EØS</a>. Estimert høyeste hastighet opptil 220 Mbit/s ned, og 35 Mbit/s opp. Hastighet varierer etter kapasitet og andre forhold.  <a class="link" href="https://www.telia.no/hastighet/" target="_blank">Mer om fart og dekning</a>.<a class="link" href="https://www.telia.no/data-sharing/" target="_blank">Data Sharing</a> gjelder deling av Rollover mellom BARN-, UNG- og SMART-abonnement. Du må være over 18 år for å dele data.</p>',
+    OPTION_THREE: '<p class="paragraph paragraph--small-text">Egne priser fra <a class="link" href="https://www.telia.no/priser-i-utlandet/" target="_blank">Norge til utlandet</a>, <a class="link" href="https://www.telia.no/priser-bat-fly-satellitt/" target="_blank">fly/båt/satellitt</a>, <a class="link" href="https://www.telia.no/privat/priser/spesialnummer/" target="_blank">spesialnumre</a> og <a class="link" href="https://www.telia.no/priser-i-utlandet/" target="_blank">bruk utenfor EU/EØS</a>. Estimert høyeste hastighet opptil 220 Mbit/s ned, og 35 Mbit/s opp. Hastighet varierer etter kapasitet og andre forhold.  <a class="link" href="https://www.telia.no/hastighet/" target="_blank">Mer om fart og dekning</a>. Music Freedom gjelder Spotify, Apple Music, Tidal, Deezer og Beat. Hastigheten reduseres til 3 mbit/s etter 40 GB, uten at dette skal påvirke opplevelsen på mobilen din. I EU, EØS og Sveits kan du bruke 30 GB, deretter koster det 4 øre pr. MB.</p>'
+};
+
+export const FEATURE_HIGHLIGHTS = {
+    WE_FIX: 'WE_FIX',
+    NEW_SIM: 'NEW_SIM',
+    BANK_ID: 'BANK_ID',
+    DATA_SHARING: 'DATA_SHARING',
+    TRY_US: 'TRY_US'
+};
+
+export const TEASER_FEATURES = {
+    X: "Fri bruk av data",
+    MUSIC_FREEDOM: "Music Freedom inkludert"
+};
+
+export const FEATURE_HIGHLIGHTS_CONTENT = {
+    [FEATURE_HIGHLIGHTS.WE_FIX]: {
+        icon: 'check-mark',
+        text: 'Vi ordner alt med flytting fra din gamle operatør.',
+        title: 'Vi ordner alt'
+    },
+    [FEATURE_HIGHLIGHTS.NEW_SIM]: {
+        icon: 'sim-card',
+        text: 'Du får nytt SIM-kort av oss etter 1-5 dager.',
+        title: 'Nytt SIM-kort'
+    },
+    [FEATURE_HIGHLIGHTS.DATA_SHARING]: {
+        icon: 'product-data-sharing',
+        text: 'Del dataen du ikke bruker opp.',
+        title: 'Data Sharing'
+    },
+    [FEATURE_HIGHLIGHTS.BANK_ID]: {
+        icon: 'laptop',
+        text: 'Hos Telia får du fri bruk av BankID.',
+        title: 'BankID'
+    },
+    [FEATURE_HIGHLIGHTS.TRY_US]: {
+        icon: 'money',
+        text: 'Prøv oss i 2 md. Passer vi ikke får du pengene tilbake.',
+        title: 'Ny kunde?'
+    }
+};
+
+export const BUTTON_TEXT = {
+    ORDER_FOR_ONE: 'Bestill for én',
+    ORDER_FOR_MORE: 'Flere sammen?',
+    ADD: 'Legg i handlekurv',
+    GO_TO_CART: 'Gå til kassen'
+};
+
+export const TERM_TEXT = {
+    REFUND: 'Dersom du ikke er fornøyd etter to måneder refunderer vi abonnementsavgiften. Andre påløpte kostnader refunderes ikke, gjelder kun for nye kunder, og du må ta kontakt innen 60 dager etter aktivering.'
+};
+
+export const GROUP_SUBSCRIPTION = {
+    MIN_QUANTITY: 2,
+    MAX_QUANTITY: 8
+};
+
+export const GROUP_SUBSCRIPTION_TEXT = {
+    MONEY_SAVED: 'Sparekalkulator',
+    NUMBER_OF_MEMBERS: 'Klikk på antall medlemmer',
+    PRICE_PER_SUB: 'Pris pr abonnement:',
+    TOTAL_PRICE: 'Totalpris:',
+    YOUR_TOTAL_PRICE: 'Din totalpris:',
+    PER_MONTH: ',-/pr. md.',
+    QUANTITY_PICKER_INCREASE: 'Øk antall',
+    QUANTITY_PICKER_REDUCE: 'Reduser antall',
+    CONTINUE: 'Fortsett',
+    HEADER_TEXT: (name) => `Flere sammen i gruppe gir billigere ${name} for alle`,
+    NUMBER_OF_SUBSCRIPTIONS: 'Antall abonnement'
+};
+
+const priceInfo = [
+    'per md.',
+    'Ingen bindingstid'
+];
+
+export const allSubscriptions = [
+    {
+        name: "Telia",
+        header: "Telia X",
+        id: "SMART_X.REGULAR",
+        dataAmount: 'X',
+        dataUnit: "GB",
+        isDataUnlimited: true,
+        price: 579,
+        priceInfo,
+        groupSubscriptionDiscount: 180,
+        color: "black",
+        size: "medium",
+        isShowingFeatures: true,
+        hasGroupSubscriptionDiscount: true,
+        highlightedFeature: FEATURES_CONTENT[FEATURES.X],
+        bullets: [
+            BULLET_POINTS.TELIA_X,
+            BULLET_POINTS.FREE_VOICE_SMS_AND_MMS,
+            BULLET_POINTS.MUSIC_FREEDOM,
+            BULLET_POINTS.ROAM_LIKE_HOME,
+            BULLET_POINTS.MINUTES_TO_CALL
+        ],
+        disclaimer: DISCLAIMER_CONTENT.OPTION_THREE
+    },
+    {
+        name: "SMART",
+        id: "SMART_20GB.REGULAR",
+        dataAmount: 20,
+        dataUnit: "GB",
+        price: 529,
+        priceInfo,
+        color: "blue",
+        size: "medium",
+        isShowingFeatures: true,
+        hasGroupSubscriptionDiscount: false,
+        highlightedFeature: FEATURES_CONTENT[FEATURES.MUSIC_FREEDOM],
+        bullets: [
+            BULLET_POINTS.FREE_VOICE_SMS_AND_MMS,
+            BULLET_POINTS.DATA_SHARING,
+            BULLET_POINTS.MUSIC_FREEDOM,
+            BULLET_POINTS.ROAM_LIKE_HOME,
+            BULLET_POINTS.MINUTES_TO_CALL
+        ],
+        disclaimer: DISCLAIMER_CONTENT.OPTION_ONE
+    },
+    {
+        name: "SMART",
+        id: "SMART_15GB.REGULAR",
+        dataAmount: 15,
+        dataUnit: "GB",
+        price: 479,
+        priceInfo,
+        color: "green",
+        size: "medium",
+        isShowingFeatures: true,
+        hasGroupSubscriptionDiscount: false,
+        highlightedFeature: FEATURES_CONTENT[FEATURES.MUSIC_FREEDOM],
+        bullets: [
+            BULLET_POINTS.FREE_VOICE_SMS_AND_MMS,
+            BULLET_POINTS.DATA_SHARING,
+            BULLET_POINTS.MUSIC_FREEDOM,
+            BULLET_POINTS.ROAM_LIKE_HOME,
+            BULLET_POINTS.MINUTES_TO_CALL
+        ],
+        disclaimer: DISCLAIMER_CONTENT.OPTION_ONE
+    },
+    {
+        name: "SMART",
+        id: "SMART_10GB.REGULAR",
+        dataAmount: 10,
+        dataUnit: "GB",
+        price: 429,
+        priceInfo,
+        color: "pink",
+        size: "medium",
+        isShowingFeatures: true,
+        hasGroupSubscriptionDiscount: false,
+        highlightedFeature: FEATURES_CONTENT[FEATURES.MUSIC_FREEDOM],
+        bullets: [
+            BULLET_POINTS.FREE_VOICE_SMS_AND_MMS,
+            BULLET_POINTS.DATA_SHARING,
+            BULLET_POINTS.MUSIC_FREEDOM,
+            BULLET_POINTS.ROAM_LIKE_HOME,
+            BULLET_POINTS.MINUTES_TO_CALL
+        ],
+        disclaimer: DISCLAIMER_CONTENT.OPTION_ONE
+    },
+    {
+        name: "SMART",
+        id: "SMART_06GB.REGULAR",
+        dataAmount: 6,
+        dataUnit: "GB",
+        price: 379,
+        priceInfo,
+        color: "green",
+        size: "small",
+        hasGroupSubscriptionDiscount: false,
+        bullets: [
+            BULLET_POINTS.FREE_VOICE_SMS_AND_MMS,
+            BULLET_POINTS.DATA_SHARING,
+            BULLET_POINTS.ROAM_LIKE_HOME,
+            BULLET_POINTS.MINUTES_TO_CALL
+        ],
+        disclaimer: DISCLAIMER_CONTENT.OPTION_TWO
+    },
+    {
+        name: "SMART",
+        id: "SMART_04GB.REGULAR",
+        dataAmount: 4,
+        dataUnit: "GB",
+        price: 329,
+        priceInfo,
+        color: "blue",
+        size: "small",
+        hasGroupSubscriptionDiscount: false,
+        bullets: [
+            BULLET_POINTS.FREE_VOICE_SMS_AND_MMS,
+            BULLET_POINTS.DATA_SHARING,
+            BULLET_POINTS.ROAM_LIKE_HOME,
+            BULLET_POINTS.MINUTES_TO_CALL
+        ],
+        disclaimer: DISCLAIMER_CONTENT.OPTION_TWO
+    },
+    {
+        name: "SMART",
+        id: "SMART_MINI_3.REGULAR",
+        dataAmount: 2,
+        dataUnit: "GB",
+        price: 249,
+        priceInfo,
+        color: "light-orange",
+        size: "small",
+        hasGroupSubscriptionDiscount: false,
+        bullets: [
+            BULLET_POINTS.FREE_VOICE_SMS_AND_MMS,
+            BULLET_POINTS.DATA_SHARING,
+            BULLET_POINTS.ROAM_LIKE_HOME,
+            BULLET_POINTS.MINUTES_TO_CALL
+        ],
+        disclaimer: DISCLAIMER_CONTENT.OPTION_TWO
+    }
+];
