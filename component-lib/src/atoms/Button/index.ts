daab0dd018cd31fe3f3ac4bb8d6c03e588002515
Robert Pinyol <robert.pinyol@gmail.com>
2020-05-22 11:30:38 +0200

    reintroduce Button exports for types, kinds and margins

--- component-lib/src/atoms/Button/index.ts
+++ component-lib/src/atoms/Button/index.ts
@@ -1,1 +1,7 @@
+import * as b from './Button';
+
 export { default } from './Button';
+
+export type ButtonKind = b.ButtonKind;
+export type ButtonType = b.ButtonType;
+export type ButtonMargin = b.ButtonMargin;
