88f03526dcd2f0fb34d4a9a8832569bd57a660e9
Helene Grini <helenegrini96@gmail.com>
2020-07-01 15:42:07 +0200

    Require onClick in Tab

--- component-lib/src/business/Tabs/Tab.tsx
+++ component-lib/src/business/Tabs/Tab.tsx
@@ -5,7 +5,7 @@ export type TabProps = {
   exact?: boolean;
   label: string | React.ReactNode;
   active?: boolean;
-  onClick?: (value: string) => void;
+  onClick: (value: string) => void;
 } & (
   | {
       path: string;
@@ -21,23 +21,13 @@ export const Tab: React.FC<TabProps> = props => {
         rel="noopener"
         onClick={e => {
           e.preventDefault();
-          if (props.onClick) {
-            props.onClick(props.path);
-          }
+          props.onClick(props.path);
         }}
       >
         {props.label}
       </a>
     ) : (
-      <button
-        onClick={() => {
-          if (props.onClick) {
-            props.onClick(props.value);
-          }
-        }}
-      >
-        {props.label}
-      </button>
+      <button onClick={() => props.onClick(props.value)}>{props.label}</button>
     );
 
   return (
