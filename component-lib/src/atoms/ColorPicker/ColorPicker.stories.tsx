af850b1763c2c795df718c5d40b90b504652fc03
Joakim Kristiansen <joakikr@gmail.com>
2020-07-23 12:16:01 +0200

    Added ColorPicker component (#624)
    * Added ColorPicker component
    
    * Adjust z-index of circle
    
    * CR changes to ColorPicker

---
+++ component-lib/src/atoms/ColorPicker/ColorPicker.stories.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import ColorPicker, { Color } from './ColorPicker';
+
+export default {
+  title: 'Component library|Atoms/ColorPicker',
+  component: ColorPicker,
+};
+
+const colors = [
+    { name: "Grey", color: "grey", id: "grey" },
+    { name: "Light", color: "#FFDAAF", id: "light" },
+    { name: "Black", color: "#000", id: "black" }
+]
+
+export const Default = () => {
+    const [selected, setSelected] = useState<Color>();
+    return (
+        <ColorPicker selected={selected} onSelect={setSelected} colors={colors} />
+    )
+}
