75a893ede3895a582f47d19596492ad3067b5667
Helene Grini <helenegrini96@gmail.com>
2020-08-05 07:36:04 +0200

    Add docblock to accessibility hook

--- component-lib/src/business/Dropdown/useA11yDropdown.ts
+++ component-lib/src/business/Dropdown/useA11yDropdown.ts
@@ -1,6 +1,14 @@
 import { useEffect } from 'react';
 import { useDropdownContext } from './index';
 
+/**
+ * Functionality for making a dropdown accessible.
+ * This hook is applied do the dropdown in the Styleguide (see InnerDropdown in Dropdown.tsx).
+ *
+ * This functionality is extracted for readability and separation of concert,
+ * and is only intended for use in the Stylguide
+ * -- no need to apply accessibility manualy when using the Dropdown
+ */
 export const useA11yDropdown = () => {
   const ctx = useDropdownContext();
 
