8102f0968e362dc82f50877744646432b3a37544
Helene Grini <helenegrini96@gmail.com>
2020-08-06 08:08:01 +0200

    Remove export of accessibility hook in index

--- component-lib/src/business/Dropdown/index.ts
+++ component-lib/src/business/Dropdown/index.ts
@@ -3,4 +3,3 @@ export { DropdownItem } from './DropdownItem';
 export { DropdownToggle } from './DropdownToggle';
 export { DropdownMenu } from './DropdownMenu';
 export { useDropdownContext, DropdownContext } from './context';
-export { useA11yDropdown } from './useA11yDropdown';
