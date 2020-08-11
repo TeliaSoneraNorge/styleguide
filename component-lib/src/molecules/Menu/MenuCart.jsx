2a3c2209e25237b5e45696394df238f3d368c83a
Alfred Birketvedt <alfredbirk@gmail.com>
2020-08-11 15:02:56 +0200

    CR changes

--- component-lib/src/molecules/Menu/MenuCart.jsx
+++ component-lib/src/molecules/Menu/MenuCart.jsx
@@ -4,11 +4,14 @@ import SvgIcon from '../../atoms/SvgIcon/SvgIcon';
 
 export default function MenuCart({ onClick, numberOfItemsInCart }) {
   return (
-    <button className="menu-cart__button" onClick={onClick}>
-      <SvgIcon className="menu-cart__icon" iconName="ico_buy" color="black" />
-      <span className="menu-cart__item-count">
-        <span className="menu-cart__item-count-text">{numberOfItemsInCart}</span>
-      </span>
+    <button className="menu-cart__button" onClick={onClick} aria-label="Kasse">
+      <div className="menu-cart__icon-container">
+        <SvgIcon className="menu-cart__icon" iconName="ico_buy" color="black" />
+        <span className="menu-cart__item-count">
+          <span className="menu-cart__item-count-text">{numberOfItemsInCart}</span>
+        </span>
+      </div>
+      <span className="menu-cart__button--label">Kasse</span>
     </button>
   );
 }
