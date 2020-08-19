ac6480b5248c7539256fdb548d179d77ca19ee82
Helene Grini <helenegrini96@gmail.com>
2020-08-19 12:39:00 +0200

    Hanlde disables elements in start of tab order in focus trap

--- component-lib/src/business/Modal/useFocusTrap.ts
+++ component-lib/src/business/Modal/useFocusTrap.ts
@@ -1,18 +1,43 @@
-import React, { useRef, useEffect } from 'react';
+import { useRef, useEffect } from 'react';
 
+const getFirstFocus = (elementList: NodeListOf<HTMLElement> | HTMLElement[]) => {
+  let first: HTMLElement;
+  for (let i = 0; i < elementList.length; i++) {
+    if (!elementList[i]['disabled']) {
+      first = elementList[i];
+      break;
+    }
+  }
+  return first;
+};
+
+const getLastFocus = (elementList: NodeListOf<HTMLElement>) => {
+  let reverse: HTMLElement[] = [];
+  elementList.forEach(elemnt => {
+    reverse = [elemnt, ...reverse];
+  });
+  return getFirstFocus(reverse);
+};
+
+/**
+ * This hook captures the focus within a container (now used only for Modal).
+ * It handle disabled elements any where in the tab order,
+ * and tabs to first element after last.
+ * Using shift it tabs to last element after first.
+ */
 export const useFocusTrap = () => {
   const container = useRef<HTMLDivElement>(null);
 
-  // get all elements within the containter that can receive focus
-  const focusable = container.current
-    ? container.current.querySelectorAll<HTMLElement>(
-        'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
-      )
-    : undefined;
-  const firstFocusableElement = focusable ? focusable[0] : undefined;
-  const lastFocusableElement = focusable ? focusable[focusable.length - 1] : undefined;
-
   useEffect(() => {
+    const focusable = container.current
+      ? container.current.querySelectorAll<HTMLElement>(
+          'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+        )
+      : undefined;
+
+    const firstFocusableElement = getFirstFocus(focusable);
+    const lastFocusableElement = getLastFocus(focusable);
+
     const handleTab = (e: KeyboardEvent) => {
       const tabKeyPressed = e.key === 'Tab' || e.keyCode === 9;
       if (!tabKeyPressed) return;
