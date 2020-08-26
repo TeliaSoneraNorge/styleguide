ed5023877e817858b8ca431f56dd9018878985a8
Nils Magnus Englund <nme@stepone.no>
2020-08-26 10:04:04 +0200

    Added LoadingPebble component (#657)

---
+++ component-lib/src/atoms/LoadingPebble/LoadingPebble.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { LoadingPebble } from './LoadingPebble';
+
+export default {
+  title: 'Component library/Atoms/LoadingPebble',
+  component: LoadingPebble,
+};
+
+export const Default = () => (
+  <>
+    <h1>LoadingPebble</h1>
+
+    <LoadingPebble />
+  </>
+);
