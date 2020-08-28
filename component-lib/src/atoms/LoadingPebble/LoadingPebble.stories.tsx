5b271d0af0dd69dd5ebfbf02f9c1c3b61388a8e3
bogsti <christoffer.bogsti@telia.no>
2020-08-28 10:02:56 +0200

    LoadingPebble css should not controll positioning

--- component-lib/src/atoms/LoadingPebble/LoadingPebble.stories.tsx
+++ component-lib/src/atoms/LoadingPebble/LoadingPebble.stories.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { LoadingPebble } from './LoadingPebble';
+import LoadingPebble from './LoadingPebble';
 
 export default {
   title: 'Component library/Atoms/LoadingPebble',
   component: LoadingPebble,
 };
 
-export const Default = () => (
-  <>
-    <h1>LoadingPebble</h1>
-
-    <LoadingPebble />
-  </>
-);
+export const Default = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', height: '50vh'}}>
+      <LoadingPebble />
+    </div>
+  )
+};
