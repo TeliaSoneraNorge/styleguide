bc7cd38a31db04d9c2136b796936f8aed6f0e8ed
Helene Grini <35767732+HeleneGrini@users.noreply.github.com>
2020-09-03 10:18:54 +0200

    Add new Modal component
    * Add modal components
    
    * Add hook to trap focus in a container -- make modal accessible
    
    * Add stories
    
    * Prefix classnames with .telia

---
+++ component-lib/src/molecules/Modal/ModalBody.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const ModalBody: React.FC = props => {
+  return <div className="telia-modal-body">{props.children}</div>;
+};
---
+++ component-lib/src/molecules/Modal/getModalRoot.tsx
@@ -0,0 +1,13 @@
+export const getModalRoot = () => {
+  const className = 'telia-modal-root';
+
+  const portalContainer = document.querySelectorAll<HTMLDivElement>(`div[class=${className}]`);
+  if (portalContainer && portalContainer.length >= 1) {
+    return portalContainer[0];
+  } else {
+    const element = document.createElement('div');
+    element.className = className;
+    document.body.appendChild(element);
+    return element;
+  }
+};
