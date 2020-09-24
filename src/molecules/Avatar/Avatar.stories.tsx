8288b2c9f0e0b9d1f67aeba8ebfb257ce85a1b9d
Helene Grini <helenegrini96@gmail.com>
2020-09-24 10:47:02 +0200

    display all avatars in one story

--- src/molecules/Avatar/Avatar.stories.tsx
+++ src/molecules/Avatar/Avatar.stories.tsx
@@ -10,58 +10,60 @@ export default {
 
 export const Default = ({}) => {
   return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ paddingRight: '1rem' }}>
-        <h3>Default</h3>
-        <Avatar text="HG" />
-        <Avatar icon="abroad" />
-        <Avatar img={images.hero1} alt="user" />
-      </div>
+    <div>
+      <div style={{ display: 'flex' }}>
+        <div style={{ paddingRight: '1rem' }}>
+          <h3>Default</h3>
+          <Avatar text="HG" />
+          <Avatar icon="abroad" />
+          <Avatar img={images.hero1} alt="user" />
+        </div>
 
-      <div style={{ paddingRight: '1rem' }}>
-        <h3>Button</h3>
-        <Avatar text="HG" onClick={action('Button avatar with text')} />
-        <Avatar icon="abroad" onClick={action('Button avatar with icon')} />
-        <Avatar img={images.hero1} onClick={action('Button avatar with image')} alt="user" />
-      </div>
+        <div style={{ paddingRight: '1rem' }}>
+          <h3>Button</h3>
+          <Avatar text="HG" onClick={action('Button avatar with text')} />
+          <Avatar icon="abroad" onClick={action('Button avatar with icon')} />
+          <Avatar img={images.hero1} onClick={action('Button avatar with image')} alt="user" />
+        </div>
 
-      <div style={{ paddingRight: '1rem' }}>
-        <h3>Anchor</h3>
-        <Avatar text="HG" href="my/path" onClick={action('Anchor avatar with text')} />
-        <Avatar icon="abroad" href="my/path" onClick={action('Anchor avatar with icon')} />
-        <Avatar img={images.hero1} href="my/path" onClick={action('Anchor avatar with image')} alt="user" />
+        <div style={{ paddingRight: '1rem' }}>
+          <h3>Anchor</h3>
+          <Avatar text="HG" href="my/path" onClick={action('Anchor avatar with text')} />
+          <Avatar icon="abroad" href="my/path" onClick={action('Anchor avatar with icon')} />
+          <Avatar img={images.hero1} href="my/path" onClick={action('Anchor avatar with image')} alt="user" />
+        </div>
       </div>
-    </div>
-  );
-};
-export const Compact = ({}) => {
-  return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ paddingRight: '1rem' }}>
-        <h3>Default</h3>
-        <Avatar text="HG" compact={true} />
-        <Avatar icon="abroad" compact={true} />
-        <Avatar img={images.hero1} compact={true} alt="user" />
-      </div>
+      <br />
+      <h2 style={{ marginBottom: 0 }}>
+        <code>size='compact'</code>
+      </h2>
+      <div style={{ display: 'flex' }}>
+        <div style={{ paddingRight: '1rem' }}>
+          <h3>Default</h3>
+          <Avatar text="HG" compact={true} />
+          <Avatar icon="abroad" compact={true} />
+          <Avatar img={images.hero1} compact={true} alt="user" />
+        </div>
 
-      <div style={{ paddingRight: '1rem' }}>
-        <h3>Button</h3>
-        <Avatar text="HG" onClick={action('Button avatar with text')} compact={true} />
-        <Avatar icon="abroad" onClick={action('Button avatar with icon')} compact={true} />
-        <Avatar img={images.hero1} onClick={action('Button avatar with image')} alt="user" compact={true} />
-      </div>
+        <div style={{ paddingRight: '1rem' }}>
+          <h3>Button</h3>
+          <Avatar text="HG" onClick={action('Button avatar with text')} compact={true} />
+          <Avatar icon="abroad" onClick={action('Button avatar with icon')} compact={true} />
+          <Avatar img={images.hero1} onClick={action('Button avatar with image')} alt="user" compact={true} />
+        </div>
 
-      <div style={{ paddingRight: '1rem' }}>
-        <h3>Anchor</h3>
-        <Avatar text="HG" href="my/path" onClick={action('Anchor avatar with text')} compact={true} />
-        <Avatar icon="abroad" href="my/path" onClick={action('Anchor avatar with icon')} compact={true} />
-        <Avatar
-          img={images.hero1}
-          href="my/path"
-          onClick={action('Anchor avatar with image')}
-          alt="user"
-          compact={true}
-        />
+        <div style={{ paddingRight: '1rem' }}>
+          <h3>Anchor</h3>
+          <Avatar text="HG" href="my/path" onClick={action('Anchor avatar with text')} compact={true} />
+          <Avatar icon="abroad" href="my/path" onClick={action('Anchor avatar with icon')} compact={true} />
+          <Avatar
+            img={images.hero1}
+            href="my/path"
+            onClick={action('Anchor avatar with image')}
+            alt="user"
+            compact={true}
+          />
+        </div>
       </div>
     </div>
   );
