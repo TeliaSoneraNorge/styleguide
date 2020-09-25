3e3713e54cb2f8007d64e14a51ce963ad6ee2c50
mariannemagnussen <marianne_magnussen@icloud.com>
2020-09-25 07:34:08 +0200

    Create business button (#691)
    * Create business button
    
    * Change class names and add compact feature
    
    * Add disable-feature for link button
    
    * Rename Button type to button kind
    
    * Add stories for compact button
    
    * Change compact property to size property
    
    * Refactor css classes and add props descriptions
    
    Co-authored-by: Marianne Magnussen <marianmagnussen@gmail.com>

---
+++ src/business/Button/Button.stories.tsx
@@ -0,0 +1,179 @@
+import React from 'react';
+import { Button } from './Button';
+
+export default {
+  component: Button,
+  title: 'Business/Button',
+};
+
+export const ButtonPrimary = () => (
+  <div>
+    <h2>Button Primary</h2>
+    Default button kind is Button primary
+    <br />
+    <h3>Normal state</h3>
+    <Button label="Button" />
+    <br />
+    <h3>Disabled state</h3>
+    <Button label="Button" disabled={true} />
+    <br />
+    <h3>Button with icon</h3>
+    <Button label="Button" icon={'add'} />
+    <br />
+    <h3>Button with icon right</h3>
+    <Button label="Button" icon={'arrow-right'} iconRight={true} />
+    <br />
+    <h3>Button ball (with icon and without label)</h3>
+    <Button icon={'add'} />
+    <br />
+    <h3>Button with action</h3>
+    <Button label="Click" onClick={() => alert('Hi')} />
+    <br />
+    <h3>Button with link </h3>
+    <Button label="Button" href="https://en.wikipedia.org/wiki/Success" />
+    <br />
+    <h3>Button with no parameters</h3>
+    <Button />
+    <br />
+  </div>
+);
+
+export const ButtonPrimaryText = () => (
+  <div>
+    <h3>Normal state</h3>
+    <Button kind={'primary-text'} label="Button" />
+    <br />
+    <h3>Disabled state</h3>
+    <Button kind={'primary-text'} label="Button" disabled={true} />
+    <br />
+    <h3>Button with icon</h3>
+    <Button kind={'primary-text'} label="Button" icon={'add'} />
+    <br />
+    <h3>Button with icon right</h3>
+    <Button kind={'primary-text'} label="Button" icon={'arrow-right'} iconRight={true} />
+    <br />
+    <h3>Button ball (with icon and without label)</h3>
+    <Button kind={'primary-text'} icon={'add'} />
+    <br />
+    <h3>Compact button</h3>
+    <Button kind={'primary-text'} label="Button" size="compact" />
+    <br />
+  </div>
+);
+
+export const ButtonSecondary = () => (
+  <div>
+    <h3>Normal state</h3>
+    <Button kind={'secondary'} label="Button" />
+    <br />
+    <h3>Disabled state</h3>
+    <Button kind={'secondary'} label="Button" disabled={true} />
+    <br />
+    <h3>Button with icon</h3>
+    <Button kind={'secondary'} label="Button" icon={'add'} />
+    <br />
+    <h3>Button with icon right</h3>
+    <Button kind={'secondary'} label="Button" icon={'arrow-right'} iconRight={true} />
+    <br />
+    <h3>Button ball (with icon and without label)</h3>
+    <Button kind={'secondary'} icon={'add'} />
+    <br />
+    <h3>Compact button</h3>
+    <Button kind={'secondary'} label="Button" size="compact" />
+  </div>
+);
+
+export const ButtonSecondaryText = () => (
+  <div>
+    <h3>Normal state</h3>
+    <Button kind={'secondary-text'} label="Button" />
+    <br />
+    <h3>Disabled state</h3>
+    <Button kind={'secondary-text'} label="Button" disabled={true} />
+    <br />
+    <h3>Button with icon</h3>
+    <Button kind={'secondary-text'} label="Button" icon={'add'} />
+    <br />
+    <h3>Button with icon right</h3>
+    <Button kind={'secondary-text'} label="Button" icon={'arrow-right'} iconRight={true} />
+    <br />
+    <h3>Button ball (with icon and without label)</h3>
+    <Button kind={'secondary-text'} icon={'add'} />
+  </div>
+);
+
+export const ButtonGhost = () => (
+  <div>
+    <h3>Normal state</h3>
+    <Button kind={'ghost'} label="Button" />
+    <br />
+    <h3>Disabled state</h3>
+    <Button kind={'ghost'} label="Button" disabled={true} />
+    <br />
+    <h3>Button with icon</h3>
+    <Button kind={'ghost'} label="Button" icon={'add'} />
+    <br />
+    <h3>Button with icon right</h3>
+    <Button kind={'ghost'} label="Button" icon={'arrow-right'} iconRight={true} />
+    <br />
+    <h3>Button ball (with icon and without label)</h3>
+    <Button kind={'ghost'} icon={'add'} />
+  </div>
+);
+
+export const ButtonPositive = () => (
+  <div>
+    <h3>Normal state</h3>
+    <Button kind={'positive'} label="Button" />
+    <br />
+    <h3>Disabled state</h3>
+    <Button kind={'positive'} label="Button" disabled={true} />
+    <br />
+    <h3>Button with icon</h3>
+    <Button kind={'positive'} label="Button" icon={'add'} />
+    <br />
+    <h3>Button with icon right</h3>
+    <Button kind={'positive'} label="Button" icon={'arrow-right'} iconRight={true} />
+    <br />
+    <h3>Button ball (with icon and without label)</h3>
+    <Button kind={'positive'} icon={'add'} />
+  </div>
+);
+
+export const ButtonNegative = () => (
+  <div>
+    <h3>Normal state</h3>
+    <Button kind={'negative'} label="Button" />
+    <br />
+    <h3>Disabled state</h3>
+    <Button kind={'negative'} label="Button" disabled={true} />
+    <br />
+    <h3>Button with icon</h3>
+    <Button kind={'negative'} label="Button" icon={'add'} />
+    <br />
+    <h3>Button with icon right</h3>
+    <Button kind={'negative'} label="Button" icon={'arrow-right'} iconRight={true} />
+    <br />
+    <h3>Button ball (with icon and without label)</h3>
+    <Button kind={'negative'} icon={'add'} />
+  </div>
+);
+
+export const ButtonCompact = () => (
+  <div>
+    <h3>Normal state</h3>
+    <Button label="Button" size="compact" />
+    <br />
+    <h3>Disabled state</h3>
+    <Button label="Button" disabled={true} size="compact" />
+    <br />
+    <h3>Button with icon</h3>
+    <Button label="Button" icon={'add'} size="compact" />
+    <br />
+    <h3>Button with icon right</h3>
+    <Button label="Button" icon={'arrow-right'} iconRight={true} size="compact" />
+    <br />
+    <h3>Button ball (with icon and without label)</h3>
+    <Button icon={'add'} size="compact" />
+  </div>
+);
