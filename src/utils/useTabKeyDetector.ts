2e32a6a74418681142598d99b267efac11bfbdf4
Helene Grini <helenegrini96@gmail.com>
2020-09-28 13:32:45 +0200

    Add hook to detect tabbing and apply classname to ref

---
+++ src/utils/useTabKeyDetector.ts
@@ -0,0 +1,35 @@
+import { useRef } from 'react';
+
+/**
+ * Detect usage of tab keys and add class .user-is-tabbing to the ref.
+ */
+
+// export const useTabKeyDetector = () => {
+//   const ref = useRef(null);
+//   return useInnerTabKeyDetector(ref);
+// };
+
+export function useTabKeyDetector() {
+  const ref = useRef<any>(null);
+  function mouseDownHandler() {
+    if (ref.current) {
+      ref.current.classList.remove('user-is-tabbing');
+    }
+    window.removeEventListener('mousedown', mouseDownHandler);
+    window.addEventListener('keydown', tabKeyHandler);
+  }
+
+  function tabKeyHandler(e: KeyboardEvent) {
+    if (e.keyCode === 9) {
+      if (ref.current) {
+        ref.current.classList.add('user-is-tabbing');
+      }
+      window.removeEventListener('keydown', tabKeyHandler);
+      window.addEventListener('mousedown', mouseDownHandler);
+    }
+  }
+
+  window.addEventListener('keydown', tabKeyHandler);
+
+  return ref;
+}
