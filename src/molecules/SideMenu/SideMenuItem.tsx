bf3a2ce6101f7abc619cb21ffbf9259ac28bcfe4
Helene Grini <35767732+HeleneGrini@users.noreply.github.com>
2020-10-07 13:28:09 +0200

    A11y and documentation improvements (#703)
    * A11y enhancements. Support setting the side menu item to a div, rather than li. Add doc blocks
    
    * prevent deafult when href and onclick. Support native linking when no onClick is provided

--- src/molecules/SideMenu/SideMenuItem.tsx
+++ src/molecules/SideMenu/SideMenuItem.tsx
@@ -5,27 +5,55 @@ import { Avatar } from '../../index';
 import { Button } from '../../business';
 
 type Props = {
+  label?: string;
+  /**
+   * Props to render an avatar in the item.
+   * See Avatar.tsx
+   */
   avatar?: {
     text: string;
   };
+  /**
+   * Icon to display in front of item
+   */
   icon?: IconDefinition;
-  label?: string;
+  /**
+   * Whether the item is currently active.
+   * Eg. we are on a page corresponding to an entry from the meny
+   */
   active?: boolean;
+  /**
+   * Renders the item as an anchor tag.
+   */
   href?: string;
+
+  /**
+   * Action to execute on click item
+   */
   onClick: () => void;
+
+  /**
+   * Wheter to render the iten as an li or div.
+   * The outermost tag should always be an <li>.
+   *
+   * If you need custom logic and markup causing the SideMenuItem
+   * to be rendered deeper in the SideMenu, set the  tag='div' on SideMenuItem
+   * and set your custom wrapper to an <li>.
+   */
+  tag?: 'div' | 'li';
 };
 
 export const SideMenuItem: React.FC<Props> = props => {
   const Tag = props.href ? 'a' : 'button';
-
+  const WrapperTag = props.tag ? props.tag : 'li';
   const handleClick = (e: React.SyntheticEvent) => {
-    if (props.href) {
+    if (props.href && props.onClick) {
       e.preventDefault();
     }
     props.onClick();
   };
   return (
-    <li>
+    <WrapperTag>
       <div
         className={cn('telia-side-menu-item telia-side-menu-item--compact', {
           'telia-side-menu-item--active': props.active,
@@ -58,6 +86,6 @@ export const SideMenuItem: React.FC<Props> = props => {
         {props.icon && <Icon className="telia-side-menu-item-icon" icon={props.icon} />}
         <div className="telia-side-menu-item-label">{props.label}</div>
       </Tag>
-    </li>
+    </WrapperTag>
   );
 };
