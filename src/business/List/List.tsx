b0b734cc4e02075517bf5905dafcd8a6ebc3f0de
Helene Grini <35767732+HeleneGrini@users.noreply.github.com>
2020-10-26 12:08:11 +0100

    Apply classname prop (#724)
    * Add classname prop to List
    
    * Set list item lable type to react node to support elements
    
    * apply classname

--- src/business/List/List.tsx
+++ src/business/List/List.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ListItemProps } from './ListItem';
 import { ListStyleContext, ListStyle } from './utils';
+import cn from 'classnames';
 
 type ListItemElement = React.ReactElement<ListItemProps>;
 
@@ -9,9 +10,9 @@ type Props = ListStyle & {
   children: ListItemElement | Array<ListItemElement>;
 };
 
-export const List = ({ children, ...styleProps }: Props) => (
+export const List = ({ children, className, ...styleProps }: Props) => (
   <ListStyleContext.Provider value={styleProps}>
-    <section className="telia-list">
+    <section className={cn('telia-list', className)}>
       <ul className="telia-list__list">{children}</ul>
     </section>
   </ListStyleContext.Provider>
