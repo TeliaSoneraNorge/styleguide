0012335b11d65b35beb9a322d960836dbbea3c60
mariannemagnussen <marianne_magnussen@icloud.com>
2020-10-29 10:49:21 +0100

    Feature/dropdown classname (#731)
    * Add className to dropdown menu
    
    * Add white-space prewrap to dropdown item
    
    Co-authored-by: Marianne Magnussen <marianmagnussen@gmail.com>

--- src/molecules/Dropdown/DropdownMenu.tsx
+++ src/molecules/Dropdown/DropdownMenu.tsx
@@ -5,17 +5,22 @@ import cs from 'classnames';
 type DropdownMenuProps = {
   position?: 'right' | 'left';
   wrapContent?: boolean;
+  className?: string;
 };
 
 export const DropdownMenu: React.FC<DropdownMenuProps> = props => {
   const { menuRef, open, allItems } = useDropdownContext();
   return (
     <div
-      className={cs('telia-dropdown-content', {
-        'telia-dropdown-content__nowrap': !props.wrapContent,
-        open: open,
-        right: props.position === 'right',
-      })}
+      className={cs(
+        'telia-dropdown-content',
+        {
+          'telia-dropdown-content__nowrap': !props.wrapContent,
+          open: open,
+          right: props.position === 'right',
+        },
+        props.className
+      )}
       ref={menuRef}
     >
       {allItems}
