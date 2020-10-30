e4a4cf4d1c58f6f5b9c60c704e2ca3e4ed1d12b6
Helene Grini <helenegrini96@gmail.com>
2020-10-30 08:04:42 +0100

    write out docs

--- src/business/Button/Button.stories.tsx
+++ src/business/Button/Button.stories.tsx
@@ -46,21 +46,93 @@ export const ButtonPrimary = () => (
     <h2>Button Primary</h2>
     Default button kind is Button primary
     <br />
-    {renderButtons('primary')}
+    <h3>Normal state</h3>
+    <Button label="Button" />
+    <br />
+    <h3>Disabled state</h3>
+    <Button label="Button" disabled={true} />
+    <br />
+    <h3>Button with icon</h3>
+    <Button label="Button" icon={'add'} />
+    <br />
+    <h3>Button with icon right</h3>
+    <Button label="Button" icon={'arrow-right'} iconRight={true} />
+    <br />
+    <h3>Button ball (with icon and without label)</h3>
+    <Button icon={'add'} />
+    <br />
+    <h3>Button with action</h3>
+    <Button label="Click" onClick={() => alert('Hi')} />
+    <br />
+    <h3>Button with link </h3>
+    <Button label="Button" href="https://en.wikipedia.org/wiki/Success" />
+    <h3>Button with link and target props</h3>
+    <Button label="Button" href="https://en.wikipedia.org/wiki/Success" target="_blank" />
+    <br />
+    <h3>Button with no parameters</h3>
+    <Button />
   </div>
 );
 
 export const ButtonPrimaryText = () => (
   <div>
     <h2>Button Primary Text</h2>
-    {renderButtons('primary-text')}
+    <h3>Normal state</h3>
+    <Button kind="primary-text" label="Button" />
+    <br />
+    <h3>Disabled state</h3>
+    <Button kind="primary-text" label="Button" disabled={true} />
+    <br />
+    <h3>Button with icon</h3>
+    <Button kind="primary-text" label="Button" icon={'add'} />
+    <br />
+    <h3>Button with icon right</h3>
+    <Button kind="primary-text" label="Button" icon={'arrow-right'} iconRight={true} />
+    <br />
+    <h3>Button ball (with icon and without label)</h3>
+    <Button kind="primary-text" icon={'add'} />
+    <br />
+    <h3>Button with action</h3>
+    <Button kind="primary-text" label="Click" onClick={() => alert('Hi')} />
+    <br />
+    <h3>Button with link </h3>
+    <Button kind="primary-text" label="Button" href="https://en.wikipedia.org/wiki/Success" />
+    <h3>Button with link and target props</h3>
+    <Button kind="primary-text" label="Button" href="https://en.wikipedia.org/wiki/Success" target="_blank" />
+    <br />
+    <h3>Button with no parameters</h3>
+    <Button kind="primary-text" />
   </div>
 );
 
 export const ButtonSecondary = () => (
   <div>
     <h2>Button Secondary</h2>
-    {renderButtons('secondary')}
+    <h3>Normal state</h3>
+    <Button kind="secondary" label="Button" />
+    <br />
+    <h3>Disabled state</h3>
+    <Button kind="secondary" label="Button" disabled={true} />
+    <br />
+    <h3>Button with icon</h3>
+    <Button kind="secondary" label="Button" icon={'add'} />
+    <br />
+    <h3>Button with icon right</h3>
+    <Button kind="secondary" label="Button" icon={'arrow-right'} iconRight={true} />
+    <br />
+    <h3>Button ball (with icon and without label)</h3>
+    <Button kind="secondary" icon={'add'} />
+    <br />
+    <h3>Button with action</h3>
+    <Button kind="secondary" label="Click" onClick={() => alert('Hi')} />
+    <br />
+    <h3>Button with link </h3>
+    <Button kind="secondary" label="Button" href="https://en.wikipedia.org/wiki/Success" />
+    <h3>Button with link and target props</h3>
+    <Button kind="secondary" label="Button" href="https://en.wikipedia.org/wiki/Success" target="_blank" />
+    <br />
+    <h3>Button with no parameters</h3>
+    <Button kind="secondary" />
   </div>
 );
 
@@ -68,7 +140,31 @@ export const ButtonSecondaryText = () => (
   <div>
     <div>
       <h2>Button Secondary Text</h2>
-      {renderButtons('secondary-text')}
+      <h3>Normal state</h3>
+      <Button kind="secondary-text" label="Button" />
+      <br />
+      <h3>Disabled state</h3>
+      <Button kind="secondary-text" label="Button" disabled={true} />
+      <br />
+      <h3>Button with icon</h3>
+      <Button kind="secondary-text" label="Button" icon={'add'} />
+      <br />
+      <h3>Button with icon right</h3>
+      <Button kind="secondary-text" label="Button" icon={'arrow-right'} iconRight={true} />
+      <br />
+      <h3>Button ball (with icon and without label)</h3>
+      <Button kind="secondary-text" icon={'add'} />
+      <br />
+      <h3>Button with action</h3>
+      <Button kind="secondary-text" label="Click" onClick={() => alert('Hi')} />
+      <br />
+      <h3>Button with link </h3>
+      <Button kind="secondary-text" label="Button" href="https://en.wikipedia.org/wiki/Success" />
+      <h3>Button with link and target props</h3>
+      <Button kind="secondary-text" label="Button" href="https://en.wikipedia.org/wiki/Success" target="_blank" />
+      <br />
+      <h3>Button with no parameters</h3>
+      <Button kind="secondary-text" />
     </div>
   </div>
 );
@@ -77,7 +173,31 @@ export const ButtonGhost = () => (
   <div>
     <div>
       <h2>Button Ghost</h2>
-      {renderButtons('ghost')}
+      <h3>Normal state</h3>
+      <Button kind="ghost" label="Button" />
+      <br />
+      <h3>Disabled state</h3>
+      <Button kind="ghost" label="Button" disabled={true} />
+      <br />
+      <h3>Button with icon</h3>
+      <Button kind="ghost" label="Button" icon={'add'} />
+      <br />
+      <h3>Button with icon right</h3>
+      <Button kind="ghost" label="Button" icon={'arrow-right'} iconRight={true} />
+      <br />
+      <h3>Button ball (with icon and without label)</h3>
+      <Button kind="ghost" icon={'add'} />
+      <br />
+      <h3>Button with action</h3>
+      <Button kind="ghost" label="Click" onClick={() => alert('Hi')} />
+      <br />
+      <h3>Button with link </h3>
+      <Button kind="ghost" label="Button" href="https://en.wikipedia.org/wiki/Success" />
+      <h3>Button with link and target props</h3>
+      <Button kind="ghost" label="Button" href="https://en.wikipedia.org/wiki/Success" target="_blank" />
+      <br />
+      <h3>Button with no parameters</h3>
+      <Button kind="ghost" />
     </div>
   </div>
 );
@@ -85,20 +205,92 @@ export const ButtonGhost = () => (
 export const ButtonPositive = () => (
   <div>
     <h2>Button Positive</h2>
-    {renderButtons('positive')}
+    <h3>Normal state</h3>
+    <Button kind="positive" label="Button" />
+    <br />
+    <h3>Disabled state</h3>
+    <Button kind="positive" label="Button" disabled={true} />
+    <br />
+    <h3>Button with icon</h3>
+    <Button kind="positive" label="Button" icon={'add'} />
+    <br />
+    <h3>Button with icon right</h3>
+    <Button kind="positive" label="Button" icon={'arrow-right'} iconRight={true} />
+    <br />
+    <h3>Button ball (with icon and without label)</h3>
+    <Button kind="positive" icon={'add'} />
+    <br />
+    <h3>Button with action</h3>
+    <Button kind="positive" label="Click" onClick={() => alert('Hi')} />
+    <br />
+    <h3>Button with link </h3>
+    <Button kind="positive" label="Button" href="https://en.wikipedia.org/wiki/Success" />
+    <h3>Button with link and target props</h3>
+    <Button kind="positive" label="Button" href="https://en.wikipedia.org/wiki/Success" target="_blank" />
+    <br />
+    <h3>Button with no parameters</h3>
+    <Button kind="positive" />
   </div>
 );
 
 export const ButtonNegative = () => (
   <div>
     <h2>Button Negative</h2>
-    {renderButtons('negative')}
+    <h3>Normal state</h3>
+    <Button kind="negative" label="Button" />
+    <br />
+    <h3>Disabled state</h3>
+    <Button kind="negative" label="Button" disabled={true} />
+    <br />
+    <h3>Button with icon</h3>
+    <Button kind="negative" label="Button" icon={'add'} />
+    <br />
+    <h3>Button with icon right</h3>
+    <Button kind="negative" label="Button" icon={'arrow-right'} iconRight={true} />
+    <br />
+    <h3>Button ball (with icon and without label)</h3>
+    <Button kind="negative" icon={'add'} />
+    <br />
+    <h3>Button with action</h3>
+    <Button kind="negative" label="Click" onClick={() => alert('Hi')} />
+    <br />
+    <h3>Button with link </h3>
+    <Button kind="negative" label="Button" href="https://en.wikipedia.org/wiki/Success" />
+    <h3>Button with link and target props</h3>
+    <Button kind="negative" label="Button" href="https://en.wikipedia.org/wiki/Success" target="_blank" />
+    <br />
+    <h3>Button with no parameters</h3>
+    <Button kind="negative" />
   </div>
 );
 
 export const ButtonCompact = () => (
   <div>
     <h2>Button compact</h2>
-    {renderButtons('primary', 'compact')}
+    <h3>Normal state</h3>
+    <Button size="compact" label="Button" />
+    <br />
+    <h3>Disabled state</h3>
+    <Button size="compact" label="Button" disabled={true} />
+    <br />
+    <h3>Button with icon</h3>
+    <Button size="compact" label="Button" icon={'add'} />
+    <br />
+    <h3>Button with icon right</h3>
+    <Button size="compact" label="Button" icon={'arrow-right'} iconRight={true} />
+    <br />
+    <h3>Button ball (with icon and without label)</h3>
+    <Button size="compact" icon={'add'} />
+    <br />
+    <h3>Button with action</h3>
+    <Button size="compact" label="Click" onClick={() => alert('Hi')} />
+    <br />
+    <h3>Button with link </h3>
+    <Button size="compact" label="Button" href="https://en.wikipedia.org/wiki/Success" />
+    <h3>Button with link and target props</h3>
+    <Button size="compact" label="Button" href="https://en.wikipedia.org/wiki/Success" target="_blank" />
+    <br />
+    <h3>Button with no parameters</h3>
+    <Button size="compact" />
   </div>
 );
