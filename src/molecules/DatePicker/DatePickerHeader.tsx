52014390729cbe5ea6c2825926f813f20bc48499
Helene Grini <helenegrini96@gmail.com>
2020-11-04 19:16:53 +0100

    work

--- src/molecules/DatePicker/DatePickerHeader.tsx
+++ src/molecules/DatePicker/DatePickerHeader.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { useDatePicker } from './DatePicker';
+import { Button } from '../../business/Button';
 
 export const DatePickerHeader = () => {
-  const context = useDatePicker();
+  const { setMonth, setYear, month, year } = useDatePicker();
 
+  const prev = () => {
+    if (month === 0) {
+      setMonth(11);
+      setYear(year - 1);
+    }
+    if (month === 11) {
+      setMonth(0);
+      setYear(year + 1);
+    }
+  };
   return (
     <div className="telia-date-picker--header">
-      {monthNames[context.month]} {context.year}
+      <Button onClick={prev} icon="arrow-left" kind="secondary-text" /> {monthNames[month]} {year}
+      <Button icon="arrow-right" kind="secondary-text" />
     </div>
   );
 };
