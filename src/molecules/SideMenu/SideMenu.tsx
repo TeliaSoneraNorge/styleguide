de637a69c3559aa689ca4dfda022e95050310241
Helene Grini <helenegrini96@gmail.com>
2020-11-19 21:19:34 +0100

    remove header item and adjust styles

--- src/molecules/SideMenu/SideMenu.tsx
+++ src/molecules/SideMenu/SideMenu.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import cn from 'classnames';
 
-interface Props {
+type Props = {
   'aria-labelledby'?: string;
-}
-export const SideMenu: React.FC<Props> = props => {
+  className?: string;
+};
+export const SideMenu: React.FC<Props> = (props) => {
   return (
-    <nav className="telia-side-menu" ariaaria-label={props['aria-labelledby']}>
+    <nav className={cn('telia-side-menu', props.className)} ariaaria-label={props['aria-labelledby']}>
       <ul>{props.children}</ul>
     </nav>
   );
 };
 
-export const SideMenuTop: React.FC = props => (
+export const SideMenuTop: React.FC = (props) => (
   <li className="telia-side-menu-top">
     <ul>{props.children}</ul>
   </li>
 );
 
-export const SideMenuBottom: React.FC = props => (
+export const SideMenuBottom: React.FC = (props) => (
   <li className="telia-side-menu-bottom">
     <ul>{props.children}</ul>
   </li>
