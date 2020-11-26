c0f216c6e8d7b256fef694e332e0c3a60e2d6a26
Alfred Birketvedt <alfredbirk@gmail.com>
2020-11-26 15:22:04 +0100

    SubscriptionLinesAccordion changes (#818)
    * SubscriptionLinesAccordion changes
    
    * Small style tweaks
    
    Co-authored-by: Joakim Kristiansen <joakikr@gmail.com>

--- src/molecules/SubscriptionLinesAccordion/SubscriptionLinesAccordion.jsx
+++ src/molecules/SubscriptionLinesAccordion/SubscriptionLinesAccordion.jsx
@@ -2,8 +2,6 @@ import React, { useRef, useState, useEffect } from 'react';
 import cn from 'classnames';
 import Heading from '../../atoms/Heading';
 import { Icon } from '../../atoms/Icon';
-import RadioButton from '../../atoms/RadioButton';
-import Button from '../../atoms/Button';
 
 const SubscriptionLinesAccordion = ({
   id,
@@ -18,21 +16,16 @@ const SubscriptionLinesAccordion = ({
   priceInfo,
   numberOfSubscriptions,
   feature,
-  disclaimers,
   scrollToOnOpen = false,
   children,
   className,
   description1,
   description2,
-  showCalculationText = 'Se utregning',
-  isSelected = false,
+  footer,
   onOpen = () => {},
-  onSelect = () => {},
-  onClickShowCalculation = () => {},
   style,
 }) => {
   const ref = useRef();
-  const [isHoveringRadioButton, setIsHoveringRadioButton] = useState(false);
 
   useEffect(() => {
     if (ref.current && isExpanded && scrollToOnOpen) {
@@ -43,43 +36,21 @@ const SubscriptionLinesAccordion = ({
     }
   }, [isExpanded, scrollToOnOpen]);
 
-  const handleOnSelect = () => {
-    onSelect();
-  };
-
-  const handleOnOpen = () => {
-    if (!isHoveringRadioButton) {
-      onOpen();
-    }
-  };
-
-  const handleOnClickShowCalculation = (event) => {
-    event.stopPropagation();
-    onClickShowCalculation();
-  };
-
-  const Footer = () => (
+  const FooterComponent = () => (
     <div
       className={cn('subscription-lines-accordion__footer-container', {
         'subscription-lines-accordion__footer-container--inverted': isInverted,
-        'subscription-lines-accordion__footer-container--inverted-expanded': isExpanded && isInverted,
         'subscription-lines-accordion__footer-container--expanded': isExpanded,
       })}
     >
-      <hr
-        className={cn('subscription-lines-accordion__seperation-line', {
-          'subscription-lines-accordion__seperation-line--inverted': isInverted,
-        })}
-      />
-      <div className="subscription-lines-accordion__footer subscription-lines-accordion__desktop-only">
-        <Button
-          component="div"
-          onClick={(event) => handleOnClickShowCalculation(event)}
-          href="#"
-          kind="link"
-          text={showCalculationText}
+      {!(isInverted && isExpanded) && (
+        <hr
+          className={cn('subscription-lines-accordion__seperation-line', {
+            'subscription-lines-accordion__seperation-line--inverted': isInverted,
+          })}
         />
-      </div>
+      )}
+      <div className="subscription-lines-accordion__footer subscription-lines-accordion__desktop-only">{footer}</div>
       <div className="subscription-lines-accordion__footer subscription-lines-accordion__mobile-only">
         <div className="subscription-lines-accordion__price-container">
           <div
@@ -98,6 +69,7 @@ const SubscriptionLinesAccordion = ({
               ))}
           </div>
         </div>
+        {footer}
       </div>
     </div>
   );
@@ -113,63 +85,64 @@ const SubscriptionLinesAccordion = ({
         'subscription-lines-accordion--inverted': isInverted,
       })}
     >
-      <div
-        className={cn('subscription-lines-accordion__main-container', {
-          'subscription-lines-accordion__main-container--expanded': isExpanded,
-        })}
-      >
-        <div className="subscription-lines-accordion__radio-button-container">
-          <RadioButton
-            onMouseEnter={() => setIsHoveringRadioButton(true)}
-            onMouseLeave={() => setIsHoveringRadioButton(false)}
-            onChange={() => handleOnSelect()}
-            checked={isSelected}
-            className="subscription-lines-accordion__radio-button-content"
-          >
-            <div className="subscription-lines-accordion__header-first-row">
-              {dataAmountIcon ? (
-                <div className="subscription-lines-accordion__heading-container">
-                  <div className="subscription-lines-accordion__desktop-only">
-                    <div className="subscription-lines-accordion__heading-icon">
-                      {dataAmountIcon}
-                      <Heading
-                        className="subscription-lines-accordion__heading-unit"
-                        tag="h2"
-                        size="s"
-                        text={dataUnit}
-                      />
-                    </div>
-                  </div>
-                  <div className="subscription-lines-accordion__heading-icon-container subscription-lines-accordion__mobile-only">
-                    <div className="subscription-lines-accordion__heading-icon">{dataAmountIcon}</div>
-                    <Heading className="subscription-lines-accordion__heading-name" tag="h2" size="s" text={dataUnit} />
+      <button className="subscription-lines-accordion__container-button" onClick={onOpen}>
+        <div
+          className={cn('subscription-lines-accordion__main-container', {
+            'subscription-lines-accordion__main-container--expanded': isExpanded,
+          })}
+        >
+          <div className="subscription-lines-accordion__header-first-row">
+            {dataAmountIcon ? (
+              <div className="subscription-lines-accordion__heading-container">
+                <div className="subscription-lines-accordion__desktop-only">
+                  <div className="subscription-lines-accordion__heading-icon">
+                    {dataAmountIcon}
+                    <Heading className="subscription-lines-accordion__heading-unit" tag="h2" size="s" text={dataUnit} />
                   </div>
                 </div>
-              ) : (
-                <div className="subscription-lines-accordion__heading-container">
-                  <div className="subscription-lines-accordion__mobile-only">
-                    <Heading className="subscription-lines-accordion__heading-name" tag="h2" size="s">
-                      {dataAmount}
-                      {dataUnit}
-                      {extraDataString && (
-                        <div className="subscription-lines-accordion__heading-extra-same-line">{extraDataString}</div>
-                      )}
-                    </Heading>
-                  </div>
-                  <div className="subscription-lines-accordion__desktop-only">
-                    <Heading className="subscription-lines-accordion__heading-name" tag="h2" size="s" text={dataAmount}>
-                      <div className="subscription-lines-accordion__heading-unit">{dataUnit}</div>
-                    </Heading>
+                <div className="subscription-lines-accordion__heading-icon-container subscription-lines-accordion__mobile-only">
+                  <div className="subscription-lines-accordion__heading-icon">{dataAmountIcon}</div>
+                  <Heading className="subscription-lines-accordion__heading-name" tag="h2" size="s" text={dataUnit} />
+                </div>
+              </div>
+            ) : (
+              <div className="subscription-lines-accordion__heading-container">
+                <div className="subscription-lines-accordion__mobile-only">
+                  <Heading className="subscription-lines-accordion__heading-name" tag="h2" size="s">
+                    {dataAmount}
+                    {dataUnit}
                     {extraDataString && (
-                      <div className="subscription-lines-accordion__heading-extra">{extraDataString}</div>
+                      <div className="subscription-lines-accordion__heading-extra-same-line">{extraDataString}</div>
                     )}
-                  </div>
+                  </Heading>
                 </div>
-              )}
-              <div className="subscription-lines-accordion__description">
-                <div className="subscription-lines-accordion__description2 subscription-lines-accordion__desktop-only">
-                  {description1}
+                <div className="subscription-lines-accordion__desktop-only">
+                  <Heading className="subscription-lines-accordion__heading-name" tag="h2" size="s" text={dataAmount}>
+                    <div className="subscription-lines-accordion__heading-unit">{dataUnit}</div>
+                  </Heading>
+                  {extraDataString && (
+                    <div className="subscription-lines-accordion__heading-extra">{extraDataString}</div>
+                  )}
                 </div>
+              </div>
+            )}
+            <div className="subscription-lines-accordion__description">
+              <div className="subscription-lines-accordion__description2 subscription-lines-accordion__desktop-only">
+                {description1}
+              </div>
+              <div
+                className={cn('subscription-lines-accordion__description1 subscription-lines-accordion__desktop-only', {
+                  'subscription-lines-accordion__description1--inverted': isInverted,
+                })}
+              >
+                {description2}
+              </div>
+            </div>
+            <div>
+              <div className="subscription-lines-accordion__vertical-line"></div>
+            </div>
+            <div className="subscription-lines-accordion__aside">
+              <div className="subscription-lines-accordion__price-container">
                 <div
                   className={cn(
                     'subscription-lines-accordion__description1 subscription-lines-accordion__desktop-only',
@@ -178,55 +151,34 @@ const SubscriptionLinesAccordion = ({
                     }
                   )}
                 >
-                  {description2}
+                  Totalt for{' '}
+                  <span className="subscription-lines-accordion__description1--bold">{numberOfSubscriptions} stk</span>.
                 </div>
-              </div>
-              <div>
-                <div className="subscription-lines-accordion__vertical-line"></div>
-              </div>
-              <div className="subscription-lines-accordion__aside">
-                <div className="subscription-lines-accordion__price-container">
-                  <div
-                    className={cn(
-                      'subscription-lines-accordion__description1 subscription-lines-accordion__desktop-only',
-                      {
-                        'subscription-lines-accordion__description1--inverted': isInverted,
-                      }
-                    )}
-                  >
-                    Totalt for{' '}
-                    <span className="subscription-lines-accordion__description1--bold">
-                      {numberOfSubscriptions} stk
-                    </span>
-                    .
-                  </div>
-                  <div className="subscription-lines-accordion__desktop-only">
-                    <span className="subscription-lines-accordion__price">{formatPrice(price)}</span>
-                    {priceInfo &&
-                      priceInfo.map((info) => (
-                        <span key={info} className="subscription-lines-accordion__price-info">
-                          {info}
-                        </span>
-                      ))}
-                  </div>
+                <div className="subscription-lines-accordion__desktop-only">
+                  <span className="subscription-lines-accordion__price">{formatPrice(price)}</span>
+                  {priceInfo &&
+                    priceInfo.map((info) => (
+                      <span key={info} className="subscription-lines-accordion__price-info">
+                        {info}
+                      </span>
+                    ))}
                 </div>
               </div>
             </div>
-            <div className="subscription-lines-accordion__description2 subscription-lines-accordion__mobile-only">
-              {description1}
+            <div className="subscription-lines-accordion__icon-container">
+              <Icon
+                icon="arrow-down"
+                className={cn('subscription-lines-accordion__icon-arrow', {
+                  'subscription-lines-accordion__icon-arrow--isExpanded': isExpanded,
+                })}
+              />
             </div>
-            {!isExpanded && <Footer />}
-          </RadioButton>
+          </div>
         </div>
-        <button onClick={(e) => handleOnOpen(e)} className="subscription-lines-accordion__icon-container">
-          <Icon
-            icon="arrow-down"
-            className={cn('subscription-lines-accordion__icon-arrow', {
-              'subscription-lines-accordion__icon-arrow--isExpanded': isExpanded,
-            })}
-          />
-        </button>
-      </div>
+        <div className="subscription-lines-accordion__description2 subscription-lines-accordion__mobile-only">
+          {description1}
+        </div>
+      </button>
       {isExpanded && children && (
         <section
           className={cn('subscription-lines-accordion__expanded-info', {
@@ -249,10 +201,22 @@ const SubscriptionLinesAccordion = ({
             </>
           )}
           {children}
-          <div className="subscription-lines-accordion__disclaimers">{disclaimers}</div>
         </section>
       )}
-      {isExpanded && <Footer />}
+      <div
+        className={cn('subscription-lines-accordion__mobile-only subscription-lines-accordion__footer-border', {
+          'subscription-lines-accordion__footer-border--inverted': isInverted,
+        })}
+      >
+        {<FooterComponent />}
+      </div>
+      <div
+        className={cn('subscription-lines-accordion__desktop-only subscription-lines-accordion__footer-border', {
+          'subscription-lines-accordion__footer-border--inverted': isInverted,
+        })}
+      >
+        {!!footer && <FooterComponent />}
+      </div>
     </section>
   );
 };
