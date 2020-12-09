e06ff10e04c7fce53f09f85c2ee6a4afe443b42e
Marianne Magnussen <marianne_magnussen@icloud.com>
2020-12-09 10:48:13 +0100

    Add expandable and open properties for listItem (#831)
    * Add expandable and open properties for listItem
    
    * Add telia-listItem class
    
    * Fix compact and focus style
    
    * Set cursor to pointer when clickable
    
    * Add Expandable story
    
    * Fix skeleton
    
    * Fix error
    
    * Set border-radius for all card states
    
    * Fix padding for skeleton
    
    Co-authored-by: Marianne Magnussen <marianmagnussen@gmail.com>

--- src/business/List/ListCard.tsx
+++ src/business/List/ListCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ListItemProps, ListItem } from './ListItem';
+import { ListStyle } from './utils';
 
-export const ListCard: React.FC<ListItemProps> = ({ border, ...props }) => (
+export const ListCard: React.FC<ListItemProps & ListStyle> = ({ border, ...props }) => (
   <ListItem {...props} border={border ? border : 'shadow'} type="card" />
 );
--- src/business/List/ListItemSkeleton.tsx
+++ src/business/List/ListItemSkeleton.tsx
@@ -37,7 +37,6 @@ export const ListItemSkeleton = (props: ListItemProps) => {
   return (
     <li
       className={cn('telia-listItem telia-listItem--skeleton ', {
-        'telia-listItem--compact': compact,
         'telia-listItem--card': type === 'card',
         'telia-listItem--underlined': border === 'underlined',
         'telia-listItem--shadow': border === 'shadow',
@@ -46,36 +45,38 @@ export const ListItemSkeleton = (props: ListItemProps) => {
         'telia-listItem--noBG': color !== 'dark' && color !== 'medium',
       })}
     >
-      <div className="telia-listItem__main">
-        {decorator && (
-          <div className="telia-listItem__decorator">
-            <div>&nbsp;</div>
+      <div className={cn('telia-listItem__mainWrapper', { 'telia-listItem--compact': compact })}>
+        <div className="telia-listItem__main">
+          {decorator && (
+            <div className="telia-listItem__decorator">
+              <div>&nbsp;</div>
+            </div>
+          )}
+          <div className="telia-listItem__content">
+            <h3 className={cn('telia-listItem__name', { 'telia-listItem__name--dark': color === 'dark' })}>
+              <div>&nbsp;</div>
+            </h3>
+            {description && (
+              <div
+                className={cn('telia-listItem__description', {
+                  'telia-listItem__description--dark': color === 'dark',
+                })}
+              >
+                <div>&nbsp;</div>
+              </div>
+            )}
           </div>
-        )}
-        <div className="telia-listItem__content">
-          <h3 className={cn('telia-listItem__name', { 'telia-listItem__name--dark': color === 'dark' })}>
-            <div>&nbsp;</div>
-          </h3>
-          {description && (
+          {caption && (
             <div
-              className={cn('telia-listItem__description', {
-                'telia-listItem__description--dark': color === 'dark',
+              className={cn('telia-listItem__caption', {
+                'telia-listItem__caption--text': typeof caption === 'string',
+                'telia-listItem__caption--dark': color === 'dark',
               })}
             >
               <div>&nbsp;</div>
             </div>
           )}
         </div>
-        {caption && (
-          <div
-            className={cn('telia-listItem__caption', {
-              'telia-listItem__caption--text': typeof caption === 'string',
-              'telia-listItem__caption--dark': color === 'dark',
-            })}
-          >
-            <div>&nbsp;</div>
-          </div>
-        )}
       </div>
     </li>
   );
