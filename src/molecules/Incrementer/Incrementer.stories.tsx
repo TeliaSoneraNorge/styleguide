59d37170ef82d1f9037865b53bbe2b0c863e4620
Helene Grini <helenegrini96@gmail.com>
2020-12-14 13:21:05 +0100

    handle bounderies in incrementer

--- src/molecules/Incrementer/Incrementer.stories.tsx
+++ src/molecules/Incrementer/Incrementer.stories.tsx
@@ -10,10 +10,23 @@ export default {
 
 export const Default = () => {
   const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
+
   return (
     <div style={{ display: 'flex' }}>
       <div style={{ paddingBottom: '10px' }}>
-        <Incrementer value={count} setValue={setCount} />
+        <div style={{ paddingBottom: '2rem' }}>
+          <div>size: 'compact'</div>
+          <Incrementer value={count} setValue={setCount} size="compact" />
+        </div>
+        <div>
+          <div style={{ display: 'flex' }}>
+            <div style={{ paddingRight: '1rem' }}>size: 'default'</div>
+            <div style={{ paddingRight: '1rem' }}>max: 10</div>
+            <div style={{ paddingRight: '1rem' }}>min: 0</div>
+          </div>
+          <Incrementer value={count1} setValue={setCount1} max={10} min={0} />
+        </div>
       </div>
     </div>
   );
--- src/molecules/Incrementer/Incrementer.tsx
+++ src/molecules/Incrementer/Incrementer.tsx
@@ -4,26 +4,51 @@ import cn from 'classnames';
 
 type Props = {
   value: number;
-  setValue: (num: number) => void;
+  setValue: (num?: number) => void;
   size?: 'compact' | 'default';
+  max?: number;
+  min?: number;
 };
 
 export const Incrementer = (props: Props) => {
-  const decrement = () => props.setValue(props.value - 1);
-  const increment = () => props.setValue(props.value + 1);
+  const decrement = () => {
+    const newVal = (props.value ?? 0) - 1;
+    props.setValue(props.min !== undefined ? Math.max(props.min, newVal) : newVal);
+  };
+
+  const increment = () => {
+    const newVal = (props.value ?? 0) + 1;
+    props.setValue(props.max !== undefined ? Math.min(props.max, newVal) : newVal);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checkValidity()) {
+      props.setValue(e.target.value ? parseInt(e.target.value) : undefined);
+    }
+  };
 
   return (
-    <div className={cn('telia-incrementer')}>
-      <button className="telia-incrementer--decrement" onClick={decrement}>
+    <div className={cn('telia-incrementer', { 'telia-incrementer__compact': props.size === 'compact' })}>
+      <button
+        className="telia-incrementer--decrement"
+        onClick={decrement}
+        disabled={Boolean(props.min !== undefined && props.value <= props.min)}
+      >
         <Icon icon="minus" />
       </button>
       <input
         className="telia-incrementer--value"
         type="number"
+        min={props.min}
+        max={props.max}
         value={props.value}
-        onChange={(e) => props.setValue(parseInt(e.target.value))}
+        onChange={handleChange}
       />
-      <button className="telia-incrementer--increment" onClick={increment}>
+      <button
+        className="telia-incrementer--increment"
+        onClick={increment}
+        disabled={Boolean(props.max && props.value >= props.max)}
+      >
         <Icon icon="add" />
       </button>
     </div>
