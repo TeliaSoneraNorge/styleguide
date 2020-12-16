a54fb6b85cc5c5110f3dba1b59f777eec8b4f43a
Helene Grini <helenegrini96@gmail.com>
2020-12-16 10:59:21 +0100

    add warnings for when lable props have wrong length

--- src/molecules/DatePicker/context/provider.tsx
+++ src/molecules/DatePicker/context/provider.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { ContextValue, ContextProps } from './types';
 import { InitialContextValue, defaultMonthLabels, defaultDayLabels } from './data';
 import { useFirstPeriod } from './useFirstPeriod';
@@ -10,6 +10,20 @@ export const DatePickerContextProvider: React.FC<ContextProps> = (props) => {
   const datePickerRef = useRef<HTMLDivElement>(null);
   const maxDate = props.maxDate ? new Date(props.maxDate) : undefined;
   const minDate = props.minDate ? new Date(props.minDate) : undefined;
+
+  useEffect(() => {
+    if (props.dayLabels && props.dayLabels.length !== 7) {
+      console.warn(`You provided dayLabels list on props with ${props.dayLabels.length} entries. Expected 7 labels.`);
+    }
+  }, [props.dayLabels]);
+
+  useEffect(() => {
+    if (props.monthLabels && props.monthLabels.length !== 12) {
+      console.warn(
+        `You provided monthLabels list on props with ${props.monthLabels.length} entries. Expected 12 labels.`
+      );
+    }
+  }, [props.monthLabels]);
 
   const dateIsInRange = (date: Date) =>
     !(minDate && date.getTime() < minDate.getTime()) && !(maxDate && date.getTime() > maxDate.getTime());
