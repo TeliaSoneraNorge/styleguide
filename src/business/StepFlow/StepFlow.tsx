e9efb23db581ef58f93c413e5301b990325b0902
Helene Grini <helenegrini96@gmail.com>
2021-01-14 12:41:11 +0100

    remove unues props

--- src/business/StepFlow/StepFlow.tsx
+++ src/business/StepFlow/StepFlow.tsx
@@ -4,10 +4,7 @@ import { StepFlowMenu } from './StepFlowMenu';
 import { StepFlowFooter } from './StepFlowFooter';
 import { Labels } from './types';
 import { useBreakpoint } from '../../utils/useBreakpoint';
-type Props<T> = {
-  /**
-   *
-   */
+type Props = {
   initialStep?: number;
   /**
    * Main title of the form
@@ -19,22 +16,25 @@ type Props<T> = {
   description: string;
   onSubmit: () => void;
   onCancel: () => void;
-  submitText?: string;
   /**
-   * Children will be each form step.
-   * Each step must be wrapped in a StepWrapper
+   * Pass inn custom labels for the different actions/buttons in the step flow.
+   * Enables use of different languages
    */
-  children: React.ReactNode;
+  labels?: Labels;
   /**
    * Displayed in the StepFlowHeader.
    * Should only be  used for a small component or some additional text
    */
   headerContent?: (stuck: boolean) => React.ReactNode;
 
-  labels?: Labels;
+  /**
+   * Children will be each form step.
+   * Each step must be wrapped in a StepWrapper
+   */
+  children: React.ReactNode;
 };
 
-export const StepFlow = <T,>(props: Props<T>) => {
+export const StepFlow = (props: Props) => {
   const [step, setStep] = useState(props.initialStep ?? 0);
   const breakpointSm = useBreakpoint('sm');
 
