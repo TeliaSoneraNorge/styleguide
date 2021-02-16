9b2c008e0d961090fd11750a4e4ac2d521f45f9e
Joakim Kristiansen <joakikr@gmail.com>
2021-02-16 14:49:34 +0100

    SmallBoxList change bg color and border

--- src/atoms/SmallBoxList/SmallBoxList.stories.tsx
+++ src/atoms/SmallBoxList/SmallBoxList.stories.tsx
@@ -7,14 +7,23 @@ export default {
 };
 
 const boxes = [
-    { name: "64 GB",  id: "64-gb" },
-    { name: "128 GB", id: "128-gb" },
-    { name: "256 GB", id: "256-gb" }
+  { name: '64 GB', id: '64-gb' },
+  { name: '128 GB', id: '128-gb' },
+  { name: '256 GB', id: '256-gb' },
+];
+
+const differentLengths = [
+  { name: '64 GB lorem ipsum', id: '64-gb' },
+  { name: '128 GB', id: '128-gb' },
+  { name: '256 GB lorem', id: '256-gb' },
 ];
 
 export const Default = () => {
-    const [selected, setSelected] = useState<string>();
-    return (
-        <SmallBoxList selected={selected} onSelect={setSelected} boxes={boxes} />
-    )
-}
+  const [selected, setSelected] = useState<string>();
+  return <SmallBoxList selected={selected} onSelect={setSelected} boxes={boxes} />;
+};
+
+export const UnevenTextLengths = () => {
+  const [selected, setSelected] = useState<string>();
+  return <SmallBoxList selected={selected} onSelect={setSelected} boxes={differentLengths} />;
+};
