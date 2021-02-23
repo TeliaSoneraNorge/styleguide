b8a0ddb9c4859303b582b7250b71ef21c2afe628
Marianne Magnussen <marianmagnussen@gmail.com>
2021-02-23 08:11:57 +0100

    Move className prop to outer div

--- src/business/List/ListItem.tsx
+++ src/business/List/ListItem.tsx
@@ -38,14 +38,14 @@ export const ListItem: React.FC<ListItemProps & ListStyle> = (props) => {
         'telia-listItem--dark': color === 'dark',
         'telia-listItem--medium': color === 'medium',
         'telia-listItem--noBG': color !== 'dark' && color !== 'medium',
+        className,
       })}
     >
       <InnerTag
-        className={cn(
-          'telia-listItem__mainWrapper',
-          { 'telia-listItem--compact': compact, 'telia-listItem--clickable': onClick },
-          className
-        )}
+        className={cn('telia-listItem__mainWrapper', {
+          'telia-listItem--compact': compact,
+          'telia-listItem--clickable': onClick,
+        })}
         onClick={onClick}
         tabIndex={onClick && 0}
         role={onClick && 'button'}
