15350e3e80e05a60cdc4741c7c392358aa85f89e
Agnethe Søraa <me@agnethesoraa.com>
2021-02-23 08:28:15 +0100

    MenuCart stop propagation

--- src/molecules/Menu/MenuCart.tsx
+++ src/molecules/Menu/MenuCart.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import SvgIcon from '../../atoms/SvgIcon/SvgIcon';
 
 interface MenuCartProps {
-  onClick?: () => void;
+  onClick?: (event: React.MouseEvent) => void;
   numberOfItemsInCart: number;
 }
 
-const MenuCart = ({ onClick = () => undefined, numberOfItemsInCart }: MenuCartProps) => (
-  <button className="menu-cart__button" onClick={onClick} aria-label="Kasse">
+const MenuCart = ({ onClick = (_event) => undefined, numberOfItemsInCart }: MenuCartProps) => {
+  const handleClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    onClick(event);
+  };
+  <button className="menu-cart__button" onClick={handleClick} aria-label="Kasse">
     <div className="menu-cart__icon-container">
       <SvgIcon className="menu-cart__icon" iconName="ico_buy" color="black" />
       <span className="menu-cart__item-count">
@@ -15,7 +19,7 @@ const MenuCart = ({ onClick = () => undefined, numberOfItemsInCart }: MenuCartPr
       </span>
     </div>
     <span className="menu-cart__button--label">Kasse</span>
-  </button>
-);
+  </button>;
+};
 
 export default MenuCart;
