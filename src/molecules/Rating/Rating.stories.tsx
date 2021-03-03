1210c44ec27250ed298c6ce03ca0ea7469709a64
klounErik <janegge1990@gmail.com>
2021-03-03 16:00:18 +0100

    added story for rating without comments and fixed link styling

--- src/molecules/Rating/Rating.stories.tsx
+++ src/molecules/Rating/Rating.stories.tsx
@@ -14,3 +14,9 @@ export const RatingNumbers = () => (
     <RatingWithNumbers rating={4.6} numberOfRatings={49} />
   </RatingStars>
 );
+
+export const RatingNumbersWithReviews = () => (
+  <RatingStars rating={4} height={20} width={20} linkName="Anmeldelser" reviewComments={325}>
+    <RatingWithNumbers rating={4.6} numberOfRatings={49} />
+  </RatingStars>
+);
