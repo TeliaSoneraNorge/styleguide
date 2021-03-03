d79c0d59dbd03861ccb6acd4f6ae1bcfdeea7eaf
klounErik <janegge1990@gmail.com>
2021-03-03 16:27:30 +0100

    removed unused imports

--- src/molecules/Review/Review.stories.tsx
+++ src/molecules/Review/Review.stories.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import { Review } from './Review';
 import { ReviewHeader } from './ReviewHeader';
 import { ReviewBody } from './ReviewBody';
