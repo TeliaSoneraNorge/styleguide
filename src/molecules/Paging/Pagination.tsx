a0c0a55a674b586bb79796645d927995f689780e
Helene Grini <helenegrini@Helenes-MBP.lan>
2021-03-23 12:45:26 +0100

    refactor pagination

--- src/molecules/Paging/Pagination.tsx
+++ src/molecules/Paging/Pagination.tsx
@@ -11,8 +11,7 @@ export interface Props {
 export const Pagination: React.FC<Props> = (props) => {
   const numPages = Math.ceil(props.dataLength / props.perPage);
   const pages = new Array(numPages).fill(0);
-  const midPagesLength = numPages > 7 ? 3 : numPages - 4;
-  const midPagesRange = new Array(midPagesLength).fill(0);
+  const midPagesLength = 3;
   const longRange = numPages > 7;
 
   const getMidRangeStart = (page: number) => {
@@ -22,6 +21,9 @@ export const Pagination: React.FC<Props> = (props) => {
   };
 
   const [midRangeStart, setMidRangeStart] = React.useState(getMidRangeStart(props.currentPage));
+  const moreButton = (
+    <Button size="compact" disabled kind="secondary-text" label={'...'} className="telia-pagination__dottes" />
+  );
 
   const pageButtons: Array<React.ReactElement> = pages.map((e, i) => (
     <Button
@@ -33,59 +35,14 @@ export const Pagination: React.FC<Props> = (props) => {
       onClick={(e) => setPage(i + 1)}
     />
   ));
+
   const getPageButton = (page: number) => pageButtons[page - 1];
-
-  const moreButton = <Button size="compact" kind="secondary-text" label={'...'} className="telia-pagination__dottes" />;
-
-  let midPageButtons = midPagesRange.map((number, i) => getPageButton(i + midRangeStart));
-  const nextPage = () => {
-    if (props.currentPage > 3 && props.currentPage < numPages - 3) {
-      setMidRangeStart(midRangeStart + 1);
-    }
-    props.setPage(props.currentPage + 1);
-  };
-
-  const previousPage = () => {
-    if (props.currentPage < numPages - 2 && props.currentPage > 4) {
-      setMidRangeStart(midRangeStart - 1);
-    }
-    props.setPage(props.currentPage - 1);
-  };
+  const midPageButtons = pageButtons.slice(midRangeStart - 1, midRangeStart + 2);
 
   const setPage = (page: number) => {
-    const pageInMidRange = page > 2 && page < numPages - 1;
-    const lowerHidden = page > 4;
-    const newMidRangeStart = getMidRangeStart(page);
-    if (!pageInMidRange) {
-      if (page === 1) {
-        props.setPage(page);
-        setMidRangeStart(newMidRangeStart);
-      } else if (page === numPages) {
-        props.setPage(page);
-        setMidRangeStart(newMidRangeStart);
-      } else {
-        props.setPage(page);
-      }
-    } else {
-      if (longRange) {
-        if (lowerHidden && page >= midRangeStart && page < numPages - 2) {
-          setMidRangeStart(newMidRangeStart);
-          props.setPage(page);
-        } else if (!lowerHidden && page === 4) {
-          setMidRangeStart(newMidRangeStart);
-          props.setPage(page);
-        } else {
-          props.setPage(page);
-        }
-      } else {
-        props.setPage(page);
-      }
-    }
+    setMidRangeStart(getMidRangeStart(page));
+    props.setPage(page);
   };
-
-  React.useEffect(() => {
-    midPageButtons = midPagesRange.map((number, i) => getPageButton(i + midRangeStart));
-  }, [midRangeStart]);
 
   return (
     <div className="telia-pagination">
@@ -94,23 +51,29 @@ export const Pagination: React.FC<Props> = (props) => {
         kind="secondary-text"
         icon="arrow-left"
         aria-label="Forrige side"
-        onClick={(e) => previousPage()}
+        onClick={() => setPage(props.currentPage - 1)}
         disabled={props.currentPage <= 1}
       />
-      {getPageButton(1)}
-      {props.currentPage > 4 && longRange ? moreButton : getPageButton(2)}
+      {longRange ? (
+        <>
+          {getPageButton(1)}
+          {props.currentPage > 4 ? moreButton : getPageButton(2)}
 
-      {midPageButtons}
+          {midPageButtons}
 
-      {props.currentPage < numPages - 3 && longRange ? moreButton : getPageButton(numPages - 1)}
-      {getPageButton(numPages)}
+          {props.currentPage < numPages - 3 ? moreButton : getPageButton(numPages - 1)}
+          {getPageButton(numPages)}
+        </>
+      ) : (
+        pageButtons
+      )}
 
       <Button
         size="compact"
         kind="secondary-text"
         icon="arrow-right"
         aria-label="Neste side"
-        onClick={(e) => nextPage()}
+        onClick={() => setPage(props.currentPage + 1)}
         disabled={props.currentPage >= numPages}
       />
     </div>
