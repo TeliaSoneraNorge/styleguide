b7adc326f8306af5d4ea62be298ba8570bbe6fac
Joakim Kristiansen <joakikr@gmail.com>
2021-03-25 11:16:10 +0100

    Change border of draft item in ShoppingCart (#952)

--- src/molecules/ShoppingCart/ShoppingCartItem.jsx
+++ src/molecules/ShoppingCart/ShoppingCartItem.jsx
@@ -79,7 +79,7 @@ const ShoppingCartItem = ({
     <>
       <ShoppingCartRow
         className={cn('shopping-cart__item', {
-          'shopping-cart__item__dashed': isDraft,
+          'shopping-cart__item__solid': isDraft,
           [`shopping-cart__item__id--${id}`]: id,
         })}
         key={id}
