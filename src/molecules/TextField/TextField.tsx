5f370cf0a83337ade0953d899ce816e1f8c54573
Marianne Magnussen <marianmagnussen@gmail.com>
2021-04-07 09:48:04 +0200

    Show placeholder on compact Textfield

--- src/molecules/TextField/TextField.tsx
+++ src/molecules/TextField/TextField.tsx
@@ -112,7 +112,6 @@ export const TextField = (props: TextFieldProps) => {
   const { onChange, onBlur, onFocus, onKeyDown, focus, setFocus, inputRef } = useFocus(props);
   const inputLabelId = props.label && props.id ? `${props.id}-label` : undefined;
   const statusIcon = props.success ? <Icon icon="check-mark-circle" /> : props.error ? <Icon icon="alert" /> : null;
-  const placeholder = props.size === 'compact' ? undefined : props.placeholder;
   const inputHasValue = (props.value && props.value.length) || (inputRef.current && inputRef.current.value.length);
 
   /**
@@ -174,7 +173,7 @@ export const TextField = (props: TextFieldProps) => {
             name={props.name}
             ref={inputRef}
             type={props.type || 'text'}
-            placeholder={placeholder}
+            placeholder={props.placeholder}
             disabled={props.disabled}
             value={props.value}
             aria-label={props['aria-label']}
