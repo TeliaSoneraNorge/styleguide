41008a51e42ff8442ce1af2fef1dae61e7ff324b
Helene Grini <helenegrini@Helenes-MBP.lan>
2021-04-14 12:30:09 +0200

    Support more-option and menu in mobile navigation component

--- src/molecules/MobileNavBar/MobileNavBarItem.tsx
+++ src/molecules/MobileNavBar/MobileNavBarItem.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Icon, IconDefinition } from '../../index';
 import cn from 'classnames';
 import { Badge } from '../../atoms/Badge';
-interface Props {
+export interface MobileNavBarItemProps {
   onClick: () => void;
   icon: IconDefinition;
   label?: string;
@@ -10,7 +10,7 @@ interface Props {
   active?: boolean;
   hasNotification?: boolean;
 }
-export const MobileNavBarItem = (props: Props) => {
+export const MobileNavBarItem = (props: MobileNavBarItemProps) => {
   const Tag = props.href ? 'a' : 'button';
 
   const clickHandler = (e: React.SyntheticEvent) => {
@@ -32,18 +32,18 @@ export const MobileNavBarItem = (props: Props) => {
   return (
     <li>
       <Tag
-        className={cn('telia-mobile-nav-bar-item', {
-          'telia-mobile-nav-bar-item--active': props.active,
-          'telia-mobile-nav-bar-item--compact': !props.label,
+        className={cn('telia-mobile-nav-bar__item', {
+          'telia-mobile-nav-bar__item--active': props.active,
+          'telia-mobile-nav-bar__item--compact': !props.label,
         })}
         onClick={clickHandler}
         data-title={props.label}
         href={props.href}
       >
         <NotificationWrapper>
-          <div>
+          <div className="telia-mobile-nav-bar__item__content">
             <Icon icon={props.icon} />
-            {props.label ? <div className="telia-mobile-nav-bar-item-label">{props.label}</div> : null}
+            {props.label ? <div className="telia-mobile-nav-bar__item__label">{props.label}</div> : null}
           </div>
         </NotificationWrapper>
       </Tag>
