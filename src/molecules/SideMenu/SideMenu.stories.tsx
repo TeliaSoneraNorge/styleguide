43ca75c9327e924c94b0830b99da24e1a4de6046
Helene Grini <35767732+HeleneGrini@users.noreply.github.com>
2021-04-14 08:58:22 +0200

    Update design of SideMenu (#959)
    * eslint ignore node mods
    
    * update side menu design
    
    * refactor side menu item
    
    * support white and grey
    
    * fix focus style and layout of rounded buttons on trablet
    
    Co-authored-by: Helene Grini <helenegrini@Helenes-MBP.lan>

--- src/molecules/SideMenu/SideMenu.stories.tsx
+++ src/molecules/SideMenu/SideMenu.stories.tsx
@@ -3,21 +3,29 @@ import { SideMenu, SideMenuBottom, SideMenuTop, SideMenuItem } from './index';
 import { action } from '@storybook/addon-actions';
 import { enableTabKeyDetection } from '../../utils/enableTabKeyDetection';
 import b from './business.svg';
+
 export default {
   component: SideMenu,
   title: 'Component library/Molecules/SideMenu',
 };
 
-export const Default = ({}) => {
+export const White = () => {
   const [active, setActive] = useState(0);
   enableTabKeyDetection();
+
   return (
     <>
       <div style={{ display: 'flex', height: '90vh' }}>
         <div style={{ marginRight: '1rem', height: '100%' }}>
           <SideMenu aria-labelledby="main_menu">
             <SideMenuTop>
-              <SideMenuItem label="Helene Grini" avatar={{ img: b }} onClick={action('user')} color="grey" />
+              <SideMenuItem
+                label="Rounded item"
+                avatar={{ img: b }}
+                onClick={action('user')}
+                color="grey"
+                kind="rounded"
+              />
               <SideMenuItem label="Hjem" icon="home" onClick={() => setActive(0)} active={active === 0} href="/" />
 
               <SideMenuItem
@@ -51,7 +59,13 @@ export const Default = ({}) => {
                 active={active === 4}
                 color="grey"
               />
-              <SideMenuItem label="Helene Grini" avatar={{ text: 'HG' }} onClick={action('user')} color="grey" />
+              <SideMenuItem
+                kind="rounded"
+                label="Helene Grini"
+                avatar={{ text: 'HG' }}
+                onClick={action('user')}
+                color="grey"
+              />
             </SideMenuBottom>
           </SideMenu>
         </div>
@@ -78,6 +92,102 @@ export const Default = ({}) => {
         </div>
         <div style={{ marginRight: '1rem', height: '100%' }}>
           <SideMenu aria-labelledby="main_menu">
+            <SideMenuBottom>
+              <SideMenuItem label="Innstillinger" icon="settings" onClick={() => setActive(3)} active={active === 3} />
+              <SideMenuItem label="Log ut" icon="logout" onClick={() => setActive(4)} active={active === 4} />
+              <SideMenuItem label="Helene Grini" avatar={{ text: 'HG' }} onClick={action('user')} />
+            </SideMenuBottom>
+          </SideMenu>
+        </div>
+      </div>
+      <h4>Try to change the viewport size in the storybook toolbar </h4>
+    </>
+  );
+};
+
+export const Grey = () => {
+  const [active, setActive] = useState(0);
+  enableTabKeyDetection();
+
+  return (
+    <>
+      <div style={{ display: 'flex', height: '90vh', backgroundColor: '#fbfbfb' }}>
+        <div style={{ marginRight: '1rem', height: '100%' }}>
+          <SideMenu aria-labelledby="main_menu" backgroundColor="grey100">
+            <SideMenuTop>
+              <SideMenuItem
+                label="Rounded item"
+                avatar={{ img: b }}
+                onClick={action('user')}
+                color="grey"
+                kind="rounded"
+              />
+              <SideMenuItem label="Hjem" icon="home" onClick={() => setActive(0)} active={active === 0} href="/" />
+
+              <SideMenuItem
+                label="Abonnenter"
+                icon="user"
+                onClick={() => setActive(1)}
+                active={active === 1}
+                href="/abonnenter"
+              />
+              <SideMenuItem
+                label="Økonomi"
+                icon="money"
+                onClick={() => setActive(2)}
+                active={active === 2}
+                href="/okonomi"
+              />
+            </SideMenuTop>
+
+            <SideMenuBottom>
+              <SideMenuItem
+                label="Innstillinger"
+                icon="settings"
+                onClick={() => setActive(3)}
+                active={active === 3}
+                color="grey"
+              />
+              <SideMenuItem
+                label="Log ut"
+                icon="logout"
+                onClick={() => setActive(4)}
+                active={active === 4}
+                color="grey"
+              />
+              <SideMenuItem
+                kind="rounded"
+                label="Helene Grini"
+                avatar={{ text: 'HG' }}
+                onClick={action('user')}
+                color="grey"
+              />
+            </SideMenuBottom>
+          </SideMenu>
+        </div>
+        <div style={{ marginRight: '1rem', height: '100%' }}>
+          <SideMenu aria-labelledby="main_menu" backgroundColor="grey100">
+            <SideMenuTop>
+              <SideMenuItem label="Hjem" icon="home" onClick={() => setActive(0)} active={active === 0} href="/" />
+              <SideMenuItem
+                label="Abonnenter"
+                icon="user"
+                onClick={() => setActive(1)}
+                active={active === 1}
+                href="/abonnenter"
+              />
+              <SideMenuItem
+                label="Økonomi"
+                icon="money"
+                onClick={() => setActive(2)}
+                active={active === 2}
+                href="/okonomi"
+              />
+            </SideMenuTop>
+          </SideMenu>
+        </div>
+        <div style={{ marginRight: '1rem', height: '100%' }}>
+          <SideMenu aria-labelledby="main_menu" backgroundColor="grey100">
             <SideMenuBottom>
               <SideMenuItem label="Innstillinger" icon="settings" onClick={() => setActive(3)} active={active === 3} />
               <SideMenuItem label="Log ut" icon="logout" onClick={() => setActive(4)} active={active === 4} />
