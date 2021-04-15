9737891ab4a9c9ae1e417162731e104882be53b0
remibengtbouvet <remi.pettersen@bouvet.no>
2021-04-15 09:25:03 +0200

    Add scaffolding for Breadcrumbs.

---
+++ src/molecules/Breadcrumbs/Breadcrumbs.d.ts
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export interface BreadcrumbsProps {}
+
+export default class Breadcrumbs extends React.Component<BreadcrumbsProps, any> {
+  render(): JSX.Element;
+}
