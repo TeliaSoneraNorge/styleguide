d209b32daa91f9ea01c57a10bd316c2dd3d783bc
remibengtbouvet <remi.pettersen@bouvet.no>
2021-04-26 13:28:44 +0200

    Refactor inner logic for build and render breadcrumb

--- src/molecules/Breadcrumbs/Breadcrumbs.stories.tsx
+++ src/molecules/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -52,12 +52,12 @@ const storybook = {
     {
       id: 1,
       name: 'Step1',
-      link: './hjem/currentpage',
+      link: 'http://localhost:6006/?path=/story/component-library-molecules-breadcrumbs--step-3',
     },
     {
       id: 2,
       name: 'Step2',
-      link: './hjem/step1/step2',
+      link: 'http://localhost:6006/?path=/story/component-library-molecules-breadcrumbs--step-3',
     },
     {
       id: 3,
@@ -92,6 +92,38 @@ const storybook = {
       link: './hjem/step1/step2/step3/currentpage',
     },
   ],
+  step5Page: [
+    {
+      id: 0,
+      name: 'Hjem',
+      link: './',
+    },
+    {
+      id: 1,
+      name: 'Step1',
+      link: './hjem/step1',
+    },
+    {
+      id: 2,
+      name: 'Step2',
+      link: './hjem/step1/step2',
+    },
+    {
+      id: 3,
+      name: 'Step3',
+      link: './hjem/step1/step2/step3',
+    },
+    {
+      id: 4,
+      name: 'Step4',
+      link: './hjem/step1/step2/step3/step4',
+    },
+    {
+      id: 5,
+      name: 'Current page',
+      link: './hjem/step1/step2/step3/curentpage',
+    },
+  ],
 };
 
 export const Default = () => <Breadcrumbs crumbs={storybook.homePage} />;
@@ -103,3 +135,5 @@ export const Step2 = () => <Breadcrumbs crumbs={storybook.step2Page} />;
 export const Step3 = () => <Breadcrumbs crumbs={storybook.step3Page} />;
 
 export const Step4 = () => <Breadcrumbs crumbs={storybook.step4Page} />;
+
+export const Step5 = () => <Breadcrumbs crumbs={storybook.step5Page} />;
