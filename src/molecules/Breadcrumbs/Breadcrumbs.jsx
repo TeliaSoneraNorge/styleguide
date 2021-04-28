cb05744b03198b388026c556c73d1efcaa2a367d
remibengtbouvet <remi.pettersen@bouvet.no>
2021-04-28 10:52:59 +0200

    Refactored after PR

--- src/molecules/Breadcrumbs/Breadcrumbs.jsx
+++ src/molecules/Breadcrumbs/Breadcrumbs.jsx
@@ -1,28 +1,28 @@
+import { result } from 'lodash';
 import React, { useState } from 'react';
 import { ArrowLeftIcon } from '../../atoms/Icon/icons';
 import { MoreIcon } from '../../atoms/Icon/icons';
 
 const Breadcrumbs = (props) => {
-  // init start values and assign props
   const initCrumbSize = 4;
   const initNumberOfCrumbsAfterMinimization = 2;
-  const crumbs = props.crumbs;
 
-  // init react hooks
+  let id = 0;
+  const crumbs = props.crumbs.map((element) => {
+    return { id: id++, name: element.name, link: element.link };
+  });
+
   const [numberOfCrumbsAfterMinimization, setNumberOfCrumbsAfterMinimization] = useState(
     initNumberOfCrumbsAfterMinimization
   );
   const [crumbSize, setCrumbSize] = useState(initCrumbSize);
 
-  // filter crumb based on init values
   let crumb = crumbs.filter((crumb) => crumb.id === 1 || crumb.id >= crumbs.length - numberOfCrumbsAfterMinimization);
 
-  // add minimaziation item - only used for render minimaziation icon
   if (crumb.length < crumbs.length && crumbs.length > crumbSize) {
     crumb.splice(1, 0, { id: -1, name: 'minimaziation item', link: 'no link' });
   }
 
-  // button click event
   const handleClick = () => {
     setNumberOfCrumbsAfterMinimization(numberOfCrumbsAfterMinimization + 1);
     setCrumbSize(crumbSize + 1);
@@ -31,7 +31,6 @@ const Breadcrumbs = (props) => {
     }
   };
 
-  // add minimaziation element to breadcrumb
   const addMinimaziation = (crumb) => {
     return (
       <li key={crumb.id} className="breadcrumb__element">
@@ -43,7 +42,6 @@ const Breadcrumbs = (props) => {
     );
   };
 
-  // add link element to breadcrumb
   const addActionLink = (crumb) => {
     return (
       <li key={crumb.id} className="breadcrumb__element">
@@ -55,7 +53,6 @@ const Breadcrumbs = (props) => {
     );
   };
 
-  // add current page element to breadcrumb
   const addCurrentPage = (crumb) => {
     return (
       <li key={crumb.id} className="breadcrumb__element">
@@ -66,25 +63,22 @@ const Breadcrumbs = (props) => {
     );
   };
 
-  // init counter and length
-  let counter = 0;
+  let index = 0;
   const length = crumb.length;
 
-  // build breadcrumbs from filtered array
   const breadcrumbs = crumb.map((crumb) => {
-    counter++;
+    index++;
     if (crumb.id === 0) {
       // do nothing - landing page => Telia logo will refresh page
     } else if (crumb.id === -1) {
       return addMinimaziation(crumb);
-    } else if (length === counter) {
+    } else if (length === index) {
       return addCurrentPage(crumb);
     } else {
       return addActionLink(crumb);
     }
   });
 
-  // render breadcrumbs
   return (
     <div className="breadcrumb">
       <ul className="breadcrumb__list">{breadcrumbs}</ul>
--- src/molecules/Breadcrumbs/Breadcrumbs.stories.tsx
+++ src/molecules/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -16,117 +16,96 @@ export const Step5 = () => <Breadcrumbs crumbs={storybook.step5Page} />;
 const storybook = {
   homePage: [
     {
-      id: 0,
       name: 'Hjem',
       link: './',
     },
   ],
   step1Page: [
     {
-      id: 0,
       name: 'Hjem',
       link: './',
     },
     {
-      id: 1,
       name: 'Current page',
       link: './hjem/currentpage',
     },
   ],
   step2Page: [
     {
-      id: 0,
       name: 'Hjem',
       link: './',
     },
     {
-      id: 1,
       name: 'Step1',
       link: './hjem/step1',
     },
     {
-      id: 2,
       name: 'Current page',
       link: './hjem/step1/currentpage',
     },
   ],
   step3Page: [
     {
-      id: 0,
       name: 'Hjem',
       link: './',
     },
     {
-      id: 1,
       name: 'Step1',
       link: 'http://localhost:6006/?path=/story/component-library-molecules-breadcrumbs--step-3',
     },
     {
-      id: 2,
       name: 'Step2',
       link: 'http://localhost:6006/?path=/story/component-library-molecules-breadcrumbs--step-3',
     },
     {
-      id: 3,
       name: 'Current page',
       link: './hjem/step1/step2/currentpage',
     },
   ],
   step4Page: [
     {
-      id: 0,
       name: 'Hjem',
       link: './',
     },
     {
-      id: 1,
       name: 'Step1',
       link: './hjem/step1',
     },
     {
-      id: 2,
       name: 'Step2',
       link: './hjem/step1/step2',
     },
     {
-      id: 3,
       name: 'Step3',
       link: './hjem/step1/step2/step3',
     },
     {
-      id: 4,
       name: 'Current page',
       link: './hjem/step1/step2/step3/currentpage',
     },
   ],
   step5Page: [
     {
-      id: 0,
       name: 'Hjem',
       link: './',
     },
     {
-      id: 1,
       name: 'Step1',
       link: './hjem/step1',
     },
     {
-      id: 2,
       name: 'Step2',
       link: './hjem/step1/step2',
     },
     {
-      id: 3,
       name: 'Step3',
       link: './hjem/step1/step2/step3',
     },
     {
-      id: 4,
       name: 'Step4',
       link: './hjem/step1/step2/step3/step4',
     },
     {
-      id: 5,
       name: 'Current page',
       link: './hjem/step1/step2/step3/curentpage',
     },
