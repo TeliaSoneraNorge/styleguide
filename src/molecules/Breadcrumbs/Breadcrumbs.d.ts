b82f69429e0fc10dfba039566f71b1e166b834a6
oyvindbergbouvet <oyvind.berg@bouvet.no>
2021-05-03 10:35:04 +0200

    ..

--- src/molecules/Breadcrumbs/Breadcrumbs.d.ts
+++ src/molecules/Breadcrumbs/Breadcrumbs.d.ts
@@ -3,15 +3,15 @@ import * as React from 'react';
 interface Crumb {
   name: string;
   link: string;
-  target: ?string;
-  title: ?string;
+  target?: string;
+  title?: string;
 }
 
 export interface BreadCrumbsProps {
   crumbs: Crumb[];
-  alwaysShowRootCrumb: ?boolean;
-  pageSize: ?number;
-  pagingSize: ?number;
+  alwaysShowRootCrumb?: boolean;
+  pageSize?: number;
+  pagingSize?: number;
 }
 
 class BreadCrumbs extends React.Component<BreadCrumbsProps, any> {
