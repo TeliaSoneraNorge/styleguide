3d9018fe80e90fdc0c25843f7c6b8a77bd3f2d66
Marianne Magnussen <marianmagnussen@gmail.com>
2021-05-03 15:01:30 +0200

    Refactor border classes

--- src/business/Card/CardOptions.tsx
+++ src/business/Card/CardOptions.tsx
@@ -1,34 +1,28 @@
 import React from 'react';
 import cn from 'classnames';
 import { CardOptionProps } from './CardOption';
+import { useBreakpoint } from '../../utils/useBreakpoint';
 
 interface Props {
   columns?: 1 | 2;
 }
 export const CardOptions: React.FC<Props> = (props) => {
   const count = React.Children.count(props.children);
+  const breakpointSm = useBreakpoint('sm');
+  const oneCol = !breakpointSm || props.columns === 1;
 
-  const getBorders = (index: number) => {
-    let borders = '';
-    if ((props.columns === 1 && count === 1) || (count % 2 !== 0 && count === index + 1))
-      borders += ' telia-card__option--spanCol';
-
-    if (props.columns !== 1) {
-      if ((index + 1) % 2 === 0) borders += ' telia-card__option--borderLeft';
-      if (index > 1) borders += ' telia-card__option--borderTop';
-    } else {
-      if (index > 0) borders += ' telia-card__option--borderTop';
-    }
-    return borders;
-  };
+  const spanCol = (index: number) =>
+    (props.columns === 1 && count === 1) || (count % 2 !== 0 && count === index + 1)
+      ? 'telia-card__option--spanCol'
+      : '';
 
   return (
-    <div className={cn('telia-card__options', { 'telia-card__options--oneCol': props.columns === 1 })}>
+    <div className={cn('telia-card__options', { 'telia-card__options--oneCol': oneCol })}>
       {React.Children.toArray(props.children).map((child, i) =>
         React.isValidElement<CardOptionProps>(child)
           ? React.cloneElement(child, {
               ...child.props,
-              className: (child.props.className ?? '') + getBorders(i),
+              className: (child.props.className ?? '') + spanCol(i),
             })
           : null
       )}
