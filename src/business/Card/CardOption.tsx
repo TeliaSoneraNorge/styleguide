04704550fdbe5960f74d3ed5e964fb9844e11365
Marianne Magnussen <marianmagnussen@gmail.com>
2021-05-04 09:52:19 +0200

    Set classname on Icon

--- src/business/Card/CardOption.tsx
+++ src/business/Card/CardOption.tsx
@@ -12,7 +12,7 @@ export interface CardOptionProps {
 export const CardOption = (props: CardOptionProps) => {
   return (
     <button className={cn('telia-card__option', props.className ?? '')} onClick={props.onClick}>
-      <Icon icon={props.icon} />
+      <Icon icon={props.icon} className="telia-card__option__icon" />
       <div>{props.label}</div>
     </button>
   );
