150095522b117ace8d6d1f45507762d04a3cdf01
Helene Grini <helenegrini@Helenes-MacBook-Pro.local>
2021-05-28 08:47:25 +0200

    remove legcy propTypes from ProgressBar

--- src/atoms/ProgressBar/ProgressBar.tsx
+++ src/atoms/ProgressBar/ProgressBar.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { colors } from '../../utils/colors';
 
@@ -101,15 +100,6 @@ export const progressBarSize = {
   md: '1rem',
   lg: '1.2rem',
   xl: '1.6rem',
-};
-
-ProgressBar.propTypes = {
-  value: PropTypes.number.isRequired,
-  min: PropTypes.number.isRequired,
-  max: PropTypes.number.isRequired,
-  disabled: PropTypes.bool,
-  color: PropTypes.oneOf(Object.keys(colors)),
-  height: PropTypes.oneOf(Object.keys(progressBarSize)),
 };
 
 export default ProgressBar;
