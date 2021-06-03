9ed3a01ecb32cc65d28d3aa471be8b282c892ecf
bogsti87 <christoffer.bogsti@telia.no>
2021-06-03 14:12:09 +0200

    NEW font TeliaSans

--- src/molecules/SubscriptionAccordion/SubscriptionAccordion.stories.tsx
+++ src/molecules/SubscriptionAccordion/SubscriptionAccordion.stories.tsx
@@ -311,7 +311,9 @@ export const SubscriptionAccordionsWithDescription = () => {
         description={
           <span style={{ display: 'flex', alignItems: 'center' }}>
             <InfiniteIcon style={{ marginRight: '0.4rem' }} />
-            <span style={{ fontFamily: 'Helvetica Neue Medium' }}>med 10mbit/s etter</span>
+            <span style={{ fontFamily: `TeliaSans-Medium, Helvetica Neue Medium, Helvetica, Arial, sans-serif` }}>
+              med 10mbit/s etter
+            </span>
           </span>
         }
         price={529}
@@ -339,7 +341,9 @@ export const SubscriptionAccordionsWithDescription = () => {
         description={
           <span style={{ display: 'flex', alignItems: 'center' }}>
             <InfiniteIcon style={{ marginRight: '0.4rem' }} />
-            <span style={{ fontFamily: 'Helvetica Neue Medium' }}>med 10mbit/s etter</span>
+            <span style={{ fontFamily: `TeliaSans-Medium, Helvetica Neue Medium, Helvetica, Arial, sans-serif` }}>
+              med 10mbit/s etter
+            </span>
           </span>
         }
         price={529}
