8fcdbf3a9695f1f4b35615f2778c9ab8b5076050
Helene Grini <helenegrini@Helenes-MBP.lan>
2021-06-09 13:17:26 +0200

    remove children from props

--- src/business/PageLayout/PageLayout.tsx
+++ src/business/PageLayout/PageLayout.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import cn from 'classnames';
 
-type Props = { className?: string } & (
-  | {
-      navBar?: { className?: string; component: React.ReactNode };
-      header?: { className?: string; component: React.ReactNode };
-      body?: { className?: string; component: React.ReactNode };
-      left?: { className?: string; component: React.ReactNode };
-      right?: { className?: string; component: React.ReactNode };
-      footer?: { className?: string; component: React.ReactNode };
-    }
-  | { children: React.ReactNode }
-);
+type Props = {
+  className?: string;
+  navBar?: { className?: string; component: React.ReactNode };
+  header?: { className?: string; component: React.ReactNode };
+  body?: { className?: string; component: React.ReactNode };
+  left?: { className?: string; component: React.ReactNode };
+  right?: { className?: string; component: React.ReactNode };
+  footer?: { className?: string; component: React.ReactNode };
+};
 
 export const PageLayout = (props: Props) => {
-  if ('children' in props) {
-    return <div className={cn('telia-page-layout', props.className)}>{props.children}</div>;
-  }
   const hasSides = props.left || props.right;
 
   return (
