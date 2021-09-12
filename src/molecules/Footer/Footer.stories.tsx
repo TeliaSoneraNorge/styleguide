d02e3454605842569f4d6f4c4d9ceb33f054a0c1
remibengtbouvet <remi.pettersen@bouvet.no>
2021-09-12 14:01:58 +0200

    Remove empty p-tag from address column

--- src/molecules/Footer/Footer.stories.tsx
+++ src/molecules/Footer/Footer.stories.tsx
@@ -21,7 +21,25 @@ export const CityAndPostalCodeEitherNullOrUndefinedFooter = () => {
   return (
     <>
       <p>{'Important! - Backend for enriching model from telia.no is under contruction -'}</p>
-      <Footer {...footer1} />
+      <Footer {...footerMissingCityPostalCode} />
+    </>
+  );
+};
+
+export const MissingBottomRowFooter = () => {
+  return (
+    <>
+      <p>{'Important! - Backend for enriching model from telia.no is under contruction -'}</p>
+      <Footer {...footerMissingBottomRow} />
+    </>
+  );
+};
+
+export const MissingTopRowFooter = () => {
+  return (
+    <>
+      <p>{'Important! - Backend for enriching model from telia.no is under contruction -'}</p>
+      <Footer {...footerMissingTopRow} />
     </>
   );
 };
@@ -213,17 +231,17 @@ const sosialLinkColumn: SocialLinkColumn = {
   links: [
     {
       name: 'Facebook',
-      url: '',
+      url: '/',
       color: 'white',
     },
     {
       name: 'Twitter',
-      url: '',
+      url: '/',
       color: 'purple',
     },
     {
       name: 'Youtube',
-      url: '',
+      url: '/',
       color: 'white',
     },
   ],
@@ -253,229 +271,39 @@ const footer: FooterProps = {
   bottomRow: bottomRow,
 };
 
-const linkColumns1: LinkColumn[] = [
-  {
-    heading: 'Support',
-    links: [
-      {
-        name: 'Dekningskart',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-      {
-        name: 'Driftsmelding',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-      {
-        name: 'Last ned apper',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-      {
-        name: 'Lade kontantkort',
-        url: './',
-        color: 'purple',
-        iconUrl: '',
-      },
-      {
-        name: 'Se Telia Play',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-      {
-        name: 'Valgmenyen',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-      {
-        name: 'Logg inn',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-    ],
-  },
-  {
-    heading: 'Kjøp',
-    links: [
-      {
-        name: 'Mobilabonnement',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-      {
-        name: 'Bredbånd',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-      {
-        name: 'TV-abonnement',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-      {
-        name: 'iPhone 12',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-      {
-        name: 'Samsung',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-    ],
-  },
-  {
-    heading: 'Kontakt',
-    links: [
-      {
-        name: 'Kundeservice',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-      {
-        name: 'Borettslag',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-      {
-        name: 'Nybygg',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-      {
-        name: 'Finn butikk',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-    ],
-  },
-  {
-    heading: 'Om Telia',
-    links: [
-      {
-        name: 'Om Telia Norge',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-      {
-        name: '5G',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-      {
-        name: 'Press',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-      {
-        name: 'Karriere',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-      {
-        name: 'Bærekraft',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-    ],
-  },
-];
-
-const addressColumn1: AddressColumn = {
+const addressColumnMissingCityPostalCode: AddressColumn = {
   companyName: 'Telia Norge AS',
   street: 'Sandakerveien 140',
-  city: undefined,
-  postalCode: undefined,
+  city: null,
+  postalCode: null,
   officeType: 'Hovedkontor',
-};
-
-const linksColumnsBot1: LinkColumn[] = [
-  {
-    heading: '',
-    links: [
-      {
-        name: 'Telia Personvern',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-      {
-        name: 'Cookieinformasjon',
-        url: './',
-        color: 'purple',
-        iconUrl: '',
-      },
-      {
-        name: 'Cookieinnstillinger',
-        url: './',
-        color: '',
-        iconUrl: '',
-      },
-    ],
-  },
-];
-
-const sosialLinkColumn1: SocialLinkColumn = {
-  links: [
-    {
-      name: 'Facebook',
-      url: '',
-      color: 'white',
-    },
-    {
-      name: 'Twitter',
-      url: '',
-      color: 'purple',
-    },
-    {
-      name: 'Youtube',
-      url: '',
-      color: 'white',
-    },
-  ],
-};
-
-const logoColumn1: LogoColumn = {
-  url: 'https://www.telia.no',
-  imageUrl: images.logo,
 };
 
 const topRow1: FooterRow = {
   logoColumn: null,
-  linkColumns: linkColumns1,
+  linkColumns: linkColumns,
   addressColumn: null,
   socialLinkColumn: null,
 };
 
 const bottomRow1: FooterRow = {
-  logoColumn: logoColumn1,
-  linkColumns: linksColumnsBot1,
-  addressColumn: addressColumn1,
-  socialLinkColumn: sosialLinkColumn1,
+  logoColumn: logoColumn,
+  linkColumns: linksColumnsBot,
+  addressColumn: addressColumnMissingCityPostalCode,
+  socialLinkColumn: sosialLinkColumn,
 };
 
-const footer1: FooterProps = {
+const footerMissingCityPostalCode: FooterProps = {
   topRow: topRow1,
   bottomRow: bottomRow1,
+};
+
+const footerMissingBottomRow: FooterProps = {
+  topRow: topRow,
+  bottomRow: null,
+};
+
+const footerMissingTopRow: FooterProps = {
+  topRow: null,
+  bottomRow: bottomRow,
 };
