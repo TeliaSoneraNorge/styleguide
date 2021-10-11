ff2850f0966b8b37a85ca681cf4e18f3ed055e8b
Lars Erik Skraastad <--get-all>
2021-10-11 11:12:25 +0200

    add support to change icon for MobileNavBarMore

--- src/molecules/MobileNavBar/MobileNavBarMore.tsx
+++ src/molecules/MobileNavBar/MobileNavBarMore.tsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
+import { IconDefinition } from '../..';
 import { MobileNavBarItem, MobileNavBarItemProps } from './MobileNavBarItem';
 import { MobileNavBarMoreMenu } from './MobileNavBarMoreMenu';
 
 interface Props {
   label?: string;
+  icon?: IconDefinition;
   items: Array<MobileNavBarItemProps | { divider: true }>;
 }
 export const MobileNavBarMore = (props: Props) => {
   const [open, setOpen] = useState(false);
   return (
     <>
-      <MobileNavBarItem label={props.label} icon="more" onClick={() => setOpen(!open)} active={open} />
+      <MobileNavBarItem label={props.label} icon={props.icon || 'more'} onClick={() => setOpen(!open)} active={open} />
       <MobileNavBarMoreMenu open={open} close={() => setOpen(false)} items={props.items} />
     </>
   );
