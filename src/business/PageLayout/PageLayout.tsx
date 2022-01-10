8e647b192c8f07fc79767627457bce3bd3f632d1
Marianne Magnussen <marianmagnussen@gmail.com>
2022-01-10 12:22:51 +0100

    Update PageLayout with new width and support sticky footer

--- src/business/PageLayout/PageLayout.tsx
+++ src/business/PageLayout/PageLayout.tsx
@@ -8,7 +8,7 @@ type Props = {
   body?: { className?: string; component: React.ReactNode };
   left?: { className?: string; component: React.ReactNode };
   right?: { className?: string; component: React.ReactNode };
-  footer?: { className?: string; component: React.ReactNode };
+  footer?: { className?: string; component: React.ReactNode; sticky?: boolean };
 };
 
 export const PageLayout = (props: Props) => {
@@ -20,16 +20,31 @@ export const PageLayout = (props: Props) => {
         <div className={cn('telia-page-layout__nav-bar', props.navBar.className)}>{props.navBar.component}</div>
       )}
       <div className={cn('telia-page-layout__header', props.header?.className)}>{props.header?.component}</div>
-      <div className={cn('telia-page-layout__content', { 'telia-page-layout__content--grid': hasSides })}>
+      <div className={cn('telia-page-layout__content')}>
         {hasSides && (
           <div className={cn('telia-page-layout__left', props.left?.className)}>{props.left?.component}</div>
         )}
         <div className="telia-page-layout__content__main">
           <div className={cn('telia-page-layout__body', props.body?.className)}>{props.body?.component}</div>
-          <div className={cn('telia-page-layout__footer', props.footer?.className)}>{props.footer?.component}</div>
+          {!props.footer?.sticky && (
+            <div className={cn('telia-page-layout__footer', props.footer?.className)}>{props.footer?.component}</div>
+          )}
         </div>
         {hasSides && (
           <div className={cn('telia-page-layout__right', props.right?.className)}>{props.right?.component}</div>
+        )}
+        {props.footer?.sticky && (
+          <div
+            className={cn(
+              'telia-page-layout__footer',
+              { 'telia-page-layout__footer--sticky': props.footer.sticky },
+              props.footer?.className
+            )}
+          >
+            <div className={cn({ 'telia-page-layout__footer__content--sticky': props.footer.sticky })}>
+              {props.footer?.component}
+            </div>
+          </div>
         )}
       </div>
     </div>
