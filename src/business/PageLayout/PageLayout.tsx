69aee78e12f12c756fc9bd7c98622d317a4cc996
Marianne Magnussen <marianmagnussen@gmail.com>
2022-01-10 13:41:54 +0100

    Use grid for PageLayout, and flex on small screens

--- src/business/PageLayout/PageLayout.tsx
+++ src/business/PageLayout/PageLayout.tsx
@@ -20,33 +20,29 @@ export const PageLayout = (props: Props) => {
         <div className={cn('telia-page-layout__nav-bar', props.navBar.className)}>{props.navBar.component}</div>
       )}
       <div className={cn('telia-page-layout__header', props.header?.className)}>{props.header?.component}</div>
-      <div className={cn('telia-page-layout__content')}>
-        {hasSides && (
-          <div className={cn('telia-page-layout__left', props.left?.className)}>{props.left?.component}</div>
-        )}
-        <div className="telia-page-layout__content__main">
-          <div className={cn('telia-page-layout__body', props.body?.className)}>{props.body?.component}</div>
-          {!props.footer?.sticky && (
-            <div className={cn('telia-page-layout__footer', props.footer?.className)}>{props.footer?.component}</div>
-          )}
-        </div>
-        {hasSides && (
-          <div className={cn('telia-page-layout__right', props.right?.className)}>{props.right?.component}</div>
-        )}
-        {props.footer?.sticky && (
-          <div
-            className={cn(
-              'telia-page-layout__footer',
-              { 'telia-page-layout__footer--sticky': props.footer.sticky },
-              props.footer?.className
-            )}
-          >
-            <div className={cn({ 'telia-page-layout__footer__content--sticky': props.footer.sticky })}>
-              {props.footer?.component}
-            </div>
-          </div>
-        )}
+      {hasSides && <div className={cn('telia-page-layout__left', props.left?.className)}>{props.left?.component}</div>}
+      <div className="telia-page-layout__content">
+        <div className={cn('telia-page-layout__body', props.body?.className)}>{props.body?.component}</div>
       </div>
+      {hasSides && (
+        <div className={cn('telia-page-layout__right', props.right?.className)}>{props.right?.component}</div>
+      )}
+      {!props.footer?.sticky && (
+        <div className={cn('telia-page-layout__footer', props.footer?.className)}>{props.footer?.component}</div>
+      )}
+      {props.footer?.sticky && (
+        <div
+          className={cn(
+            'telia-page-layout__footer',
+            { 'telia-page-layout__footer--sticky': props.footer.sticky },
+            props.footer?.className
+          )}
+        >
+          <div className={cn({ 'telia-page-layout__footer__content--sticky': props.footer.sticky })}>
+            {props.footer?.component}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
