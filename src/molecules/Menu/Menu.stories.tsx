cac2a9fd16573e4106c5645ddaac496ded1fd669
Agnethe Søraa <me@agnethesoraa.com>
2022-01-21 08:55:38 +0100

    Update bedrift menu so it contains some sub menus

--- src/molecules/Menu/Menu.stories.tsx
+++ src/molecules/Menu/Menu.stories.tsx
@@ -32,6 +32,14 @@ export const Default = () => {
       links: [
         { text: 'Tjenester', url: '#' },
         { text: 'Digitalisering', url: '#' },
+        {
+          text: 'Teknologi og samfunn',
+          subLinks: [
+            { text: 'IoT', url: '#' },
+            { text: '5G', url: '#' },
+            { text: 'StartUp', url: '#' },
+          ],
+        },
         { text: 'Nettbutikk', url: '#' },
         { text: 'Kundeservice', url: '#' },
       ],
@@ -262,6 +270,14 @@ export const SingleTopLevelMenu = () => {
         },
         { text: 'Mobilabonnement', url: '#' },
         { text: 'Mobilt bredbånd', url: '#' },
+        {
+          text: 'Teknologi og samfunn',
+          subLinks: [
+            { text: 'IoT', url: '#' },
+            { text: '5G', url: '#' },
+            { text: 'StartUp', url: '#' },
+          ],
+        },
         { text: 'Dekning', url: '#' },
         { text: 'Hjelp', url: '#' },
       ],
