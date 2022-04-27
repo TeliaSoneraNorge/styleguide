c40133a7cd61d40b86442501fd1503199fb06b3b
Joakim Kristiansen <joakikr@gmail.com>
2022-04-27 14:53:20 +0200

    Feature/summary table compact version (#1205)
    * CSS changes
    
    * Added compact version for SummaryTable

--- src/atoms/SummaryTable/SummaryTable.tsx
+++ src/atoms/SummaryTable/SummaryTable.tsx
@@ -1,9 +1,11 @@
 import cn from 'classnames';
 import React from 'react';
+import _ from 'lodash';
 
 export interface SummaryTableItem {
   label: string;
   value: string;
+  valueStriketrough?: string;
   price?: number;
   isSuccess?: boolean;
   isStriketrough?: boolean;
@@ -12,10 +14,11 @@ export interface SummaryTableItem {
 
 export interface SummaryTableProps {
   items: SummaryTableItem[];
+  kind?: 'normal' | 'compact';
 }
 
-const SummaryTable = ({ items }: SummaryTableProps) => (
-  <dl className="summary-table">
+const SummaryTable = ({ items, kind = 'normal' }: SummaryTableProps) => (
+  <dl className={cn('summary-table', { [`summary-table--${kind}`]: true })}>
     {items.map((item, index) => (
       <div
         key={`${item.label}-${index}`}
@@ -23,10 +26,16 @@ const SummaryTable = ({ items }: SummaryTableProps) => (
           'summary-table__row--bold': item.isBold,
           'summary-table__row--green': item.isSuccess,
           'summary-table__row--st': item.isStriketrough,
+          'summary-table__row--indent': index > 0 && index < _.size(items) - 1,
         })}
       >
         <dt className="summary-table__label">{item.label}</dt>
-        <dd className="summary-table__value">{item.value}</dd>
+        <dd className="summary-table__value">
+          {item.valueStriketrough && (
+            <span className="summary-table__value--striketrough">{item.valueStriketrough}</span>
+          )}
+          {item.value}
+        </dd>
       </div>
     ))}
   </dl>
