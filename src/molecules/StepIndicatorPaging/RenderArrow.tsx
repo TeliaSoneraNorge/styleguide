068105d3770843a5cafa74dda0965805e44359bb
oyvindbergbouvet <oyvind.berg@bouvet.no>
2022-08-22 13:44:35 +0200

    Removed unused code

--- src/molecules/StepIndicatorPaging/RenderArrow.tsx
+++ src/molecules/StepIndicatorPaging/RenderArrow.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { RenderLine } from './RenderLine';
-import { RenderTransparentLayer } from './RenderTransparentLayer';
 import { Icon } from '../../atoms/Icon';
 
 export const RenderArrow = (props: any) => {
@@ -41,24 +40,4 @@ export const RenderArrow = (props: any) => {
       )}
     </li>
   );
-
-  // return (
-  //   <li className={'telia-step-indicator-paging__arrow telia-step-indicator-paging__arrow--' + modifier}>
-  //     <RenderLine isComplete={isComplete} className={'telia-step-indicator-paging__line--' + modifier} isActive={false} />
-
-  //     <RenderTransparentLayer modifier={modifier} />
-
-  //     {url && url.length > 0 && (
-  //       <a href={url} className={buttonClassName}>
-  //         <Icon icon={iconName} />
-  //       </a>
-  //     )}
-
-  //     {!url && (
-  //       <button type="button" onClick={onPaging} className={buttonClassName}>
-  //         <Icon icon={iconName} />
-  //       </button>
-  //     )}
-  //   </li>
-  // );
 };
