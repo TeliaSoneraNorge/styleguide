1fa94b4306d8342a56c3d0b2f21b0bba8d0caca9
Adriana <112877281+adrianasabanaite@users.noreply.github.com>
2022-09-26 17:23:09 +0300

    add SpeechBubbleTabs (#1251)
    add SpeechBubbleTabs

---
+++ src/molecules/SpeechBubbleTabs/SpeechBubbleTabs.stories.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { SpeechBubbleTabs } from '../../index';
+
+export default {
+  title: 'Component library/Molecules/SpeechBubbleTabs',
+  component: SpeechBubbleTabs,
+};
+
+const TABS = [
+  { id: 'FWA', name: 'Bredbånd' },
+  { id: 'FWA_TV', name: 'Bredbånd og TV' },
+];
+
+export const DefaultSpeechBubbleTabs = () => {
+  const [selectedTabId, setSelectedTabId] = useState('FWA');
+
+  const handleChangeTab = (id: string) => {
+    setSelectedTabId(id);
+  };
+
+  return <SpeechBubbleTabs tabs={TABS} selectedTabId={selectedTabId} onClick={handleChangeTab} />;
+};
+
+export const SpeechBubbleTabsWithoutDefaultTab = () => {
+  const [selectedTabId, setSelectedTabId] = useState<string | undefined>();
+
+  const handleChangeTab = (id?: string) => {
+    setSelectedTabId(id);
+  };
+
+  return <SpeechBubbleTabs tabs={TABS} selectedTabId={selectedTabId} onClick={handleChangeTab} />;
+};
