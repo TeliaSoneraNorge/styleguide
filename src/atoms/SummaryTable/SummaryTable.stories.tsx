c6168d315b7c5bbc242d8f04c44728672ad01941
oyvindbergbouvet <oyvind.berg@bouvet.no>
2022-11-02 15:42:31 +0100

    SummaryTable - added grey background option - added title option - added badge option - added 'description option' which shows below "price" per item - added multiple stories/variations

--- src/atoms/SummaryTable/SummaryTable.stories.tsx
+++ src/atoms/SummaryTable/SummaryTable.stories.tsx
@@ -15,10 +15,26 @@ const items = [
   { isBold: true, label: 'Din månedspris', value: '1395,-' },
 ];
 
+const itemsWithDescriptions = [
+  { label: '6 GB', price: 399, value: '399,-', description: 'Hello world 1' },
+  { label: '6 GB m/rabatt', price: 249, value: '249,-' },
+  { label: '6 GB  m/rabatt', price: 249, value: '249,-', description: 'Hello world 2' },
+  { isBold: true, isSuccess: true, label: 'Rabatt', value: `-XXX,-`, description: 'Hello world 3' },
+  { isBold: true, isStriketrough: true, label: 'Ordinærpris', value: '1395,-', description: 'Hello world 4' },
+  { isBold: true, label: 'Din månedspris', value: '1395,-', description: 'Hello world 5' },
+];
+
 const itemsCompact = [
   { label: '6 GB', price: 399, value: '399,-' },
   { label: '6 GB m/rabatt', price: 249, value: '249,-' },
   { label: '6 GB  m/rabatt', price: 249, value: '249,-' },
+  { isBold: true, label: 'Totalt pr.md. med familierabatt og årsavtale', valueStriketrough: '1500,-', value: '1395,-' },
+];
+
+const itemsCompactWithDescriptions = [
+  { label: '6 GB', price: 399, value: '399,-', description: 'text below 399' },
+  { label: '6 GB m/rabatt', price: 249, value: '249,-' },
+  { label: '6 GB  m/rabatt', price: 249, value: '249,-', description: 'text below 399' },
   { isBold: true, label: 'Totalt pr.md. med familierabatt og årsavtale', valueStriketrough: '1500,-', value: '1395,-' },
 ];
 
@@ -28,4 +44,42 @@ export const Default = () => {
 
 export const Compact = () => {
   return <SummaryTable kind="compact" items={itemsCompact} />;
+};
+
+export const CompactGrey = () => {
+  return <SummaryTable kind="compact" items={itemsCompact} useGreyBackground={true} />;
+};
+
+export const CompactGreyNoIndentWithBadge = () => {
+  return <SummaryTable kind="compact" items={itemsCompact} useGreyBackground={true} useIndent={false} badgeText="Ok" />;
+};
+
+export const CompactGreyWithAdditionalValues = () => {
+  return <SummaryTable kind="compact" items={itemsCompactWithDescriptions} useGreyBackground={true} />;
+};
+
+export const CompactGreyWithAdditionalValuesNoIndent = () => {
+  return (
+    <SummaryTable kind="compact" items={itemsCompactWithDescriptions} useGreyBackground={true} useIndent={false} />
+  );
+};
+
+export const NormalGreyWithDescriptionsWithIndent = () => {
+  return <SummaryTable items={itemsWithDescriptions} useGreyBackground={true} useIndent={true} />;
+};
+
+export const NormalGreyWithDescriptionsWithIndentAndTitle = () => {
+  return <SummaryTable items={itemsWithDescriptions} useGreyBackground={true} useIndent={true} title="Hello world" />;
+};
+
+export const NormalGreyWithDescriptionsWithIndentAndBadge = () => {
+  return (
+    <SummaryTable
+      items={itemsWithDescriptions}
+      useGreyBackground={true}
+      useIndent={true}
+      badgeStatus="ok"
+      badgeText="Du sparer 2000,-"
+    />
+  );
 };
--- src/atoms/SummaryTable/SummaryTable.tsx
+++ src/atoms/SummaryTable/SummaryTable.tsx
@@ -1,10 +1,12 @@
 import cn from 'classnames';
 import React from 'react';
 import _ from 'lodash';
+import { Badge } from '../Badge';
 
 export interface SummaryTableItem {
   label: string;
   value: string;
+  description?: string;
   valueStriketrough?: string;
   price?: number;
   isSuccess?: boolean;
@@ -15,28 +17,65 @@ export interface SummaryTableItem {
 export interface SummaryTableProps {
   items: SummaryTableItem[];
   kind?: 'normal' | 'compact';
+  title?: string;
+  badgeText?: string;
+  badgeStatus?: 'ok' | 'caution' | 'warning' | 'communication' | 'offer' | 'communication-light';
+  useGreyBackground?: boolean;
+  useIndent?: boolean;
 }
 
-const SummaryTable = ({ items, kind = 'normal' }: SummaryTableProps) => (
-  <dl className={cn('summary-table', { [`summary-table--${kind}`]: true })}>
-    {items.map((item, index) => (
-      <div
-        key={`${item.label}-${index}`}
-        className={cn('summary-table__row', {
-          'summary-table__row--bold': item.isBold,
-          'summary-table__row--green': item.isSuccess,
-          'summary-table__row--st': item.isStriketrough,
-          'summary-table__row--indent': index > 0 && index < _.size(items) - 1,
-        })}
-      >
-        <dt className="summary-table__label">{item.label}</dt>
-        <dd className="summary-table__value">
-          {item.valueStriketrough && (
-            <span className="summary-table__value--striketrough">{item.valueStriketrough}</span>
-          )}
-          {item.value}
-        </dd>
+const SummaryTable = ({
+  items,
+  kind = 'normal',
+  title = '',
+  useGreyBackground = false,
+  useIndent = true,
+  badgeText = '',
+  badgeStatus = 'ok',
+}: SummaryTableProps) => (
+  <dl
+    className={cn('summary-table', {
+      [`summary-table--${kind}`]: true,
+      'summary-table--background-grey': useGreyBackground,
+    })}
+  >
+    {badgeText && (
+      <div className="summary-table__badge-container">
+        <Badge status={badgeStatus} text={badgeText} />
       </div>
+    )}
+
+    {title && <h3>{title}</h3>}
+
+    {items.map((item, index) => (
+      <>
+        <div
+          key={`${item.label}-${index}`}
+          className={cn('summary-table__row', {
+            'summary-table__row--bold': item.isBold,
+            'summary-table__row--green': item.isSuccess,
+            'summary-table__row--st': item.isStriketrough,
+            'summary-table__row--indent': useIndent && index > 0 && index < _.size(items) - 1,
+            'summary-table__row--first': index == 0 && !title,
+          })}
+        >
+          <div className="summary-table__row-container">
+            <dt className="summary-table__label">{item.label}</dt>
+            <dd className="summary-table__value">
+              {item.valueStriketrough && (
+                <span className="summary-table__value--striketrough">{item.valueStriketrough}</span>
+              )}
+              {item.value}
+            </dd>
+          </div>
+
+          {item.description && (
+            <div className="summary-table__row-description">
+              <span className="summary-table__description">{item.description}</span>
+            </div>
+          )}
+        </div>
+      </>
     ))}
   </dl>
 );
