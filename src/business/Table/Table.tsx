aa53c4074088f9b62ca88ecb125f37af331486ea
Tomas Anikevičius <97449975+tomas-anikevicius@users.noreply.github.com>
2022-12-16 11:05:34 +0200

    feat: business list empty state (#1296)

--- src/business/Table/Table.tsx
+++ src/business/Table/Table.tsx
@@ -4,7 +4,7 @@ import _ from 'lodash';
 
 import { Icon } from '../../atoms/Icon';
 import { Checkbox } from '../../atoms/Checkbox';
-import { emptyTableSvg } from './emptyTableSvg';
+import { emptySvg } from '../assets/emptySvg';
 
 type TableBodyCellProps = {
   rightAligned?: boolean;
@@ -248,7 +248,7 @@ export const Table: React.FC<TableProps> = (props) => {
     <>
       {'tableIsEmpty' in props && props.tableIsEmpty && !props.loading ? (
         <div className="data-table__empty-state">
-          {emptyTableSvg}
+          {emptySvg}
           <div>{props.emptyTableLabel}</div>
         </div>
       ) : (
--- src/business/assets/emptySvg.tsx
+++ src/business/assets/emptySvg.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const emptyTableSvg = (
+export const emptySvg = (
   <svg width="316" height="217" viewBox="0 0 316 217" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       opacity="0.3"
