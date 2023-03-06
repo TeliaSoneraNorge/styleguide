485395f9f2e0843fcf1f19b8adada9778ebce8b2
Jan Erik <janegge1990@gmail.com>
2023-03-06 12:41:29 +0100

    update interface (#1310)

--- src/molecules/TextAreaWithLabel/TextAreaWithLabel.d.ts
+++ src/molecules/TextAreaWithLabel/TextAreaWithLabel.d.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-declare interface TextAreaWithLabelProps {
+declare interface TextAreaWithLabelProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   labelText: string;
   hideLabel?: boolean;
   placeholder?: string;
