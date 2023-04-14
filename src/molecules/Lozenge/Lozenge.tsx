0bd11c806a8fa47c28399f3f35022da6019df543
Justas <justas.sobutas@teliacompany.com>
2023-04-14 11:16:21 +0300

    NOCT-29370: add data tracking id prop

--- src/molecules/Lozenge/Lozenge.tsx
+++ src/molecules/Lozenge/Lozenge.tsx
@@ -22,13 +22,17 @@ export interface LozengeProps {
   type?: Type;
   className?: string;
   icon?: IconDefinition;
+  dataTrackingId?: string;
 }
 
 export const Lozenge = (props: LozengeProps) => {
-  const { label, status = 'neutral', type = 'round', className } = props;
+  const { label, status = 'neutral', type = 'round', className, dataTrackingId } = props;
 
   return (
-    <div className={cn('telia-lozenge', `telia-lozenge__${status}`, `telia-lozenge__${type}`, className)}>
+    <div
+      className={cn('telia-lozenge', `telia-lozenge__${status}`, `telia-lozenge__${type}`, className)}
+      data-tracking-id={dataTrackingId}
+    >
       {props.icon && <Icon icon={props.icon} />} {label}
     </div>
   );
