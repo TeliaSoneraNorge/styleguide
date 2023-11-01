d3eaeb73db778c1617e4ead2a17a0f4808ff9578
Fredrik Oterholt <fredrik@technologic.as>
2023-11-01 16:39:44 +0100

    readd rules

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,14 +8,12 @@ module.exports = {
   docs: {
     autodocs: true,
   },
-  /*
   async webpackFinal(config) {
-
     process.env['STORYBOOK'] = true;
 
     config.module.rules.push({
       test: /\.md$/,
-      loader: require.resolve('markdown-loader'),
+      use: require.resolve('markdown-loader'),
     });
 
     config.resolve.extensions.push('.ts', '.tsx');
@@ -23,18 +21,19 @@ module.exports = {
     config.module.rules.push({
       test: /\.pcss$/,
       sideEffects: true,
-      loaders: ['style-loader', 'postcss-loader'],
+      use: ['style-loader', 'postcss-loader'],
     });
 
     config.module.rules.push({
       test: '/.(png|jpg)',
-      loader: 'file-loader',
-      options: {
-        name: 'static/media/[name].[hash:8].[ext]',
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
       },
     });
 
     return config;
   },
-  */
 };
