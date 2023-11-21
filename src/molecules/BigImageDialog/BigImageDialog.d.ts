07c2ffa95513f67181866aac5d7cb1b543a10d81
Alfred Birketvedt <alfredbirk@gmail.com>
2023-11-21 15:18:39 +0100

    BigImageDialog style changes (#1427)

--- src/molecules/BigImageDialog/BigImageDialog.d.ts
+++ src/molecules/BigImageDialog/BigImageDialog.d.ts
@@ -3,6 +3,7 @@ import * as React from 'react';
 export interface BigImageDialogProps {
   name: string;
   heading: string;
+  topHeading: string;
   description?: string;
   src: string;
   children: React.ReactNode;
