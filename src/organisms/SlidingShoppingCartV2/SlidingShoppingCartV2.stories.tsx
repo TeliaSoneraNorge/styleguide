ffe03e7e05355c342567b66abcb2cc54f99d9af7
Jan Erik <janegge1990@gmail.com>
2023-12-12 10:23:48 +0100

    Cart toast on mobile (#1431)
    * wip
    
    * use breakpoints and show overlay
    
    * add opt-in to show on mobile
    
    * Apply suggestions from code review
    
    Co-authored-by: Joakim Kristiansen <joakikr@gmail.com>
    
    ---------
    
    Co-authored-by: Joakim Kristiansen <joakikr@gmail.com>

--- src/organisms/SlidingShoppingCartV2/SlidingShoppingCartV2.stories.tsx
+++ src/organisms/SlidingShoppingCartV2/SlidingShoppingCartV2.stories.tsx
@@ -11,6 +11,63 @@ export default {
   },
 };
 
+const heading = 'Handlekurv';
+const pricePerMonth = 500;
+const priceUpfront = 500;
+const priceFirstInvoice = 500;
+
+const priceDetails = [
+  { label: 'Delbetaling telefon 12 md.', value: '1583,-/md.' },
+  { label: 'Mobilabonnement 8 GB', value: '329,-/md.' },
+];
+const delivery = {
+  label: 'Bedriftspakken',
+  value: 'Fri frakt',
+};
+
+const leaseContent: ICartItem[] = [
+  {
+    type: 'HANDSET',
+    subtype: '',
+    href: '#',
+    id: '9076528',
+    bundleId: 'e16132',
+    brand: 'Samsung',
+    brandSlug: 'samsung',
+    model: 'Galaxy S20 Ultra 5G 128 GB',
+    modelSlug: 'galaxy-s20-ultra',
+    subtitle: '',
+    code: '',
+    color: 'Grå',
+    modelSize: '256 GB',
+    lineThrough: '',
+    image: {
+      url:
+        '//images.ctfassets.net/iz15t1lxx44v/6wL89AOWs6Z7dMISvz6Ta1/0eee85d33d7b20632d706ce51df0836e/samsung-galaxy-s20-ultra-5g-cosmic-gray-2.png',
+    },
+    name: 'Samsung Galaxy S20 Ultra 5G',
+    quantity: {
+      modifiable: false,
+      removable: true,
+      value: 1,
+    },
+    price: {
+      upfront: 10799,
+      monthly: 799,
+      originalSalesPrice: 15790,
+    },
+    discount: {
+      value: {
+        upfront: 0,
+      },
+      description: '',
+    },
+    leaseMonths: '12',
+    isReSwitch: false,
+    isWebDeal: false,
+  },
+];
+
 const menuLinks = [
   {
     heading: { text: 'Privat', url: '#' },
@@ -42,64 +99,7 @@ const menuLinks = [
 ];
 
 export const Default = () => {
-  const heading = 'Handlekurv';
-  const pricePerMonth = 500;
-  const priceUpfront = 500;
-  const priceFirstInvoice = 500;
-
-  const priceDetails = [
-    { label: 'Delbetaling telefon 12 md.', value: '1583,-/md.' },
-    { label: 'Mobilabonnement 8 GB', value: '329,-/md.' },
-  ];
-  const delivery = {
-    label: 'Bedriftspakken',
-    value: 'Fri frakt',
-  };
   const [shouldShowCart, setShouldShowCart] = useState(false);
-
-  const leaseContent: ICartItem[] = [
-    {
-      type: 'HANDSET',
-      subtype: '',
-      href: '#',
-      id: '9076528',
-      bundleId: 'e16132',
-      brand: 'Samsung',
-      brandSlug: 'samsung',
-      model: 'Galaxy S20 Ultra 5G 128 GB',
-      modelSlug: 'galaxy-s20-ultra',
-      subtitle: '',
-      code: '',
-      color: 'Grå',
-      modelSize: '256 GB',
-      lineThrough: '',
-      image: {
-        url:
-          '//images.ctfassets.net/iz15t1lxx44v/6wL89AOWs6Z7dMISvz6Ta1/0eee85d33d7b20632d706ce51df0836e/samsung-galaxy-s20-ultra-5g-cosmic-gray-2.png',
-      },
-      name: 'Samsung Galaxy S20 Ultra 5G',
-      quantity: {
-        modifiable: false,
-        removable: true,
-        value: 1,
-      },
-      price: {
-        upfront: 10799,
-        monthly: 799,
-        originalSalesPrice: 15790,
-      },
-      discount: {
-        value: {
-          upfront: 0,
-        },
-        description: '',
-      },
-      leaseMonths: '12',
-      isReSwitch: false,
-      isWebDeal: false,
-    },
-  ];
-
   return (
     <>
       <Menu
@@ -120,7 +120,63 @@ export const Default = () => {
         onCartClick={() => setShouldShowCart(!shouldShowCart)}
         numberOfItemsInCart={3}
       />
-      <SlidingShoppingCartV2 shouldShowCart={shouldShowCart} setShouldShowCart={setShouldShowCart}>
+      <SlidingShoppingCartV2
+        numberOfItemsInCart={3}
+        showMobileCart={false}
+        pricePerMonth={pricePerMonth}
+        shouldShowCart={shouldShowCart}
+        setShouldShowCart={setShouldShowCart}
+      >
+        <ShoppingCart
+          heading={heading}
+          cartItems={leaseContent}
+          delivery={delivery}
+          totalPriceFirstInvoice={priceFirstInvoice}
+          totalPriceMonthly={pricePerMonth}
+          monthlyPriceDetails={priceDetails}
+          disclaimer={'Total telefonpris med SVITSJ i 24 md.: 16 056,-'}
+          totalPriceUpfront={priceUpfront}
+          totalVAT={150}
+          totalPriceWithoutVAT={350}
+          onChangeQuantity={() => {}}
+          onRemoveItem={() => {}}
+          formatPrice={(price: any) => `${price},-`}
+          isAnyCartItemsQuantityModifiable
+        />
+      </SlidingShoppingCartV2>
+    </>
+  );
+};
+
+export const SlidingShoppingCartV2WithMobileCart = () => {
+  const [shouldShowCart, setShouldShowCart] = useState(false);
+  return (
+    <>
+      <Menu
+        loginUrl="#Menu"
+        logoUrl="#"
+        logoTitle="Telia logo"
+        activeIndex={0}
+        activeLinkIndex={-1}
+        menuLinks={menuLinks}
+        logoImageDesktopPath={img.logo}
+        logoImageInverseDesktopPath={img.logoInverted}
+        onSearchSubmit={() => {}}
+        searchLabel="Fyll inn det du skal søke på"
+        searchButtonLabel="Søk"
+        searchButtonAbortText="Lukk"
+        mobileMenuCloseButtonLabel="Lukk"
+        lockBodyOnMenuOpen
+        onCartClick={() => setShouldShowCart(!shouldShowCart)}
+        numberOfItemsInCart={3}
+      />
+      <SlidingShoppingCartV2
+        numberOfItemsInCart={3}
+        showMobileCart={true}
+        pricePerMonth={pricePerMonth}
+        shouldShowCart={shouldShowCart}
+        setShouldShowCart={setShouldShowCart}
+      >
         <ShoppingCart
           heading={heading}
           cartItems={leaseContent}
--- src/organisms/SlidingShoppingCartV2/SlidingShoppingCartV2.tsx
+++ src/organisms/SlidingShoppingCartV2/SlidingShoppingCartV2.tsx
@@ -4,14 +4,30 @@ import { useClickOutsideListener } from '../SlidingShoppingCart/useClickOutsideL
 import { useFocusTrap } from '../../utils/useFocusTrap';
 import { useEscapeListener } from '../../molecules/Modal/useEscapeListener';
 import { Icon } from '../../atoms/Icon';
+import { Badge } from '../../atoms/Badge';
+import Heading from '../../atoms/Heading';
+import Button from '../../atoms/Button';
+import { useBreakpoint } from '../../utils/useBreakpoint';
 
 export interface SlidingShoppingCartV2Props {
   shouldShowCart: boolean;
+  showMobileCart?: boolean;
   children: React.ReactNode;
   setShouldShowCart(shouldShowCart: boolean): void;
+  numberOfItemsInCart?: number;
+  pricePerMonth?: number;
+  priceUpfront?: number;
 }
 
-const SlidingShoppingCartV2 = ({ setShouldShowCart, shouldShowCart, children }: SlidingShoppingCartV2Props) => {
+const SlidingShoppingCartV2 = ({
+  setShouldShowCart,
+  shouldShowCart,
+  showMobileCart = false,
+  children,
+  numberOfItemsInCart,
+  pricePerMonth,
+  priceUpfront,
+}: SlidingShoppingCartV2Props) => {
   const { container } = useFocusTrap();
   const ref = useRef<HTMLDivElement>(null);
 
@@ -22,9 +38,59 @@ const SlidingShoppingCartV2 = ({ setShouldShowCart, shouldShowCart, children }: 
     ref,
   });
 
-  return (
+  const xs = useBreakpoint('xs');
+  const isMobile = !xs;
+
+  return showMobileCart ? (
     <div ref={container}>
-      <div className={cn({ 'telia-sliding-shopping-cart__overlay': shouldShowCart })} />
+      {isMobile && (
+        <>
+          <div className={cn({ 'telia-sliding-shopping-cart__overlay': shouldShowCart })} />
+          <div className="telia-sliding-shopping-cart-mobile" ref={ref}>
+            <div
+              className={cn('telia-sliding-shopping-cart-mobile__wrapper', {
+                'telia-sliding-shopping-cart-mobile__wrapper--open': shouldShowCart,
+              })}
+            >
+              {!shouldShowCart && (
+                <div className="telia-sliding-shopping-cart-mobile__wrapper--cart">
+                  {numberOfItemsInCart && (
+                    <Badge text={`${numberOfItemsInCart}`}>
+                      <Icon icon="shoppingcart" />
+                    </Badge>
+                  )}
+                  <Heading tag="h2" size="s">
+                    {priceUpfront ? `${priceUpfront},-` : `${pricePerMonth},-/md.`}
+                  </Heading>
+                </div>
+              )}
+              <Button
+                className="telia-sliding-shopping-cart-mobile__wrapper--button"
+                icon={shouldShowCart ? 'chevron-down' : 'chevron-up'}
+                onClick={() => setShouldShowCart(!shouldShowCart)}
+                kind="link"
+              />
+            </div>
+            {shouldShowCart && children}
+          </div>
+        </>
+      )}
+      {!isMobile && (
+        <aside
+          ref={ref}
+          className={cn('telia-sliding-shopping-cart__container', {
+            'telia-sliding-shopping-cart__container--show': shouldShowCart,
+          })}
+        >
+          <button className="telia-sliding-shopping-cart__close-button" onClick={() => setShouldShowCart(false)}>
+            <Icon icon="close" />
+          </button>
+          {children}
+        </aside>
+      )}
+    </div>
+  ) : (
+    <div ref={container}>
       <aside
         ref={ref}
         className={cn('telia-sliding-shopping-cart__container', {
