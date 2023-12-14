3c6153da9965225663d7c484629b0db0212d13b6
SimkeAsta <simke.asta@gmail.com>
2023-12-14 15:46:08 +0200

    position change for carousel

--- src/molecules/Carousel/Carousel.tsx
+++ src/molecules/Carousel/Carousel.tsx
@@ -34,7 +34,7 @@ export const Carousel: React.FC<CarouselProps> = ({ items }: CarouselProps) => {
     const width = window.innerWidth;
     if (width > 1600) return 4;
     if (width > 1300) return 3;
-    if (width > 424) return 2;
+    if (width >= 410) return 2;
     return 1;
   }, []);
 
