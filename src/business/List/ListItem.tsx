d744fa1d4909077721a55b22c5192514b7d4d368
Tomas Anikevičius <97449975+tomas-anikevicius@users.noreply.github.com>
2024-01-02 14:01:20 +0200

    feat(ListItem): No horizontal paddings for list items without background (#1447)

--- src/business/List/ListItem.tsx
+++ src/business/List/ListItem.tsx
@@ -44,6 +44,7 @@ export const ListItem: React.FC<ListItemProps & ListStyle> = (props) => {
         'telia-listItem--dark': color === 'dark',
         'telia-listItem--medium': color === 'medium',
         'telia-listItem--noBG': color !== 'dark' && color !== 'medium' && color !== 'white',
+        'telia-listItem--noHorizontalPaddingsOnMobile': !border && !color && !type,
       })}
     >
       <InnerTag
