754490d78dc084ee5381a4f08ed587677f3a4dfd
Lars Erik Skraastad <larserikskraastad@gmail.com>
2024-01-24 12:58:53 +0100

    add attention status

--- src/molecules/Lozenge/Lozenge.tsx
+++ src/molecules/Lozenge/Lozenge.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import cn from 'classnames';
+import React from 'react';
 import { Icon, IconDefinition } from '../../atoms/Icon';
 
-type Status = 'positive' | 'negative' | 'warning' | 'neutral' | 'communication' | 'info';
+type Status = 'positive' | 'negative' | 'warning' | 'neutral' | 'communication' | 'info' | 'attention';
 type Type = 'round' | 'square';
 
 export interface LozengeProps {
