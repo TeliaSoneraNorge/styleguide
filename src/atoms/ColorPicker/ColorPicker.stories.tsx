571a5dbd8c866086c7e56a2d17850b6e10b74590
Kristian Harlem <117192532+KristianHarlem@users.noreply.github.com>
2024-04-03 15:13:09 +0200

    ColorPicker: support both button and link (#1501)

--- src/atoms/ColorPicker/ColorPicker.stories.tsx
+++ src/atoms/ColorPicker/ColorPicker.stories.tsx
@@ -57,3 +57,27 @@ export const SmallWithLabel = () => {
     />
   );
 };
+
+const colorsWithLink = [
+  { name: 'Grey', color: 'grey', id: 'grey', href: '/mobiltelefoner/apple/iphone-15-pro-max/256-gb-svart-titan/' },
+  {
+    name: 'Light',
+    color: '#FFDAAF',
+    id: 'light',
+    href: '/mobiltelefoner/apple/iphone-15-pro-max/256-gb-svart-titan2/',
+  },
+  { name: 'Black', color: '#000', id: 'black', href: '/mobiltelefoner/apple/iphone-15-pro-max/256-gb-svart-titan3/' },
+];
+export const WithLink = () => {
+  const [selected, setSelected] = useState<Color>();
+  return (
+    <ColorPicker
+      size="m"
+      showSelected={false}
+      selected={selected}
+      onSelect={(id) => setSelected(colorsWithLink.find((c) => c.id === id))}
+      colors={colorsWithLink}
+      component="link"
+    />
+  );
+};
