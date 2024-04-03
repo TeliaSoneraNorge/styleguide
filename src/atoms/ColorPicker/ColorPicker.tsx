434bafa4575c6c1f03a996327283127de8f859a2
Kristian Harlem <117192532+KristianHarlem@users.noreply.github.com>
2024-04-03 13:03:07 +0000

    ColorPicker: support both button and link

--- src/atoms/ColorPicker/ColorPicker.tsx
+++ src/atoms/ColorPicker/ColorPicker.tsx
@@ -8,6 +8,7 @@ export interface Color {
   id: ColorId;
   color: string;
   name: string;
+  href?: string;
 }
 
 export interface ColorPickerProps {
@@ -16,6 +17,7 @@ export interface ColorPickerProps {
   colors: Color[];
   size?: 's' | 'm' | 'l';
   onSelect?: (id: ColorId) => void;
+  component?: 'button' | 'link';
 }
 
 const ColorPicker = ({
@@ -24,27 +26,37 @@ const ColorPicker = ({
   colors = [],
   size = 'l',
   onSelect = () => undefined,
-}: ColorPickerProps) => (
-  <div
-    className={cn('color-picker', {
-      [`color-picker-size-${size}`]: true,
-    })}
-  >
-    <div className="color-picker__colors">
-      {colors.map((color) => (
-        <button
-          className={cn('color-picker__button', {
-            'color-picker__selected': _.get(selected, 'id') === color.id,
-          })}
-          style={{ backgroundColor: color.color }}
-          aria-label={color.name}
-          key={color.id}
-          onClick={() => onSelect(color.id)}
-        />
-      ))}
+  component = 'button',
+}: ColorPickerProps) => {
+  const Tag: any = component === 'link' ? 'a' : component;
+  return (
+    <div
+      className={cn('color-picker', {
+        [`color-picker-size-${size}`]: true,
+      })}
+    >
+      <div className="color-picker__colors">
+        {colors.map((color) => (
+          <Tag
+            href={color.href}
+            className={cn('color-picker__button', {
+              'color-picker__selected': _.get(selected, 'id') === color.id,
+            })}
+            style={{ backgroundColor: color.color }}
+            aria-label={color.name}
+            key={color.id}
+            onClick={(event: React.MouseEvent) => {
+              event.preventDefault();
+              onSelect(color.id);
+            }}
+          />
+        ))}
+      </div>
+      {!_.isEmpty(selected) && showSelected && (
+        <div className="color-picker__preview">{_.get(selected, 'name', '')}</div>
+      )}
     </div>
-    {!_.isEmpty(selected) && showSelected && <div className="color-picker__preview">{_.get(selected, 'name', '')}</div>}
-  </div>
-);
+  );
+};
 
 export default ColorPicker;
