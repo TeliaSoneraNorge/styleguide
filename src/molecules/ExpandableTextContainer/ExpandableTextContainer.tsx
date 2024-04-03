31719806cc0839529329d06e2a422c0266ade7d9
Jan Erik <janegge1990@gmail.com>
2024-04-03 12:35:47 +0200

    add white and gray version and use id for key (#1500)
    * add white and gray version and use id for key
    
    * fix color
    
    * place button at bottom
    
    * use div
    
    * move button outside of gradient
    
    * remove classname old structure

--- src/molecules/ExpandableTextContainer/ExpandableTextContainer.tsx
+++ src/molecules/ExpandableTextContainer/ExpandableTextContainer.tsx
@@ -5,42 +5,48 @@ import cn from 'classnames';
 import Paragraph from '../../atoms/Paragraph';
 import Button from '../../atoms/Button';
 
-type textBlock = {
-  heading: string;
-  content: string;
+type Props = {
+  textBlocks: {
+    id: string;
+    heading: string;
+    content: string;
+  }[];
+  whiteBg?: boolean;
 };
 
-const ExpandableTextContainer = ({ textBlocks }: { textBlocks: textBlock[] }) => {
+const ExpandableTextContainer: React.FC<Props> = ({ textBlocks, whiteBg = false }) => {
   const [expanded, setExpanded] = useState(false);
   if (!textBlocks) {
     return null;
   }
 
   return (
-    <Container
-      className={cn('telia-expandableTextContainer', {
-        ['telia-expandableTextContainer--expanded']: expanded,
-      })}
-    >
-      {textBlocks.map((block) => (
-        <div className="telia-expandableTextContainer__textblock">
-          <Heading tag="h2" size="xs">
-            {block.heading}
-          </Heading>
-          <Paragraph>{block.content}</Paragraph>
-        </div>
-      ))}
-      <span className={cn('button-container', { ['gradient']: !expanded })}>
+    <div className="telia-expandableTextContainer">
+      <div
+        className={cn('telia-expandableTextContainer__textblock-container', {
+          ['telia-expandableTextContainer__textblock-container--expanded']: expanded,
+        })}
+      >
+        {textBlocks.map((block) => (
+          <div key={`${block.id}-${block.heading}`} className="telia-expandableTextContainer__textblock">
+            <Heading tag="h2" size="xs">
+              {block.heading}
+            </Heading>
+            <Paragraph>{block.content}</Paragraph>
+          </div>
+        ))}
+        <span className={cn({ ['gradient']: !expanded, ['gradient--white']: !expanded && whiteBg })}></span>
+      </div>
+      <div className="telia-expandableTextContainer__button-container">
         <Button
-          className="telia-expandableTextContainer__button"
           kind="link"
           iconPlacement="right"
           icon={expanded ? 'chevron-up' : 'chevron-down'}
           text="Les mer"
           onClick={() => setExpanded(!expanded)}
         />
-      </span>
-    </Container>
+      </div>
+    </div>
   );
 };
 
