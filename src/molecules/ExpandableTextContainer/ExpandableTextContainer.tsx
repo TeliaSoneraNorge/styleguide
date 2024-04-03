60cec678620b248aaf1058597b4986f0a75e1988
klounerik <janegge1990@gmail.com>
2024-04-03 10:44:49 +0200

    add white and gray version and use id for key

--- src/molecules/ExpandableTextContainer/ExpandableTextContainer.tsx
+++ src/molecules/ExpandableTextContainer/ExpandableTextContainer.tsx
@@ -5,12 +5,16 @@ import cn from 'classnames';
 import Paragraph from '../../atoms/Paragraph';
 import Button from '../../atoms/Button';
 
-type textBlock = {
-  heading: string;
-  content: string;
+type Props = {
+  textBlocks: {
+    id: string;
+    heading: string;
+    content: string;
+  }[];
+  whiteBg?: boolean;
 };
 
-const ExpandableTextContainer = ({ textBlocks }: { textBlocks: textBlock[] }) => {
+const ExpandableTextContainer: React.FC<Props> = ({ textBlocks, whiteBg = false }) => {
   const [expanded, setExpanded] = useState(false);
   if (!textBlocks) {
     return null;
@@ -23,14 +27,14 @@ const ExpandableTextContainer = ({ textBlocks }: { textBlocks: textBlock[] }) =>
       })}
     >
       {textBlocks.map((block) => (
-        <div className="telia-expandableTextContainer__textblock">
+        <div key={`${block.id}-${block.heading}`} className="telia-expandableTextContainer__textblock">
           <Heading tag="h2" size="xs">
             {block.heading}
           </Heading>
           <Paragraph>{block.content}</Paragraph>
         </div>
       ))}
-      <span className={cn('button-container', { ['gradient']: !expanded })}>
+      <span className={cn('button-container', { ['gradient']: !expanded, ['gradient--white']: !expanded && whiteBg })}>
         <Button
           className="telia-expandableTextContainer__button"
           kind="link"
