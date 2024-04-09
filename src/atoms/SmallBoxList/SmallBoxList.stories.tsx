0d6ceddf27066ee398b9012a5e079551742c0c9e
Kristian Harlem <117192532+KristianHarlem@users.noreply.github.com>
2024-04-09 16:07:26 +0200

    Add link option to Small ListBox (#1508)

--- src/atoms/SmallBoxList/SmallBoxList.stories.tsx
+++ src/atoms/SmallBoxList/SmallBoxList.stories.tsx
@@ -12,6 +12,12 @@ const boxes = [
   { name: '256 GB', id: '256-gb' },
 ];
 
+const boxesWithLink = [
+  { name: '64 GB', id: '64-gb', href: '/mobiltelefoner/apple/iphone-15-pro-max/64-gb-svart-titan/' },
+  { name: '128 GB', id: '128-gb', href: '/mobiltelefoner/apple/iphone-15-pro-max/128-gb-svart-titan/' },
+  { name: '256 GB', id: '256-gb', href: '/mobiltelefoner/apple/iphone-15-pro-max/256-gb-svart-titan/' },
+];
+
 const differentLengths = [
   { name: '64 GB lorem ipsum', id: '64-gb' },
   { name: '128 GB', id: '128-gb' },
@@ -27,6 +33,11 @@ const purpleVariants: SmallBox[] = [
 export const Default = () => {
   const [selected, setSelected] = useState<string>();
   return <SmallBoxList selected={selected} onSelect={setSelected} boxes={boxes} />;
+};
+
+export const VariantAsLink = () => {
+  const [selected, setSelected] = useState<string>();
+  return <SmallBoxList selected={selected} onSelect={setSelected} boxes={boxesWithLink} component="link" />;
 };
 
 export const UnevenTextLengths = () => {
--- src/atoms/SmallBoxList/SmallBoxList.tsx
+++ src/atoms/SmallBoxList/SmallBoxList.tsx
@@ -6,29 +6,44 @@ type SmallBoxId = string;
 export interface SmallBox {
   id: SmallBoxId;
   name: string;
+  href?: string;
 }
 
 export interface SmallBoxListProps {
   selected?: SmallBoxId;
   boxes: SmallBox[];
   type?: 'normal' | 'purple';
+  component?: 'button' | 'link';
   onSelect?: (id: SmallBoxId) => void;
 }
 
-const SmallBoxList = ({ selected, boxes = [], type, onSelect = () => undefined }: SmallBoxListProps) => (
-  <div className={cn('telia-small-box-list', { 'telia-small-box-list--purple': type === 'purple' })}>
-    {boxes.map((box) => (
-      <button
-        className={cn('telia-small-box-list__button', {
-          'telia-small-box-list__button--selected': selected === box.id,
-        })}
-        key={box.id}
-        onClick={() => onSelect(box.id)}
-      >
-        {box.name}
-      </button>
-    ))}
-  </div>
-);
+const SmallBoxList = ({
+  selected,
+  boxes = [],
+  type,
+  onSelect = () => undefined,
+  component = 'button',
+}: SmallBoxListProps) => {
+  const Tag: any = component === 'link' ? 'a' : component;
+  return (
+    <div className={cn('telia-small-box-list', { 'telia-small-box-list--purple': type === 'purple' })}>
+      {boxes.map((box) => (
+        <Tag
+          href={box.href}
+          className={cn('telia-small-box-list__button', {
+            'telia-small-box-list__button--selected': selected === box.id,
+          })}
+          key={box.id}
+          onClick={(event: React.MouseEvent) => {
+            event.preventDefault();
+            onSelect(box.id);
+          }}
+        >
+          <span>{box.name}</span>
+        </Tag>
+      ))}
+    </div>
+  );
+};
 
 export default SmallBoxList;
