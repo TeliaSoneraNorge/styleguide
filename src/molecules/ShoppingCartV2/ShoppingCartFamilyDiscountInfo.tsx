5a4d5dfef305a0e56e01ea2d762567c3b36f0706
KlounErik <janegge1990@gmail.com>
2025-02-19 13:41:25 +0100

    show name and link

--- src/molecules/ShoppingCartV2/ShoppingCartFamilyDiscountInfo.tsx
+++ src/molecules/ShoppingCartV2/ShoppingCartFamilyDiscountInfo.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import Heading from '../../atoms/Heading';
 import { Icon } from '../../atoms/Icon';
 
-const ShoppingCartFamilyDiscountInfo = () => (
+const ShoppingCartFamilyDiscountInfo = ({ name, link }: { name?: string; link?: JSX.Element }) => (
   <div className="shopping-cart-family-discount-info">
     <div className="shopping-cart-family-discount-info__heading-container">
-      <Icon className="shopping-cart-family-discount-info__heading-icon" icon="money-krone" />
+      <Icon className="shopping-cart-family-discount-info__heading-icon" icon={name ? 'lightbulb' : 'money-krone'} />
       <Heading tag="h3" size="s">
-        Du kan få rabatt
+        {name ? `Hei ${name}!` : 'Du kan få rabatt'}
       </Heading>
     </div>
     <div className="shopping-cart-family-discount-info__content">
-      Ved å legge flere mobilabonnement i handlekurven vil du få rabatt hver måned. Du betaler fullpris kun for det
-      største abonnementet, resten får du rabatt på. Få 30,-/md. for fast datamengde og 100,-/md. for ubegrenset data.
+      Visste du at hvis du legger til flere abonnement hos oss får du familierabatt?
     </div>
+    {link}
   </div>
 );
 
